a2abb534b2559dfc951c722fba789c735aa9fb2f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-22 17:17:12 +0500

    browser router to be added here

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import Sidebar from './components/SideBar'
+import HomePage from './components/Homepage'
 
 function App() {
   return (
     <div className=" flex min-h-screen">
       <Sidebar />
 
-      <h1 className="text-3xl font-bold text-[#003366]">Welcome to WorkWise</h1>
-      <p className="mt-4 text-gray-700">
-        This is your hub for managing tasks, check-ins, and more.
-      </p>
+      <HomePage />
     </div>
   )
 }
