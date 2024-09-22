e3a654397e3ef4f577ab97eeb953e8441ad60d3a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-22 16:57:57 +0500

    styled sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import SidebarLinks from './SidebarLinks'
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -8,7 +9,7 @@ const Sidebar = () => {
   }
 
   return (
-    <div className="flex w-1/6">
+    <div className="flex w-1/5 ">
       {/* Hamburger button for mobile */}
       <button
         className="md:hidden p-2 text-white bg-[#003366] "
@@ -19,33 +20,21 @@ const Sidebar = () => {
 
       {/* Sidebar */}
       <div
-        className={`fixed inset-0  bg-[#003366] transition-transform transform md:relative md:flex md:flex-col w-full  ${
+        className={`fixed inset-0  bg-[#003366] transition-transform transform md:relative md:flex md:flex-col md:justify-between w-full  ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
-        } md:translate-x-0 md:block`}
+        } md:translate-x-0 md:block `}
       >
-        <div className="flex items-center justify-between p-4">
-          <div className="text-white text-xl font-bold">WorkWise</div>
-          <button className="text-white md:hidden" onClick={toggleSidebar}>
-            &times;
-          </button>
+        <div className=" flex flex-col gap-5 items-start">
+          <div className="flex items-center justify-start  ">
+            <img src="/Logo.png" alt="Logo" className=" w-28" />
+            <div className="text-white text-xl font-extrabold ">WorkWise</div>
+            <button className="text-white md:hidden" onClick={toggleSidebar}>
+              &times;
+            </button>
+          </div>
+          <SidebarLinks />
         </div>
-        <div className="flex flex-col p-4 space-y-2">
-          <a href="#" className="text-white hover:text-[#FF5733]">
-            Home
-          </a>
-          <a href="#" className="text-white hover:text-[#FF5733]">
-            Tasks
-          </a>
-          <a href="#" className="text-white hover:text-[#FF5733]">
-            Check In/Out
-          </a>
-          <a href="#" className="text-white hover:text-[#FF5733]">
-            Profile
-          </a>
-          <a href="#" className="text-white hover:text-[#FF5733]">
-            Logout
-          </a>
-        </div>
+        <div>user details</div>
       </div>
     </div>
   )
