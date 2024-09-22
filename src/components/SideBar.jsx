cc267d5a4698450499407b845a10047617920a17
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-22 17:24:44 +0500

    fixed sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
   }
 
   return (
-    <div className="flex w-1/5 ">
+    <div className="flex">
       {/* Hamburger button for mobile */}
       <button
         className="md:hidden p-2 text-white bg-[#003366] "
@@ -20,21 +20,29 @@ const Sidebar = () => {
 
       {/* Sidebar */}
       <div
-        className={`fixed inset-0  bg-[#003366] transition-transform transform md:relative md:flex md:flex-col md:justify-between w-full  ${
+        className={`fixed inset-0 bg-[#003366] transition-transform transform md:relative md:flex md:flex-col md:justify-between w-64 ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
-        } md:translate-x-0 md:block `}
+        } md:translate-x-0 md:block`}
       >
-        <div className=" flex flex-col gap-5 items-start">
-          <div className="flex items-center justify-start  ">
-            <img src="/Logo.png" alt="Logo" className=" w-28" />
-            <div className="text-white text-xl font-extrabold ">WorkWise</div>
-            <button className="text-white md:hidden" onClick={toggleSidebar}>
-              &times;
-            </button>
-          </div>
+        {/* Logo and Title */}
+        <div className="flex items-center">
+          <img src="/Logo.png" alt="Logo" className="w-24" />
+          <div className="text-white text-2xl font-extrabold ">WorkWise</div>
+          <button
+            className="text-white md:hidden ml-auto"
+            onClick={toggleSidebar}
+          >
+            &times;
+          </button>
+        </div>
+
+        {/* Sidebar Links */}
+        <div className="flex flex-col gap-5 items-start ">
           <SidebarLinks />
         </div>
-        <div>user details</div>
+
+        {/* User Details Section */}
+        <div className="text-white p-4 md:mt-auto">User Details</div>
       </div>
     </div>
   )
