eee0be5b02f7af52140b488691b21ab83881f343
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-24 03:46:58 +0500

    home setup mostly done

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,32 @@ import './App.css'
 import SignIn from './components/Auth/SignIn'
 import SignUp from './components/Auth/Register'
 import ResetPassword from './components/Auth/ResetPassword'
+import HomePage from './components/Homepage'
+import { useSelector } from 'react-redux'
+
+import Sidebar from './components/SideBar'
+import { RootState } from './utils/Redux/Store/Store'
 
 function App() {
+  const UserData = useSelector((state: RootState) => state.user)
   return (
-    <div className="    flex  items-center justify-center h-screen ">
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/Login" element={<SignIn />} />
-        <Route path="/Register" element={<SignUp />} />
-        <Route path="/ResetPassword" element={<ResetPassword />} />
-      </Routes>
-    </div>
+    <>
+      {!UserData.Name ? (
+        <div className="    flex  items-center justify-center h-screen ">
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/Login" element={<SignIn />} />
+            <Route path="/Register" element={<SignUp />} />
+            <Route path="/ResetPassword" element={<ResetPassword />} />
+          </Routes>
+        </div>
+      ) : (
+        <div className=" min-h-screen flex gap-4">
+          <Sidebar />
+          <HomePage />
+        </div>
+      )}
+    </>
   )
 }
 
