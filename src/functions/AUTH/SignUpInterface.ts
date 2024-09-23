b76d713c77a6f9cc9e1cc5efde070feda9552bf0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-23 21:06:33 +0500

    data types of sign in

---
+++ src/functions/AUTH/SignUpInterface.ts
@@ -0,0 +1,12 @@
+export interface InputValues {
+  email: string
+  password: string
+  Name: string
+  Image: File | null
+}
+export interface UserData {
+  Name: string
+  UserID: string
+  email: string
+  imageUrl: string
+}
