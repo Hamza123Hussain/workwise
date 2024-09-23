8ee5394478467638145df212e0951a15fa31dfc6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-24 04:39:16 +0500

    data types for task form

---
+++ src/utils/TaskformInterface.ts
@@ -0,0 +1,10 @@
+export interface TaskFormProps {
+  name: string
+  description: string
+  dueDate: string
+  assignedTo: string
+  setName: (value: string) => void
+  setDescription: (value: string) => void
+  setDueDate: (value: string) => void
+  setAssignedTo: (value: string) => void
+}
