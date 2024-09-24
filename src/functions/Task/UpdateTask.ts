39b7fba8938adc5fde878f5cbf115cec938059c0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-24 11:55:03 +0500

    updating progress of a task

---
+++ src/functions/Task/UpdateTask.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+// Function to create a task
+export const createTask = async (
+  id: string,
+  Email: string,
+  progressupdate: string
+) => {
+  try {
+    const response = await axios.put(
+      'http://localhost:8000/Api/Task/UpdateTask',
+      { id, Email, progressupdate }
+    )
+    return response.data // Return the response data from the backend
+  } catch (error) {
+    console.error('Error creating task:', error)
+    throw error // Propagate the error to be handled by the caller
+  }
+}
