4b61ca375cf5e25cf3e378a8a9140cc23ef12f8d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-24 11:56:37 +0500

    getting  all the attendance details

---
+++ src/functions/Attendance/AllAttendance.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const GetAllAttendance = async (email: string) => {
+  try {
+    const Response = await axios.get(
+      `http://localhost:8000/Api/Attendance/AllAttendance?Email=${email} `
+    )
+    if (Response.status === 200) {
+      console.log('DATA IS HERE', Response.data)
+      return Response.data
+    }
+  } catch (error) {
+    console.error('Error creating task:', error)
+    throw error // Propagate the error to be handled by the caller
+  }
+}
