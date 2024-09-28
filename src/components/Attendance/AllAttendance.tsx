e21ab87c24ffce6449cd11a7721cba7f5fa6dc98
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-28 16:40:56 +0500

    main/ parent function of the attendance

---
+++ src/components/Attendance/AllAttendance.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../utils/Redux/Store/Store'
+import { GetAllAttendance } from '../../functions/Attendance/AllAttendance'
+import { AttendanceRecord } from '../../utils/AttendanceInterface'
+import AttendanceTable from './AttendanceTable'
+const AllAttendance: React.FC = () => {
+  const user = useSelector((state: RootState) => state.user)
+  const [allAttendance, setAttendance] = useState<AttendanceRecord[]>([])
+  // Fetch attendance data
+  const getAttendance = async () => {
+    try {
+      const data = await GetAllAttendance(user.Email)
+      if (data) {
+        setAttendance(data)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(() => {
+    if (user.Email) {
+      getAttendance()
+    }
+  }, [user.Email])
+  if (!user.Email) {
+    return (
+      <div className="container mx-auto px-4 py-6">
+        Please log in to view attendance records.
+      </div>
+    )
+  }
+  return <AttendanceTable Attendance={allAttendance} />
+}
+export default AllAttendance
