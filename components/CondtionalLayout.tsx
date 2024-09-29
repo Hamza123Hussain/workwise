fa9172146e7b6e97408d234dcc8c8c999a617b20
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-30 01:32:15 +0500

    fixed condtional layout once and for all

--- components/CondtionalLayout.tsx
+++ components/CondtionalLayout.tsx
@@ -1,16 +1,15 @@
 'use client'
 import { RootState } from '@/utils/Redux/Store/Store'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { GetUserData } from '@/utils/Redux/Slice/User/UserSlice'
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import { decryptData } from '@/utils/Encryprion'
-import Sidebar from './SideBar'
+
+import SignIn from './Auth/SignIn'
 const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
+  const User = useSelector((state: RootState) => state.user)
   const dispatch = useDispatch()
-  // const Pathname = usePathname()
-  // // const Router = useRouter()
-  // const AuthPages = ['/signin', '/signup', '/reset']
   useEffect(() => {
     const encryptedData = localStorage.getItem('UserData')
     if (encryptedData) {
@@ -18,27 +17,32 @@ const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
       dispatch(GetUserData(decryptedData))
     }
   }, [dispatch])
-  // const User = useSelector((state: RootState) => state.user)
-  // // const isAuthenticated = !!User.Email
-  // // useEffect(() => {
-  // //   if (isAuthenticated && AuthPages.includes(Pathname)) {
-  // //     Router.push('/') // Redirect to the home page if logged in and on an auth page
-  // //   } else if (!isAuthenticated && !AuthPages.includes(Pathname)) {
-  // //     Router.push('/signin') // Redirect to sign-in page if not logged in and trying to access a protected page
-  // //   }
-  // // }, [isAuthenticated, Pathname, Router])
+  const [isClient, setIsClient] = useState(false)
+  const pathname = usePathname()
 
-  // return AuthPages.includes(Pathname) ? (
-  //   <div className=" min-h-screen  items-center justify-center flex">
-  //     {children}
-  //   </div>
-  // ) : (
-  //   <div className="  flex">
-  //     <Sidebar />
-  //     <div className=" flex-1 ">{children}</div>
-  //   </div>
-  // )
-  return <div>{children}</div>
+  // Use useEffect to only set isClient on the client-side
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  // Check if the current route is login or signup
+  const isAuthPage =
+    pathname === '/login' ||
+    pathname === '/Register' ||
+    pathname === '/forgotpass'
+
+  // Render the layout based on client-side state
+  if (!isClient) {
+    return null // or a loading spinner if preferred
+  }
+
+  return User.Email ? (
+    <div>{children}</div>
+  ) : isAuthPage ? (
+    <div>{children}</div>
+  ) : (
+    <SignIn />
+  )
 }
 
 export default ConditionalLayout
