2d4737fcf7b3dd6106d7c9f3382b15db31e49791
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-29 19:23:58 +0500

    fixed the issue of losing user data

--- components/CondtionalLayout.tsx
+++ components/CondtionalLayout.tsx
@@ -1,10 +1,18 @@
 'use client'
 import { RootState } from '@/utils/Redux/Store/Store'
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import SignIn from './Auth/SignIn'
+import { GetUserData } from '@/utils/Redux/Slice/User/UserSlice'
 
 const CondtionalLayout = ({ children }: { children: React.ReactNode }) => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const userData = localStorage.getItem('UserData')
+    if (userData) {
+      dispatch(GetUserData(JSON.parse(userData)))
+    }
+  }, [])
   const User = useSelector((state: RootState) => state.user)
   console.log('user details', User)
   return User.Email ? <div>{children}</div> : <SignIn />
