fb7dee50cbf91628900b664f139a8eb98743dea0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-29 20:40:58 +0500

    this has to be fixed

--- components/CondtionalLayout.tsx
+++ components/CondtionalLayout.tsx
@@ -8,9 +8,9 @@ import { decryptData } from '@/utils/Encryprion'
 import Sidebar from './SideBar'
 const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch()
-  const Pathname = usePathname()
-  const Router = useRouter()
-  const AuthPages = ['/signin', '/signup', '/reset']
+  // const Pathname = usePathname()
+  // // const Router = useRouter()
+  // const AuthPages = ['/signin', '/signup', '/reset']
   useEffect(() => {
     const encryptedData = localStorage.getItem('UserData')
     if (encryptedData) {
@@ -18,26 +18,27 @@ const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
       dispatch(GetUserData(decryptedData))
     }
   }, [dispatch])
-  const User = useSelector((state: RootState) => state.user)
-  const isAuthenticated = !!User.Email
-  useEffect(() => {
-    if (isAuthenticated && AuthPages.includes(Pathname)) {
-      Router.push('/') // Redirect to the home page if logged in and on an auth page
-    } else if (!isAuthenticated && !AuthPages.includes(Pathname)) {
-      Router.push('/signin') // Redirect to sign-in page if not logged in and trying to access a protected page
-    }
-  }, [isAuthenticated, Pathname, Router])
+  // const User = useSelector((state: RootState) => state.user)
+  // // const isAuthenticated = !!User.Email
+  // // useEffect(() => {
+  // //   if (isAuthenticated && AuthPages.includes(Pathname)) {
+  // //     Router.push('/') // Redirect to the home page if logged in and on an auth page
+  // //   } else if (!isAuthenticated && !AuthPages.includes(Pathname)) {
+  // //     Router.push('/signin') // Redirect to sign-in page if not logged in and trying to access a protected page
+  // //   }
+  // // }, [isAuthenticated, Pathname, Router])
 
-  return AuthPages.includes(Pathname) ? (
-    <div className=" min-h-screen  items-center justify-center flex">
-      {children}
-    </div>
-  ) : (
-    <div className="  flex">
-      <Sidebar />
-      <div className=" flex-1 ">{children}</div>
-    </div>
-  )
+  // return AuthPages.includes(Pathname) ? (
+  //   <div className=" min-h-screen  items-center justify-center flex">
+  //     {children}
+  //   </div>
+  // ) : (
+  //   <div className="  flex">
+  //     <Sidebar />
+  //     <div className=" flex-1 ">{children}</div>
+  //   </div>
+  // )
+  return <div>{children}</div>
 }
 
 export default ConditionalLayout
