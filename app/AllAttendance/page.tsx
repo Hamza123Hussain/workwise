886ba2bb062776e6b6ca8337375774b953c0fe3a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-29 20:38:11 +0500

    showing attendance details

---
+++ app/AllAttendance/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import AllAttendance from '@/components/Attendance/AllAttendance'
+import React from 'react'
+
+const Attendance = () => {
+  return <AllAttendance />
+}
+
+export default Attendance
