a7b4c723020ed9d974859d4e1d2022ddc79a9266
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-30 15:32:42 +0500

    Added loader and it will occur when data will be fetched

--- components/Attendance/AllAttendance.tsx
+++ components/Attendance/AllAttendance.tsx
@@ -4,15 +4,18 @@ import { RootState } from '../../utils/Redux/Store/Store'
 import { GetAllAttendance } from '../../functions/Attendance/AllAttendance'
 import { AttendanceRecord } from '../../utils/AttendanceInterface'
 import AttendanceTable from './AttendanceTable'
+import Loader from '../Loader'
 const AllAttendance: React.FC = () => {
   const user = useSelector((state: RootState) => state.user)
   const [allAttendance, setAttendance] = useState<AttendanceRecord[]>([])
-  // Fetch attendance data
+  const [loading, setloading] = useState(false)
   const getAttendance = async () => {
+    setloading(true)
     try {
       const data = await GetAllAttendance(user.Email)
       if (data) {
         setAttendance(data)
+        setloading(false)
       }
     } catch (error) {
       console.error(error)
@@ -23,6 +26,14 @@ const AllAttendance: React.FC = () => {
       getAttendance()
     }
   }, [user.Email])
+  if (loading) {
+    return (
+      <div className=" min-h-screen flex justify-center items-center">
+        {' '}
+        <Loader />
+      </div>
+    )
+  }
 
   return <AttendanceTable Attendance={allAttendance} />
 }
