e7fb23fe44224a29876f7af8c11a7adb89480e1e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-30 14:24:43 +0500

    allowed firebase image domain

--- components/UserDetails.tsx
+++ components/UserDetails.tsx
@@ -1,6 +1,7 @@
 import { handleSignOut } from '@/functions/AUTH/SignOut'
 import { ClearUser } from '@/utils/Redux/Slice/User/UserSlice'
 import { RootState } from '@/utils/Redux/Store/Store'
+import Image from 'next/image'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 const UserDetails = () => {
@@ -13,6 +14,7 @@ const UserDetails = () => {
   }
   return (
     <div className=" flex flex-col items-center gap-2">
+      <Image src={user.imageUrl} width={20} height={20} alt="User Image" />
       <h1 className=" text-xl text-white">{user.Name}</h1>
       <button
         onClick={() => Signout()}
