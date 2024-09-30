dd6d4a77606b2034d25a224054dbcec44120def3
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-30 15:49:12 +0500

    this is to be fixed

--- components/Attendance/AttendanceTable.tsx
+++ components/Attendance/AttendanceTable.tsx
@@ -12,44 +12,48 @@ const AttendanceTable = ({
   const attendancePercentage = ((totalDaysWorked / 22) * 100).toFixed(2)
 
   return (
-    <div className="container mx-auto px-4 py-6">
-      <h2 className="text-2xl font-semibold mb-4 text-white">
+    <div className=" mx-auto px-4 py-6">
+      <h2 className="text-2xl font-semibold mb-4 text-white text-center">
         All Attendance Records
       </h2>
       <div className="overflow-x-auto flex flex-col">
-        <table className="min-w-full bg-blend-darken border-2 border-charcoal-gray shadow-md rounded-lg ">
+        <table className="min-w-full bg-blend-darken border-2 border-charcoal-gray shadow-md rounded-lg">
           <thead>
             <tr className="bg-blend-darken">
-              <th className="border border-purple-800 px-4 py-2">User Data</th>
-              <th className="border border-purple-800 px-4 py-2">Entry Time</th>
-              <th className="border border-purple-800 px-4 py-2">Exit Time</th>
-              <th className="border border-purple-800 px-4 py-2">Duration</th>
-              <th className="border border-purple-800 px-4 py-2">
+              <th className="border border-purple-800 px-4 py-2 text-left">
+                User Data
+              </th>
+              <th className="border border-purple-800 px-4 py-2 text-left">
+                Entry Time
+              </th>
+              <th className="border border-purple-800 px-4 py-2 text-left">
+                Exit Time
+              </th>
+              <th className="border border-purple-800 px-4 py-2 text-left">
+                Duration
+              </th>
+              <th className="border border-purple-800 px-4 py-2 text-left">
                 Remaining Time
               </th>
-              <th className="border border-purple-800 px-4 py-2">
+              <th className="border border-purple-800 px-4 py-2 text-left">
                 Current Date
               </th>
             </tr>
           </thead>
           <tbody>
-            {Attendance.map((element) => {
-              return <AttendanceCard key={element.id} element={element} />
-            })}
+            {Attendance.map((element) => (
+              <AttendanceCard key={element.id} element={element} />
+            ))}
           </tbody>
         </table>
-        <div className="flex gap-5 items-center text-white my-5 ">
-          <div className="bg-purpleGradientStart p-2 rounded-lg shadow-md transition-transform duration-200 hover:scale-105">
-            <h1 className="text-lg font-bold">
-              Number Of Days Worked:{' '}
-              <span className="text-2xl">{totalDaysWorked}</span>
-            </h1>
+        <div className="flex flex-col sm:flex-row justify-between gap-4 items-center text-white my-5 space-y-4 sm:space-y-0">
+          <div className="bg-purpleGradientStart p-4 flex flex-col items-center text-center rounded-lg shadow-md transition-transform duration-200 hover:scale-105 w-full sm:w-1/2">
+            <h1 className="text-lg font-bold">Number Of Days Worked</h1>
+            <span className="text-2xl">{totalDaysWorked}</span>
           </div>
-          <div className="bg-purpleGradientStart p-2 rounded-lg shadow-md transition-transform duration-200 hover:scale-105">
-            <h1 className="text-lg font-bold">
-              Attendance Percentage:{' '}
-              <span className="text-2xl">{attendancePercentage}%</span>
-            </h1>
+          <div className="bg-purpleGradientStart p-4 flex flex-col items-center text-center rounded-lg shadow-md transition-transform duration-200 hover:scale-105 w-full sm:w-1/2">
+            <h1 className="text-lg font-bold">Attendance Percentage</h1>
+            <span className="text-2xl">{attendancePercentage}%</span>
           </div>
         </div>
       </div>
