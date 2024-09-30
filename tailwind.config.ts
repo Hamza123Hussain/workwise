038d5a325ee8d86607668d7871bda27c3711a540
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-30 14:59:29 +0500

    new Colors Added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+
+        purpleGradientStart: '#7B38F6',
+        pinkGradientEnd: '#D13CE8',
+      },
+      backgroundImage: {
+        'logo-gradient': 'linear-gradient(90deg, #7B38F6, #D13CE8)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
