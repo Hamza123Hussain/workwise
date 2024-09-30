d4b72f5ddd3237b446954ae28f97beb86f837ee3
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-30 16:33:43 +0500

    admin access given

--- components/Home/SideBar/SidebarLinks.tsx
+++ components/Home/SideBar/SidebarLinks.tsx
@@ -1,6 +1,9 @@
+import { RootState } from '@/utils/Redux/Store/Store'
 import Link from 'next/link'
+import { useSelector } from 'react-redux'
 
 const SideBarLinks = () => {
+  const User = useSelector((state: RootState) => state.user)
   return (
     <>
       <Link
@@ -9,12 +12,16 @@ const SideBarLinks = () => {
       >
         Home
       </Link>
-      <Link
-        href="/CreLinkteTLinksk"
-        className="text-white hover:text-[#FF5733]  hover:rounded-lg hover:px-2"
-      >
-        Create Task
-      </Link>
+      {User.Email == '70110719@student.uol.edu.pk' ? (
+        <Link
+          href="/CreLinkteTLinksk"
+          className="text-white hover:text-[#FF5733]  hover:rounded-lg hover:px-2"
+        >
+          Create Task
+        </Link>
+      ) : (
+        ''
+      )}
       <Link
         href="#"
         className="text-white hover:text-[#FF5733]  hover:rounded-lg hover:px-2"
@@ -22,23 +29,31 @@ const SideBarLinks = () => {
         User Tasks
       </Link>
       <Link
-        href="#"
+        href="/userattendance"
         className="text-white hover:text-[#FF5733]  hover:rounded-lg hover:px-2"
       >
         User Attendance
       </Link>
-      <Link
-        href={'/AllAttendance'}
-        className="text-white hover:text-[#FF5733]  hover:rounded-lg hover:px-2"
-      >
-        Total Attendance
-      </Link>
-      <Link
-        href="#"
-        className="text-white hover:text-[#FF5733]  hover:rounded-lg hover:px-2"
-      >
-        Report
-      </Link>
+      {User.Email == '70110719@student.uol.edu.pk' ? (
+        <Link
+          href={'/AllAttendance'}
+          className="text-white hover:text-[#FF5733]  hover:rounded-lg hover:px-2"
+        >
+          Total Attendance
+        </Link>
+      ) : (
+        ''
+      )}
+      {User.Email == '70110719@student.uol.edu.pk' ? (
+        <Link
+          href="#"
+          className="text-white hover:text-[#FF5733]  hover:rounded-lg hover:px-2"
+        >
+          Report
+        </Link>
+      ) : (
+        ''
+      )}
       <Link
         href="#"
         className="text-white hover:text-[#FF5733]  hover:rounded-lg hover:px-2"
