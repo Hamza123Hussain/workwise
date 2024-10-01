2f374e7c127613e76107ab00789d89139baa3549
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-01 17:41:08 +0500

    black and purple color scheme

--- components/Tasks/TaskForm.tsx
+++ components/Tasks/TaskForm.tsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TaskFormProps } from '../../utils/TaskformInterface'
+import { Allusers } from '@/functions/AUTH/Allusers'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/utils/Redux/Store/Store'
+import { UserFetched } from '@/functions/AUTH/SignUpInterface'
+
 const TaskForm: React.FC<TaskFormProps> = ({
   name,
   description,
@@ -12,77 +17,92 @@ const TaskForm: React.FC<TaskFormProps> = ({
   Priority,
   setPriority,
 }) => {
+  const [UserFetched, SetUserFetched] = useState<UserFetched[]>([])
+  const User = useSelector((state: RootState) => state.user)
+
+  const Getusers = async () => {
+    const Data = await Allusers(User.Email)
+    if (Data) {
+      SetUserFetched(Data)
+    }
+  }
+
+  useEffect(() => {
+    Getusers()
+  }, [])
+
   return (
     <div>
       <div className="mb-4">
-        <label className="block text-sm font-bold mb-2 text-gray-700">
+        <label className="block text-sm font-bold mb-2 text-purple-400">
           Task Name
         </label>
         <input
           type="text"
-          className="w-full p-3 rounded-lg shadow-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          className="w-full p-3 rounded-lg shadow-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
           value={name}
           onChange={(e) => setName(e.target.value)}
           required
         />
       </div>
       <div className="mb-4">
-        <label className="block text-sm font-bold mb-2 text-gray-700">
+        <label className="block text-sm font-bold mb-2 text-purple-400">
           Description
         </label>
         <textarea
-          className="w-full p-3 rounded-lg shadow-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          className="w-full p-3 rounded-lg shadow-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           required
         ></textarea>
       </div>
       <div className="mb-4">
-        <label className="block text-sm font-bold mb-2 text-gray-700">
+        <label className="block text-sm font-bold mb-2 text-purple-400">
           Due Date
         </label>
         <input
           type="date"
-          className="w-full p-3 rounded-lg shadow-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          className="w-full p-3 rounded-lg shadow-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
           value={dueDate}
           onChange={(e) => setDueDate(e.target.value)}
           required
         />
       </div>
       <div className="mb-4">
-        <label className="block text-sm font-bold mb-2 text-gray-700">
+        <label className="block text-sm font-bold mb-2 text-purple-400">
           Assigned To (User ID)
         </label>
         <select
-          className="w-full p-3 rounded-lg shadow-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          className="w-full p-3 rounded-lg shadow-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
           value={assignedTo}
           onChange={(e) => setAssignedTo(e.target.value)}
           required
         >
           <option value="">Select a user</option>
-          <option value="user1">User 1</option>
-          <option value="user2">User 2</option>
-          <option value="user3">User 3</option>
+          {UserFetched.map((element) => (
+            <option key={element.Email} value={element.Name}>
+              {element.Name}
+            </option>
+          ))}
         </select>
       </div>
       <div className="mb-4">
-        <label className="block text-sm font-bold mb-2 text-gray-700">
+        <label className="block text-sm font-bold mb-2 text-purple-400">
           Priority
         </label>
         <select
-          className="w-full p-3 rounded-lg shadow-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          className="w-full p-3 rounded-lg shadow-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
           value={Priority}
           onChange={(e) => setPriority(e.target.value)}
           required
         >
           <option value="">Select A Priority</option>
-          <option value="user1">LOW</option>
-          <option value="user2">MEDIUM</option>
-          <option value="user3">HIGH</option>
+          <option value="LOW">LOW</option>
+          <option value="MEDIUM">MEDIUM</option>
+          <option value="HIGH">HIGH</option>
         </select>
       </div>
     </div>
   )
 }
-
 export default TaskForm
