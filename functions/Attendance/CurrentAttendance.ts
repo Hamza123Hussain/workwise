9208cacd1f6d42d13361a0720acb74b7f10869c8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-01 15:12:55 +0500

    getting current attendance only

---
+++ functions/Attendance/CurrentAttendance.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+export const CurrentAttendance = async (Email: string) => {
+  const Response = await axios.get(
+    `https://workwise-backend-puce.vercel.app/Api/Attendance/CurrentAttendance?Email=${Email}`
+  )
+  try {
+    if (Response.status == 200) {
+      return Response.data
+    }
+  } catch (error) {
+    console.error('Error getting current attendance:', error)
+    throw error
+  }
+}
