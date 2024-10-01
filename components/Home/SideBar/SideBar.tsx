383fa83a848685c1c70ce3829d492998869a1c27
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-01 17:16:34 +0500

    removed empty spaces

--- components/Home/SideBar/SideBar.tsx
+++ components/Home/SideBar/SideBar.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-
 import UserDetails from '../UserDetails'
 import SideBarLinks from './SidebarLinks'
 import Image from 'next/image'
@@ -10,21 +9,17 @@ const Sidebar = () => {
   }
   return (
     <div className="flex ">
-      {/* Hamburger button for mobile */}
       <button
         className="md:hidden p-2 text-white bg-logo-gradient "
         onClick={toggleSidebar}
       >
         ☰
       </button>
-
-      {/* Sidebar */}
       <div
         className={`fixed inset-0  bg-purple-black  border-r-2 transition-transform transform md:relative md:flex md:flex-col min-h-screen p-5 w-64   ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         } md:translate-x-0 md:block`}
       >
-        {/* Logo and Title */}
         <div className="flex items-center">
           <Image
             width={150}
@@ -41,11 +36,9 @@ const Sidebar = () => {
             &times;
           </button>
         </div>
-        {/* Sidebar Links */}
         <div className="flex flex-col space-y-5 px-4 my-5 items-start ">
           <SideBarLinks />
         </div>
-        {/* User Details Section */}
         <div className="text-white p-4 md:mt-auto md:mb-28 ">
           <UserDetails />
         </div>
