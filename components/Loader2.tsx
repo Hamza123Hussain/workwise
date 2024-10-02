d239486125e9f3d9c39ddae3fe3c76727ff79057
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-02 17:15:55 +0500

    new loader for tasks

---
+++ components/Loader2.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Loader2 = () => {
+  return (
+    <div className="loader2">
+      <div className="cell d-0"></div>
+      <div className="cell d-1"></div>
+      <div className="cell d-2"></div>
+
+      <div className="cell d-1"></div>
+      <div className="cell d-2"></div>
+
+      <div className="cell d-2"></div>
+      <div className="cell d-3"></div>
+
+      <div className="cell d-3"></div>
+      <div className="cell d-4"></div>
+    </div>
+  )
+}
+
+export default Loader2
