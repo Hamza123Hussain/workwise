f477333ca4fa47f42f5a8ad5b08ed03e3344e7e7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-03 18:32:42 +0500

    Getting a single task

---
+++ functions/Task/GetSingleTask.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+export const fetchTask = async (email: string, taskId: string) => {
+  try {
+    const response = await axios.get(
+      `https://workwise-backend-puce.vercel.app/Api/Task/singleTask?`,
+      {
+        params: { Email: email, taskId }, // Use params to send query parameters
+      }
+    )
+    return response.data.task
+  } catch (err) {
+    // Use 'any' or a specific error type for TypeScript
+    console.error('Error fetching task:', err)
+  }
+}
