b971100a9b23d80064b9bbe7fac5670abeb43d28
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-03 12:22:29 +0500

    frontend function to update user in the backend

---
+++ functions/AUTH/UpdateUser.ts
@@ -0,0 +1,37 @@
+import { InputValues } from '@/utils/SignUpInterface'
+import axios from 'axios'
+
+export const updateUser = async (userData: InputValues): Promise<void> => {
+  try {
+    const formData = new FormData()
+
+    // Append the user data to the form
+    formData.append('Email', userData.email)
+    formData.append('JobTitle', userData.JobTitle)
+    formData.append('Name', userData.Name)
+
+    // Append the image if provided
+    if (userData.Image) {
+      formData.append('Image', userData.Image) // 'Image' is the field name for the image file
+    }
+
+    // Make the POST request to the backend API
+    const response = await axios.post(
+      'https://workwise-backend-puce.vercel.app/Api/Auth/UpdateUser',
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data', // Required for file uploads
+        },
+      }
+    )
+
+    if (response.status === 200) {
+      console.log('User updated successfully', response.data)
+    } else {
+      console.error('Failed to update user', response.data.message)
+    }
+  } catch (error) {
+    console.error('Error updating user:', error)
+  }
+}
