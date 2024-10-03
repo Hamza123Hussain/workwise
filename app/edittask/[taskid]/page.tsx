b368b588c8629f6e11e7a7d7bb77f770cb0db004
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-03 18:33:17 +0500

    in progress of completion

--- app/edittask/[taskid]/page.tsx
+++ app/edittask/[taskid]/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const TaskEdit = () => {
-  return <div></div>
+interface Params {
+  taskid: string
+}
+
+const TaskEdit = ({ params }: { params: Params }) => {
+  console.log('params are here', params.taskid)
+  return <div>{params.taskid}</div>
 }
 
 export default TaskEdit
