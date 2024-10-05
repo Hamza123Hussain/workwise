0f50511b51a742eb4d7bab7779a3b5db61024f2b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-05 14:24:21 +0500

    Gave Our Color Theme

--- components/Attendance/TableAttendanceRow.tsx
+++ components/Attendance/TableAttendanceRow.tsx
@@ -12,23 +12,32 @@ const AttendanceTableRow = ({
   const attendanceCount = countUniqueDates(records)
 
   return (
-    <table className=" w-full text-center my-5">
+    <table className="w-full text-center my-5">
       <thead>
-        <tr className="bg-gray-200">
-          <th className="border border-gray-300 px-6 py-4">User Name</th>
-          <th className="border border-gray-300 px-6 py-4">Attendance</th>
-          <th className="border border-gray-300 px-6 py-4">
+        <tr className="bg-purple-900">
+          <th className="border border-purple-600 px-6 py-4 text-white">
+            User Name
+          </th>
+          <th className="border border-purple-600 px-6 py-4 text-white">
+            Attendance
+          </th>
+          <th className="border border-purple-600 px-6 py-4 text-white">
             Attendance Percentage
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr key={userData} className="bg-white">
-          <td className="border border-gray-300 w-56">{userData}</td>
-          <td className="border border-gray-300 px-6 py-4">
+        <tr
+          key={userData}
+          className="bg-black text-white hover:bg-purple-700 transition duration-300"
+        >
+          <td className="border border-purple-600 w-56 px-6 py-4">
+            {userData}
+          </td>
+          <td className="border border-purple-600 px-6 py-4">
             {attendanceCount}
           </td>
-          <td className="border border-gray-300 px-6 py-4">
+          <td className="border border-purple-600 px-6 py-4">
             {((attendanceCount / 22) * 100).toFixed(2)}%
           </td>
         </tr>
--- components/Attendance/TableTaskRow.tsx
+++ components/Attendance/TableTaskRow.tsx
@@ -11,34 +11,57 @@ const TaskTableRow = ({
   const tasksCompleted = records.filter(
     (task) => task.progress === 'DONE'
   ).length
+  const HighPirority = records.filter((task) => task.priority === 'HIGH').length
+  const Medium_Pirotiy = records.filter(
+    (task) => task.priority === 'MEDIUM'
+  ).length
   return (
-    <table className=" w-full text-center my-5">
+    <table className="w-full text-center my-5">
       <thead>
-        <tr className="bg-gray-200">
-          <th className="border border-gray-300 px-4 py-2">User Name</th>
-          <th className="border border-gray-300 px-4 py-2">Task Assigned</th>
-          <th className="border border-gray-300 px-4 py-2">Task Completed</th>
-          <th className="border border-gray-300 px-4 py-2">Task Percentage</th>
+        <tr className="bg-purple-900">
+          <th className="border border-purple-600 text-xs w-56 text-white p-2">
+            User Name
+          </th>
+          <th className="border border-purple-600 text-xs text-white p-2">
+            High Priority
+          </th>
+          <th className="border border-purple-600 text-xs text-white p-2">
+            Medium Priority
+          </th>
+          <th className="border border-purple-600 text-xs text-white p-2">
+            Low Priority
+          </th>
+          <th className="border border-purple-600 text-xs text-white p-2">
+            Task Assigned
+          </th>
+          <th className="border border-purple-600 text-xs text-white p-2">
+            Task Completed
+          </th>
+          <th className="border border-purple-600 text-xs text-white p-2">
+            Task Percentage
+          </th>
         </tr>
       </thead>
-      <tbody className="min-w-full border border-gray-300">
-        <tr key={userData}>
-          <td className="border border-gray-300 px-4 py-2 w-56 text-white">
-            {userData}
+      <tbody className="min-w-full">
+        <tr
+          key={userData}
+          className="bg-black text-white hover:bg-purple-700 transition duration-300"
+        >
+          <td className="border border-purple-600 w-56 p-4">{userData}</td>
+          <td className="border border-purple-600 p-4">{HighPirority}</td>
+          <td className="border border-purple-600 p-4">{Medium_Pirotiy}</td>
+          <td className="border border-purple-600 p-4">
+            {records.length - (Medium_Pirotiy + HighPirority)}
           </td>
-          <td className="border border-gray-300 px-4 py-2 text-white">
-            {tasksAssigned}
-          </td>
-          <td className="border border-gray-300 px-4 py-2 text-white">
-            {tasksCompleted}
-          </td>
-          <td className="border border-gray-300 px-4 py-2 text-white">
+          <td className="border border-purple-600 p-4">{tasksAssigned}</td>
+          <td className="border border-purple-600 p-4">{tasksCompleted}</td>
+          <td className="border border-purple-600 p-4">
             {tasksAssigned > 0
               ? ((tasksCompleted / tasksAssigned) * 100).toFixed(2)
               : '0'}
             %
           </td>
-        </tr>{' '}
+        </tr>
       </tbody>
     </table>
   )
