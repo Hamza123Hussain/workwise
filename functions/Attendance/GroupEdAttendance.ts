37c3e2c2be0c5f8d4701a9d74e4106a1a132486d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-05 12:39:25 +0500

    made the function compatible for tasks fetch and attendance

--- functions/Attendance/GroupEdAttendance.ts
+++ functions/Attendance/GroupEdAttendance.ts
@@ -1,12 +1,24 @@
 import { AttendanceRecord } from '@/utils/AttendanceInterface'
+import { TaskFetch } from '@/utils/TaskformInterface'
 
-export const groupByUserData = (attendance: AttendanceRecord[]) => {
-  return attendance.reduce((acc, record) => {
-    const userData = record.UserData
-    if (!acc[userData]) {
-      acc[userData] = []
+export const groupByUserData = (Data: AttendanceRecord[] | TaskFetch[]) => {
+  return Data.reduce((acc, record) => {
+    if ('UserData' in record) {
+      // Handle AttendanceRecord
+      const userData = record.UserData
+      if (!acc[userData]) {
+        acc[userData] = []
+      }
+      acc[userData].push(record as AttendanceRecord)
+    } else if ('assignedTo' in record) {
+      // Handle TaskFetch
+      const assignedTo = record.assignedTo
+      if (!acc[assignedTo]) {
+        acc[assignedTo] = []
+      }
+      acc[assignedTo].push(record as TaskFetch)
     }
-    acc[userData].push(record)
     return acc
-  }, {} as { [key: string]: AttendanceRecord[] })
+  }, {} as { [key: string]: (AttendanceRecord | TaskFetch)[] })
 }
+// learn the as part
