563531289b0d587be78843f41a0354e9b445bd46
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-05 11:37:33 +0500

    showing all the details of the user tasks

--- components/Home/TaskStatus.tsx
+++ components/Home/TaskStatus.tsx
@@ -1,22 +1,55 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { TaskFetch } from '@/utils/TaskformInterface'
+import { RootState } from '@/utils/Redux/Store/Store'
+import { useSelector } from 'react-redux'
+import { GetAllTasks } from '@/functions/Task/AllTasks'
+import Loader from '../Loader'
 const TaskStatus = () => {
+  const [Loading, SetLoading] = useState(false)
+  const [allTasks, setTasks] = useState<TaskFetch[]>([])
+  const user = useSelector((state: RootState) => state.user)
+  const fetchData = async () => {
+    SetLoading(true)
+    try {
+      const data = await GetAllTasks(user.Email)
+      if (data) {
+        setTasks(data)
+        SetLoading(false)
+      }
+    } catch (error) {
+      SetLoading(false)
+      console.error('Error fetching tasks:', error)
+    }
+  }
+  useEffect(() => {
+    fetchData()
+  }, [user.Email])
+  const tasksDone = allTasks.filter((task) => task.progress === 'DONE')
   return (
-    <div className="bg-white flex-1 p-6 rounded-lg shadow-md">
-      <div className="mt-6">
-        <p>
-          <strong>Total Tasks Assigned:</strong>{' '}
-          <span className="font-bold">50</span>
-        </p>
-        <p>
-          <strong>Tasks Done:</strong> <span className="font-bold">30</span>
-        </p>
-        <p>
-          <strong>Tasks Left:</strong> <span className="font-bold">20</span>
-        </p>
-      </div>
+    <div className="bg-black border-2 border-purple-600 text-white flex-1 p-6 rounded-lg shadow-md">
+      {Loading ? (
+        <div className="flex justify-center items-center">
+          <Loader />
+        </div>
+      ) : (
+        <div className="mt-6">
+          <p>
+            <strong>Total Tasks Assigned:</strong>{' '}
+            <span className="font-bold">{allTasks.length}</span>
+          </p>
+          <p>
+            <strong>Tasks Done:</strong>{' '}
+            <span className="font-bold">{tasksDone.length}</span>
+          </p>
+          <p>
+            <strong>Tasks Left:</strong>{' '}
+            <span className="font-bold">
+              {allTasks.length - tasksDone.length}
+            </span>
+          </p>
+        </div>
+      )}
     </div>
   )
 }
-
 export default TaskStatus
