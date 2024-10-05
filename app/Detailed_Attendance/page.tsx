279aa565721d89ef6e47fd89997b5745242723c8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-05 11:36:07 +0500

    work to be done here

---
+++ app/Detailed_Attendance/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import AllAttendance from '@/components/Attendance/AllAttendance'
+import React from 'react'
+
+const DetailedAttendance = () => {
+  return <AllAttendance />
+}
+
+export default DetailedAttendance
