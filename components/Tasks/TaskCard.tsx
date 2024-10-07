31bac503c0fad6a4b424653c4c10c4ebfbb939fe
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-07 14:11:49 +0500

    Update TaskCard.tsx

--- components/Tasks/TaskCard.tsx
+++ components/Tasks/TaskCard.tsx
@@ -5,32 +5,22 @@ import TaskBody from './TaskBody'
 
 const TaskCard = ({ allTasks }: { allTasks: TaskFetch[] }) => {
   return (
-    <table className="min-w-full border-collapse border border-purple-500">
+    <table className="min-w-full border-collapse border border-black text-center">
       {allTasks.length > 0 ? (
         <>
           <thead>
             <tr className="bg-gradient-to-r from-purple-600 via-purple-500 to-purple-700">
-              <th className="border border-purple-500 p-2 text-left text-white">
-                Task
-              </th>
-              <th className="border border-purple-500 p-2 text-left text-white">
+              <th className="border border-black p-2  text-white">Task</th>
+              <th className="border border-black p-2  text-white">
                 Assigned To
               </th>
-              <th className="border border-purple-500 p-2 text-left text-white">
-                Status
-              </th>
-              <th className="border border-purple-500 p-2 text-left text-white">
+              <th className="border border-black p-2  text-white">Status</th>
+              <th className="border border-black p-2  text-white">
                 Description
               </th>
-              <th className="border border-purple-500 p-2 text-left text-white">
-                Due Date
-              </th>
-              <th className="border border-purple-500 p-2 text-left text-white">
-                Priority
-              </th>
-              <th className="border border-purple-500 p-2 text-left text-white">
-                Edit
-              </th>
+              <th className="border border-black p-2  text-white">Due Date</th>
+              <th className="border border-black p-2  text-white">Priority</th>
+              <th className="border border-black p-2  text-white">Edit</th>
             </tr>
           </thead>
           <tbody className="text-gray-300">
@@ -44,7 +34,7 @@ const TaskCard = ({ allTasks }: { allTasks: TaskFetch[] }) => {
           <tr>
             <td
               colSpan={7}
-              className="border border-purple-500 p-2 text-center text-gray-400"
+              className="border border-black p-2 text-center text-gray-400"
             >
               No tasks available.
             </td>
