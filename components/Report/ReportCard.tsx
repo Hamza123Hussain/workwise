b917d43fe76e02bc22d71269f506c6551cef8ac2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-09 12:09:13 +0500

    Update ReportCard.tsx

--- components/Report/ReportCard.tsx
+++ components/Report/ReportCard.tsx
@@ -1,74 +1,51 @@
-import { AttendanceRecord } from '@/utils/AttendanceInterface'
-import { TaskFetch } from '@/utils/TaskformInterface'
-import React from 'react'
-import ReportBody from './ReportBody' // Make sure to import ReportBody
-import Image from 'next/image'
-
-// Define interfaces for user data
-interface MergedUserData {
-  user: string
-  attendance: AttendanceRecord[]
-  tasks: TaskFetch[]
-}
-
-interface ReportCardProps {
-  mergedData: MergedUserData[] // Array of user data
-}
-
+import React, { useRef } from 'react'
+import ReportBody from './ReportBody'
+import ReportHead from './ReportHead'
+import DownloadButton from './DownloadButton'
+import { ReportCardProps } from '@/utils/Report_Interface'
 const ReportCard: React.FC<ReportCardProps> = ({ mergedData }) => {
+  const reportRef = useRef(null)
   return (
-    <div className="p-4  text-purple-200">
-      <div className=" flex flex-col justify-center items-center my-10">
-        {' '}
-        <Image
-          width={350}
-          height={350}
-          src="/Logo.png"
-          alt="Logo"
-          className=" cursor-pointer object-cover"
-        />{' '}
-        <h1 className="text-4xl">
-          Performance Report{' '}
-          {new Date().toLocaleString('default', { month: 'long' })}{' '}
-          {new Date().getFullYear()}
-        </h1>
+    <div className=" flex flex-col justify-center items-center">
+      <div
+        className="overflow-x-auto p-4 text-center w-[90vw] sm:w-auto"
+        ref={reportRef}
+      >
+        <ReportHead />
+        <table className=" border-collapse border border-purple-600 text-center text-white">
+          <thead>
+            <tr className="bg-purple-600 text-white">
+              <th className="border border-purple-500 p-2 text-[12px]">
+                Username
+              </th>
+              <th className="border border-purple-500 p-2 text-[12px]">
+                Attendance Percentage
+              </th>
+              <th className="border border-purple-500 p-2 text-[12px]">
+                High Priority Tasks
+              </th>
+              <th className="border border-purple-500 p-2 text-[12px]">
+                Medium Priority Tasks
+              </th>
+              <th className="border border-purple-500 p-2 text-[12px]">
+                Low Priority Tasks
+              </th>
+              <th className="border border-purple-500 p-2 text-[12px]">
+                Task Completion Percentage
+              </th>
+              <th className="border border-purple-500 p-2 text-[12px]">
+                Task & Attendance Percentage
+              </th>
+              <th className="border border-purple-500 p-2 text-[12px]">
+                Salary
+              </th>
+            </tr>
+          </thead>
+          <ReportBody mergedData={mergedData} />
+        </table>
       </div>
-      {/* Background black with purple text */}
-      {/* Table with summary */}
-      <table className="min-w-full border-collapse border border-purple-600 text-center">
-        <thead>
-          <tr className="bg-purple-600 text-white">
-            <th className="border border-purple-500  p-2 text-[12px]">
-              Username
-            </th>
-            <th className="border border-purple-500  p-2 text-[12px]">
-              Attendance Percentage
-            </th>
-            <th className="border border-purple-500  p-2 text-[12px]">
-              High Priority Tasks
-            </th>
-            <th className="border border-purple-500  p-2 text-[12px]">
-              Medium Priority Tasks
-            </th>
-            <th className="border border-purple-500  p-2 text-[12px]">
-              Low Priority Tasks
-            </th>
-            <th className="border border-purple-500  p-2 text-[12px]">
-              Task Completion Percentage
-            </th>
-            <th className="border border-purple-500  p-2 text-[12px]">
-              Task & Attendance Percentage
-            </th>
-            <th className="border border-purple-500  p-2 text-[12px]">
-              Salary
-            </th>
-          </tr>
-        </thead>
-        <ReportBody mergedData={mergedData} />{' '}
-        {/* Pass mergedData to ReportBody */}
-      </table>
+      <DownloadButton reportRef={reportRef} />
     </div>
   )
 }
-
 export default ReportCard
