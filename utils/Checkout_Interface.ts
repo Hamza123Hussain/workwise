2d83e337fb75d314fdea5e9704f37b7c1df9dc4f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-09 16:35:10 +0500

    data types needed for checkin

---
+++ utils/Checkout_Interface.ts
@@ -0,0 +1,8 @@
+export interface CheckInProps {
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  currentTime: Date
+  checkinStatus: boolean
+  attendanceId: string | null
+  setAttendanceId: (id: string | null) => void
+  setCheckinStatus: (status: boolean) => void
+}
