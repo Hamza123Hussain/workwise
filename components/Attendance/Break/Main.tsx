9a3aef9b696b7d7198482657e12117bdc20b4375
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-10 13:13:59 +0500

    most of it has been build

--- components/Attendance/Break/Main.tsx
+++ components/Attendance/Break/Main.tsx
@@ -1,24 +1,41 @@
 import { handleBreakStartEnd } from '@/functions/Attendance/Checkout/BreakFunction'
+import { RootState } from '@/utils/Redux/Store/Store'
 import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+import { useSelector } from 'react-redux'
 
-const BreakMain: React.FC = () => {
+const BreakMain = ({ attendanceId }: { attendanceId: string | null }) => {
   const [onBreak, setOnBreak] = useState(false) // Break status
   const [breakStartTime, setBreakStartTime] = useState<Date | null>(null) // When break started
   const [breakDuration, setBreakDuration] = useState<number>(0) // Total break duration in seconds
 
-  const handleBreak = () => {
-    handleBreakStartEnd(
-      onBreak,
-      breakStartTime,
-      setBreakStartTime,
-      setOnBreak,
-      breakDuration,
-      setBreakDuration
-    )
+  const user = useSelector((state: RootState) => state.user) // Get user from redux store
+
+  const handleBreak = async () => {
+    if (!attendanceId) {
+      toast.error('Attendance ID is missing') // Handle missing attendanceId
+      return
+    }
+
+    try {
+      await handleBreakStartEnd(
+        user.Email,
+        onBreak,
+        breakStartTime,
+        setBreakStartTime,
+        setOnBreak,
+        breakDuration,
+        setBreakDuration,
+        attendanceId
+      )
+    } catch (error) {
+      toast.error('An error occurred during break handling') // Error handling
+      console.error('Break handling error:', error)
+    }
   }
 
   return (
-    <>
+    <div className="text-center">
       <button
         onClick={handleBreak}
         className={`${
@@ -29,9 +46,11 @@ const BreakMain: React.FC = () => {
       >
         {onBreak ? 'End Break' : 'Start Break'}
       </button>
-      {onBreak && <p className="text-yellow-400 mt-2">On Break</p>}
-      <p>Total Break Duration: {Math.floor(breakDuration)} seconds</p>
-    </>
+      {onBreak && <p className="text-white mt-2">On Break</p>}
+      <p className="text-yellow-50 mt-2">
+        Total Break Duration: {Math.floor(breakDuration)} seconds
+      </p>
+    </div>
   )
 }
 
