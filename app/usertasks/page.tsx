35363acd9b2354c9f5fa62a778695a6a78cfe954
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-12 13:11:18 +0500

    Update page.tsx

--- app/usertasks/page.tsx
+++ app/usertasks/page.tsx
@@ -6,18 +6,16 @@ import { RootState } from '@/utils/Redux/Store/Store'
 import { TaskFetch } from '@/utils/TaskformInterface'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-
 const UserTasks = () => {
   const [Loading, SetLoading] = useState(false)
   const [allTasks, setTasks] = useState<TaskFetch[]>([])
   const User = useSelector((state: RootState) => state.user)
-
-  const fetchData = async () => {
+  const [selectedUser, setSelectedUser] = useState<string>(User.Name) // Set default to first dummy user
+  const fetchData = async (userName: string) => {
     SetLoading(true)
     try {
-      const data = await GetUserTasks(User.Name, User.Email)
+      const data = await GetUserTasks(userName, User.Email)
       if (data) {
-        // Sort tasks in descending order based on createdAt
         const sortedTasks = data.sort(
           (a: TaskFetch, b: TaskFetch) =>
             new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
@@ -30,11 +28,9 @@ const UserTasks = () => {
       SetLoading(false)
     }
   }
-
   useEffect(() => {
-    fetchData()
-  }, [User.Email])
-
+    fetchData(User.Email === 'octtoppus1@gmail.com' ? selectedUser : User.Name) // Fetch tasks based on condition
+  }, [selectedUser, User.Email])
   if (Loading) {
     return (
       <div className="flex items-center justify-center min-h-screen">
@@ -42,12 +38,13 @@ const UserTasks = () => {
       </div>
     )
   }
-
   return (
     <div className="p-6 rounded-lg shadow-md">
       <h2 className="font-semibold text-2xl text-purple-500">
-        Tasks of {User.Name}
+        Tasks of{' '}
+        {User.Email === 'octtoppus1@gmail.com' ? selectedUser : User.Name}
       </h2>
+      {/* ADMIN ACESS HERE */}
       <div className="my-5 grid grid-cols-1 lg:grid-cols-2 mx-auto">
         {allTasks.length > 0 ? (
           allTasks.map((task) => (
@@ -60,5 +57,4 @@ const UserTasks = () => {
     </div>
   )
 }
-
 export default UserTasks
