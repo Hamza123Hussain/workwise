f3dd5977d2d2687268480f42550e811b81112633
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-12 16:50:49 +0500

    showing details of new attribute

--- components/Attendance/AttendanceCard.tsx
+++ components/Attendance/AttendanceCard.tsx
@@ -1,18 +1,6 @@
-import {
-  calculateRemainingTime,
-  formatDuration,
-} from '../../functions/Attendance/TimeFunctions'
+import { convertHoursToString } from '@/functions/Attendance/TimeFunctions'
 import { AttendanceRecord } from '../../utils/AttendanceInterface'
 const AttendanceCard = ({ element }: { element: AttendanceRecord }) => {
-  let duration = '-'
-  let remainingTime = '-'
-  if (element.entry && element.exit) {
-    const entryTime = new Date(element.entry).getTime()
-    const exitTime = new Date(element.exit).getTime()
-    const timeDifference = exitTime - entryTime
-    duration = formatDuration(timeDifference)
-    remainingTime = calculateRemainingTime(timeDifference)
-  }
   return (
     <tr key={element.id} className="border-b hover:bg-purple-80">
       <td className="border border-purple-800 px-4 py-2 text-[8px] text-white  sm:text-base md:text-lg">
@@ -25,12 +13,15 @@ const AttendanceCard = ({ element }: { element: AttendanceRecord }) => {
         {element.exit ? new Date(element.exit).toLocaleTimeString() : '-'}
       </td>
       <td className="border border-purple-800 px-4 py-2 text-[8px] text-white  sm:text-base md:text-lg">
-        {duration}
+        {element.Hours_Worked ? convertHoursToString(element.Hours_Worked) : ''}
       </td>
       <td className="border border-purple-800 px-4 py-2 text-[8px] text-white  sm:text-base md:text-lg">
-        {remainingTime}
+        {element.Hours_Worked
+          ? element.Hours_Worked > 8
+            ? '0HR 0MINS 0SECS'
+            : (8 - element.Hours_Worked).toFixed(2)
+          : ''}
       </td>
-
       <td className="border border-purple-800 px-4 py-2 text-[8px] text-white  sm:text-base md:text-lg">
         {element.currentDate
           ? new Date(element.currentDate).toLocaleDateString()
