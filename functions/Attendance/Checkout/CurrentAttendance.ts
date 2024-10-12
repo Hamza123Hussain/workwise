7c22a4d4805fd1d005bbc408321e098bb353f0fd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-12 17:26:05 +0500

    fixed spaces

--- functions/Attendance/Checkout/CurrentAttendance.ts
+++ functions/Attendance/Checkout/CurrentAttendance.ts
@@ -5,7 +5,8 @@ export const GetCurrentAttendance = async (
   email: string,
   setLoading: (loading: boolean) => void,
   setAttendanceId: (id: string | null) => void,
-  setCheckinStatus: (status: boolean) => void
+  setCheckinStatus: (status: boolean) => void,
+  setOnBreak: (status: boolean) => void
 ) => {
   setLoading(true)
   try {
@@ -13,14 +14,17 @@ export const GetCurrentAttendance = async (
     if (Data) {
       setAttendanceId(Data[0]._id)
       setCheckinStatus(Data[0].CheckInStatus)
+      setOnBreak(Data[0].onBreak)
     } else {
       setAttendanceId(null)
       setCheckinStatus(false)
+      setOnBreak(false)
     }
   } catch (error) {
     if (axios.isAxiosError(error) && error.response?.status === 404) {
       setAttendanceId(null)
       setCheckinStatus(false)
+      setOnBreak(false)
     } else {
       console.error('Error getting current attendance:', error)
     }
