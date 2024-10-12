7fc4b3aac5f544fccaa8f17c1fe19fa5c21d1f04
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-12 13:34:05 +0500

    component completed and working

--- app/usertasks/page.tsx
+++ app/usertasks/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import UserSelection from '@/components/Layout/UserSelection'
 import Loader from '@/components/Loader'
 import TaskCard2 from '@/components/Tasks/TaskCard2'
 import { GetUserTasks } from '@/functions/Task/GetUserTasks'
@@ -6,11 +7,13 @@ import { RootState } from '@/utils/Redux/Store/Store'
 import { TaskFetch } from '@/utils/TaskformInterface'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+
 const UserTasks = () => {
   const [Loading, SetLoading] = useState(false)
   const [allTasks, setTasks] = useState<TaskFetch[]>([])
   const User = useSelector((state: RootState) => state.user)
-  const [selectedUser, setSelectedUser] = useState<string>(User.Name) // Set default to first dummy user
+  const SelectedUser = useSelector((state: RootState) => state.Select)
+
   const fetchData = async (userName: string) => {
     SetLoading(true)
     try {
@@ -28,9 +31,11 @@ const UserTasks = () => {
       SetLoading(false)
     }
   }
+
   useEffect(() => {
-    fetchData(User.Email === 'octtoppus1@gmail.com' ? selectedUser : User.Name) // Fetch tasks based on condition
-  }, [selectedUser, User.Email])
+    fetchData(User.Email === 'octtoppus1@gmail.com' ? SelectedUser : User.Name) // Fetch tasks based on condition
+  }, [SelectedUser, User.Email])
+
   if (Loading) {
     return (
       <div className="flex items-center justify-center min-h-screen">
@@ -38,14 +43,15 @@ const UserTasks = () => {
       </div>
     )
   }
+
   return (
-    <div className="p-6 rounded-lg shadow-md">
-      <h2 className="font-semibold text-2xl text-purple-500">
+    <div className="p-6 rounded-lg shadow-md my-10">
+      <h2 className="font-semibold text-2xl text-purple-500 mb-4">
         Tasks of{' '}
-        {User.Email === 'octtoppus1@gmail.com' ? selectedUser : User.Name}
+        {User.Email === 'octtoppus1@gmail.com' ? SelectedUser : User.Name}
       </h2>
-      {/* ADMIN ACESS HERE */}
-      <div className="my-5 grid grid-cols-1 lg:grid-cols-2 mx-auto">
+      <UserSelection />
+      <div className="grid grid-cols-1 lg:grid-cols-2  gap-6">
         {allTasks.length > 0 ? (
           allTasks.map((task) => (
             <TaskCard2 key={task.createdAt} TaskDetail={task} />
@@ -57,4 +63,5 @@ const UserTasks = () => {
     </div>
   )
 }
+
 export default UserTasks
--- components/Layout/UserSelection.tsx
+++ components/Layout/UserSelection.tsx
@@ -1,37 +1,49 @@
+import { Allusers } from '@/functions/AUTH/Allusers'
 import { GetUserName } from '@/utils/Redux/Slice/SelectUser/SelectSlice'
 import { RootState } from '@/utils/Redux/Store/Store'
-import React from 'react'
+import { UserFetched } from '@/utils/SignUpInterface'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 const UserSelection = () => {
-    const Dispatch=useDispatch()
-    const SelectedUser=useSelector((state:RootState)=>state.Select)
-    const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-      Dispatch(GetUserName(event.target.value)) // Update selected user when dropdown changes
+  const [Users, SetUserFetched] = useState<UserFetched[]>([])
+  const User = useSelector((state: RootState) => state.user)
+  const Dispatch = useDispatch()
+  const SelectedUser = useSelector((state: RootState) => state.Select)
+  const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    Dispatch(GetUserName(event.target.value)) // Update selected user when dropdown changes
+  }
+  const Getusers = async () => {
+    const Data = await Allusers(User.Email)
+    if (Data) {
+      SetUserFetched(Data)
     }
-  
+  }
+  useEffect(() => {
+    Getusers()
+  }, [])
   return (
-
-    {User.Email === 'octtoppus1@gmail.com' && (
+    <>
+      {User.Email === 'octtoppus1@gmail.com' && (
         <div className="my-4">
           {/* Dropdown for selecting a user if email is octtoppus1@gmail.com */}
-          <label htmlFor="user-select" className="mr-3 font-medium">
+          <label htmlFor="user-select" className="mr-3 font-medium text-white">
             Select User:
           </label>
           <select
             id="user-select"
-            value={selectedUser}
+            value={SelectedUser}
             onChange={handleUserChange}
             className="p-2 border border-gray-300 rounded-md"
           >
-            {dummyUsers.map((user) => (
-              <option key={user} value={user}>
-                {user}
+            {Users.map((user) => (
+              <option key={user.createdAt} value={user.Name}>
+                {user.Name}
               </option>
             ))}
           </select>
         </div>
       )}
+    </>
   )
 }
-
 export default UserSelection
