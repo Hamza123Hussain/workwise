4a69dc0e50303e0cf79c9f00e4d9cef4dce7f116
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-12 13:41:36 +0500

    minor loading fix is to be done

--- components/Layout/UserSelection.tsx
+++ components/Layout/UserSelection.tsx
@@ -4,10 +4,11 @@ import { RootState } from '@/utils/Redux/Store/Store'
 import { UserFetched } from '@/utils/SignUpInterface'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch } from '@/utils/Redux/Store/Store'
 const UserSelection = () => {
   const [Users, SetUserFetched] = useState<UserFetched[]>([])
   const User = useSelector((state: RootState) => state.user)
-  const Dispatch = useDispatch()
+  const Dispatch = useDispatch<AppDispatch>()
   const SelectedUser = useSelector((state: RootState) => state.Select)
   const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     Dispatch(GetUserName(event.target.value)) // Update selected user when dropdown changes
@@ -23,18 +24,22 @@ const UserSelection = () => {
   }, [])
   return (
     <>
-      {User.Email === 'octtoppus1@gmail.com' && (
+      {User.Email === 'octtoppus1@gmail.com' && SelectedUser && (
         <div className="my-4">
           {/* Dropdown for selecting a user if email is octtoppus1@gmail.com */}
-          <label htmlFor="user-select" className="mr-3 font-medium text-white">
+          {/* <label
+            htmlFor="user-select"
+            className="mr-3 font-medium text-purple-400"
+          >
             Select User:
-          </label>
+          </label> */}
           <select
             id="user-select"
             value={SelectedUser}
             onChange={handleUserChange}
-            className="p-2 border border-gray-300 rounded-md"
+            className="p-2 bg-black border border-purple-500 text-purple-400 rounded-md"
           >
+            <option value={User.Name}>Select A User Name</option>
             {Users.map((user) => (
               <option key={user.createdAt} value={user.Name}>
                 {user.Name}
