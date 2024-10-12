9186705d181bacd53a8146f1b71ca1ac42cfc182
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-12 16:50:07 +0500

    getting a string from hrs

--- functions/Attendance/TimeFunctions.ts
+++ functions/Attendance/TimeFunctions.ts
@@ -1,25 +1,12 @@
-// Function to format duration into HH:mm:ss
-export const formatDuration = (timeDifference: number) => {
-  const hours = Math.floor(timeDifference / (1000 * 60 * 60))
-  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
-  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+export function convertHoursToString(hoursWorked: number) {
+  // Calculate total seconds
+  const totalSeconds = hoursWorked * 3600
 
-  return `${hours} hours ${minutes} mins ${seconds} secs`
-}
+  // Calculate hours, minutes, and seconds
+  const hours = Math.floor(totalSeconds / 3600)
+  const minutes = Math.floor((totalSeconds % 3600) / 60)
+  const seconds = Math.floor(totalSeconds % 60)
 
-export const calculateRemainingTime = (timeDifference: number) => {
-  const targetHours = 8 // Assuming an 8-hour workday in hours
-  const targetMilliseconds = targetHours * 60 * 60 * 1000 // Convert target hours to milliseconds
-
-  const remainingMilliseconds = Math.max(targetMilliseconds - timeDifference, 0)
-
-  const remainingHours = Math.floor(remainingMilliseconds / (1000 * 60 * 60))
-  const remainingMinutes = Math.floor(
-    (remainingMilliseconds % (1000 * 60 * 60)) / (1000 * 60)
-  )
-  const remainingSeconds = Math.floor(
-    (remainingMilliseconds % (1000 * 60)) / 1000
-  )
-
-  return `${remainingHours} hrs ${remainingMinutes} mins ${remainingSeconds} secs`
+  // Return the formatted string
+  return `${hours} hours, ${minutes} minutes, ${seconds} seconds`
 }
