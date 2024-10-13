1a32aa1663f692157db54f25ebcfe45c27159971
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-13 20:28:53 +0500

    added comments to understand the code

--- components/Tasks/RecentTasks.tsx
+++ components/Tasks/RecentTasks.tsx
@@ -10,18 +10,30 @@ const RecentTasks = () => {
   const [allTasks, setTasks] = useState<TaskFetch[]>([])
   const user = useSelector((state: RootState) => state.user)
   const fetchData = async () => {
+    // Set loading state to true to indicate data fetching is in progress
     SetLoading(true)
+
     try {
+      // Call an asynchronous function to get user tasks using 'user.Name' and 'user.Email'
       const data = await GetUserTasks(user.Name, user.Email)
+
+      // If the data is successfully retrieved (not null or undefined)
       if (data) {
+        // Set 'tasks' state with the last 3 items from the fetched data (most recent tasks)
         setTasks(data.slice(-3))
+
+        // Set loading state to false since the data fetching is complete
         SetLoading(false)
       }
     } catch (error) {
+      // If an error occurs during the data fetch, set loading state to false
       SetLoading(false)
+
+      // Log the error to the console for debugging purposes
       console.error('Error fetching tasks:', error)
     }
   }
+
   useEffect(() => {
     fetchData()
   }, [user.Email])
