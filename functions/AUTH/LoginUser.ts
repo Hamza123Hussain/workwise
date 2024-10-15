ec23d47778222775295583cf2d7ab9628d248e28
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-15 16:35:30 +0500

    Update LoginUser.ts

--- functions/AUTH/LoginUser.ts
+++ functions/AUTH/LoginUser.ts
@@ -1,6 +1,5 @@
 import { ApiUrl } from '@/utils/AttendanceInterface'
 import axios from 'axios'
-
 import toast from 'react-hot-toast'
 
 export const loginUser = async (email: string, password: string) => {
@@ -10,27 +9,15 @@ export const loginUser = async (email: string, password: string) => {
       Password: password,
     })
 
-    // Handle successful login
-    const userData = response.data
-    console.log('User logged in successfully:', userData)
-    // Optionally, redirect or set user state here
-
-    // Show success message
-    toast.success('Login successful!')
     return response.data
   } catch (error) {
-    // Show toast notification for login errors
-
-    // Display the error message returned from the server
-
-    toast.error('Incorrect Email or Password', {
-      // Customize toast style (optional)
-      style: {
-        borderRadius: '8px',
-        background: 'black',
-        color: 'white',
-      },
-    })
-    console.log(error)
+    // Handle error
+    if (error) {
+      console.error('Login failed:', error)
+      toast.error('Incorrect Email or Password')
+    } else {
+      console.error('An error occurred:', error)
+    }
+    throw error
   }
 }
