6d37c84a85fcb71a3f9371a0a39135e42f91ac26
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-15 15:38:51 +0500

    showing password error

--- functions/AUTH/LoginUser.ts
+++ functions/AUTH/LoginUser.ts
@@ -1,21 +1,32 @@
 import { ApiUrl } from '@/utils/AttendanceInterface'
-import axios from 'axios'
-
+import axios, { AxiosResponse } from 'axios'
+import toast from 'react-hot-toast'
 export const loginUser = async (email: string, password: string) => {
   try {
-    const response = await axios.post(`${ApiUrl}Api/Auth/SignIn`, {
-      Email: email,
-      Password: password,
-    })
-
-    return response.data
+    const response: AxiosResponse = await axios.post(
+      `${ApiUrl}Api/Auth/SignIn`,
+      {
+        Email: email,
+        Password: password,
+      }
+    )
+    return response.data // Return the expected response data
   } catch (error) {
     // Handle error
-    if (error) {
-      console.error('Login failed:', error)
+    if (axios.isAxiosError(error) && error.response) {
+      if (error.response.status === 401) {
+        // 401 status code for unauthorized, assuming it's incorrect credentials
+        toast.error('Incorrect password or email!')
+      } else {
+        // Handle other specific errors if necessary
+        toast.error('An error occurred while logging in.')
+      }
     } else {
-      console.error('An error occurred:', error)
+      // Handle non-Axios errors (network issues, etc.)
+      toast.error('An error occurred while logging in.')
     }
-    throw error
+
+    console.error('Login failed:', error)
+    throw error // Re-throw the error to allow the caller to handle it further
   }
 }
