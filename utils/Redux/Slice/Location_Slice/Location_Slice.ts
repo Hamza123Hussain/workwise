7d9d300b3fe7492ffa64b5217d42525041ca4cfc
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-15 14:08:43 +0500

    getting current location cordinates

---
+++ utils/Redux/Slice/Location_Slice/Location_Slice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  latitude: 0,
+  longitude: 0,
+}
+
+const Location_Slice = createSlice({
+  name: 'Location_Slice',
+  initialState,
+  reducers: {
+    New_Location_Cords: (state, action) => {
+      state.latitude = action.payload.latitude
+      state.longitude = action.payload.longitude
+    },
+  },
+})
+
+export const { New_Location_Cords } = Location_Slice.actions
+export default Location_Slice.reducer
