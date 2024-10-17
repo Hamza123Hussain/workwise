da67791a58bda923cdb4d2787cef76a1df845654
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-17 19:07:26 +0500

    Update ReportHead.tsx

--- components/Report/ReportHead.tsx
+++ components/Report/ReportHead.tsx
@@ -1,16 +1,15 @@
-import Image from 'next/image'
 import React from 'react'
 
 const ReportHead = () => {
   return (
     <div className="flex flex-col justify-center items-center my-10">
-      <Image
-        width={350}
-        height={350}
+      {/* <Image
+        width={200}
+        height={200}
         src="/Logo.png"
         alt="Logo"
-        className="cursor-pointer object-cover"
-      />
+        className="cursor-pointer object-cover mb-10"
+      /> */}
       <h1 className="text-4xl text-white">
         Performance Report{' '}
         {new Date().toLocaleString('default', { month: 'long' })}{' '}
