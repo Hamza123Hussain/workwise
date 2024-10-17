a5be58f958cfc9f19aca36fc32992ca58c5998d5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-17 17:09:28 +0500

    showing userdetail on the sidebar

--- components/Home/SideBar/SideBar.tsx
+++ components/Home/SideBar/SideBar.tsx
@@ -1,19 +1,15 @@
 import React, { useState } from 'react'
 import SideBarLinks from './SidebarLinks'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import UserDetails from '../UserDetails'
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false)
-  const Router = useRouter()
-
   const toggleSidebar = () => {
     setIsOpen(!isOpen)
   }
 
   return (
     <>
-      {' '}
       <button
         onClick={toggleSidebar}
         className="md:hidden p-2 text-purple-600 rounded-md absolute top-2 left-2 "
@@ -22,28 +18,20 @@ const Sidebar = () => {
       </button>
       <div className="flex">
         <div
-          className={`fixed inset-0 bg-purple-black  border-r-2 transition-transform transform md:relative md:flex md:flex-col min-h-screen p-5 w-64 ${
+          className={`fixed inset-0 bg-purple-black border-r-2 transition-transform transform md:relative md:flex md:flex-col min-h-screen px-5 w-64 ${
             isOpen ? 'translate-x-0' : '-translate-x-full'
           } md:translate-x-0 md:block`}
         >
-          <div className="flex items-center">
-            <Image
-              onClick={() => Router.push('/')}
-              width={150}
-              height={150}
-              src="/Logo.png"
-              alt="Logo"
-              className="cursor-pointer object-cover"
-            />
-            <button
-              className="text-white md:hidden ml-auto"
-              onClick={toggleSidebar}
-            >
-              &times;
-            </button>
-          </div>
-          <div className="flex flex-col space-y-5 px-4 my-5 items-start">
+          <button
+            className="text-white md:hidden ml-auto mb-5"
+            onClick={toggleSidebar}
+          >
+            &times;
+          </button>
+
+          <div className="flex flex-col space-y-5 px-4 my-5  items-start">
             <SideBarLinks />
+            <UserDetails />
           </div>
         </div>
       </div>
