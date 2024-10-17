746204344edb04382419945993a1ac5b72558b98
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-17 15:25:20 +0500

    added location attribute for check in

--- utils/Checkout_Interface.ts
+++ utils/Checkout_Interface.ts
@@ -1,3 +1,5 @@
+import { LocationCoords } from './AttendanceInterface'
+
 export interface CheckInProps {
   setLoading: React.Dispatch<React.SetStateAction<boolean>>
   currentTime: Date
@@ -5,4 +7,5 @@ export interface CheckInProps {
   attendanceId: string | null
   setAttendanceId: (id: string | null) => void
   setCheckinStatus: (status: boolean) => void
+  setLocation: (currentlocation: LocationCoords) => void
 }
