1ae1244a44ec96101379d4254c876bcecf3e78a5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-21 17:30:01 +0500

    fixed spacing removed and added a shadow

--- components/Tasks/RecentTasks.tsx
+++ components/Tasks/RecentTasks.tsx
@@ -5,32 +5,23 @@ import { useSelector } from 'react-redux'
 import TaskCard from './TaskCard'
 import Loader from '../Loader'
 import { GetUserTasks } from '@/functions/Task/GetUserTasks'
+
 const RecentTasks = () => {
   const [Loading, SetLoading] = useState(false)
   const [allTasks, setTasks] = useState<TaskFetch[]>([])
   const user = useSelector((state: RootState) => state.user)
+
   const fetchData = async () => {
-    // Set loading state to true to indicate data fetching is in progress
     SetLoading(true)
-
     try {
-      // Call an asynchronous function to get user tasks using 'user.Name' and 'user.Email'
       const data = await GetUserTasks(user.Name, user.Email)
-
-      // If the data is successfully retrieved (not null or undefined)
       if (data) {
-        // Set 'tasks' state with the last 3 items from the fetched data (most recent tasks)
         setTasks(data.slice(-3))
-
-        // Set loading state to false since the data fetching is complete
-        SetLoading(false)
       }
     } catch (error) {
-      // If an error occurs during the data fetch, set loading state to false
-      SetLoading(false)
-
-      // Log the error to the console for debugging purposes
       console.error('Error fetching tasks:', error)
+    } finally {
+      SetLoading(false)
     }
   }
 
@@ -39,15 +30,14 @@ const RecentTasks = () => {
   }, [user.Email])
 
   return (
-    <div className=" p-6 rounded-lg shadow-md">
+    <div className="p-6 rounded-lg shadow-lg bg-white">
       {Loading ? (
-        <div className=" flex items-center justify-center my-10">
+        <div className="flex items-center justify-center my-10">
           <Loader />
         </div>
       ) : (
         <>
-          {' '}
-          <h2 className="font-semibold text-2xl text-purple-500">
+          <h2 className="font-semibold text-2xl text-[#a078ff]">
             Recent Tasks
           </h2>
           <div className="mt-4 overflow-x-auto">
@@ -58,4 +48,5 @@ const RecentTasks = () => {
     </div>
   )
 }
+
 export default RecentTasks
