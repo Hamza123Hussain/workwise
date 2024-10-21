522aa141440e1aba816506be96d8d17327727f47
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-21 17:30:28 +0500

    increase dimensions of the image

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -2,17 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div className=" flex justify-center items-center mt-5">
-      <Image
-        width={250}
-        height={250}
-        src="/Logo.png"
-        alt="Logo"
-        className=" object-cover"
-      />
-    </div>
-  )
+  return <Image width={250} height={250} src="/Logo.png" alt="Logo" />
 }
 
 export default Header
