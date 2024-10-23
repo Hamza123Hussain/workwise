ec79b7da5a8baad87ec28f0b34e53f94fcfe1681
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-24 03:09:12 +0500

    Update UserAssign.tsx

--- components/Tasks/UserAssign.tsx
+++ components/Tasks/UserAssign.tsx
@@ -3,6 +3,7 @@ import { RootState } from '@/utils/Redux/Store/Store'
 import { UserFetched } from '@/utils/SignUpInterface'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+
 const UserAssign = ({
   value,
   handleChange,
@@ -15,17 +16,20 @@ const UserAssign = ({
   ) => void
 }) => {
   const User = useSelector((state: RootState) => state.user)
+
+  const [UserFetched, SetUserFetched] = useState<UserFetched[]>([])
+
   const GetUsers = async () => {
     const Data = await Allusers(User.Email)
     if (Data) {
       SetUserFetched(Data)
     }
   }
+
   useEffect(() => {
     GetUsers()
   }, [])
 
-  const [UserFetched, SetUserFetched] = useState<UserFetched[]>([])
   return (
     <div className="mb-4">
       <label className="block text-sm font-bold mb-2 text-purple-400">
@@ -39,17 +43,14 @@ const UserAssign = ({
         required
       >
         <option value="">Select a user</option>
-        {User.Email !== 'octtoppus1@gmail.com' ? (
-          <option value={User.Name}>{User.Name}</option>
-        ) : (
-          UserFetched.map((element) => (
-            <option key={element.Email} value={element.Name}>
-              {element.Name}
-            </option>
-          ))
-        )}
+        {UserFetched.map((element) => (
+          <option key={element.Email} value={element.Name}>
+            {element.Name}
+          </option>
+        ))}
       </select>
     </div>
   )
 }
+
 export default UserAssign
