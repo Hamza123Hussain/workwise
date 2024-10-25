1e6a631d783906cad8839a2d2eec064c33e9767a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-25 17:37:06 +0500

    Update Report_Interface.ts

--- utils/Report_Interface.ts
+++ utils/Report_Interface.ts
@@ -1,10 +1,12 @@
 import { AttendanceRecord } from './AttendanceInterface'
+import { UserFetched } from './SignUpInterface'
 import { TaskFetch } from './TaskformInterface'
 
 export interface MergedUserData {
   user: string
   attendance: AttendanceRecord[]
   tasks: TaskFetch[]
+  userData: UserFetched
 }
 export interface ReportCardProps {
   mergedData: MergedUserData[]
