9585650cab881866073bac0b59cd315fb691d459
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-25 17:22:58 +0500

    showing remaining time too

--- components/Attendance/AttendanceTable.tsx
+++ components/Attendance/AttendanceTable.tsx
@@ -6,39 +6,39 @@ const AttendanceTable = ({
 }: {
   Attendance: AttendanceRecord[]
 }) => {
-  const HoursWorked = Attendance.reduce((acc, element) => {
-    // Sum the Hours_Worked values, which are assumed to be floating-point numbers
-    return acc + element.Hours_Worked
-  }, 0)
-
-  // Calculate the attendance percentage, rounding to two decimal places
+  // Calculate total hours worked
+  const HoursWorked = Attendance.reduce(
+    (acc, element) => acc + element.Hours_Worked,
+    0
+  )
+  // Calculate remaining hours and attendance percentage
+  const remainingHours = 176 - HoursWorked
   const attendancePercentage = ((HoursWorked / 176) * 100).toFixed(2)
-
   return (
-    <div className=" mx-auto px-4 py-6 w-[90vw] sm:w-auto  ">
+    <div className="mx-auto px-4 py-6 w-[90vw] sm:w-auto">
       <div className="overflow-x-auto flex flex-col">
-        <table className="min-w-full bg-blend-darken border-2 bg-[#bd8bff]  border-charcoal-gray shadow-md rounded-lg">
-          <thead className=" bg-black">
+        <table className="min-w-full bg-blend-darken border-2 bg-[#bd8bff] border-charcoal-gray shadow-md rounded-lg">
+          <thead className="bg-black">
             <tr className="bg-blend-darken">
-              <th className="border border-purple-800 px-4 py-2  text-white">
+              <th className="border border-purple-800 px-4 py-2 text-white">
                 User Data
               </th>
-              <th className="border border-purple-800 px-4 py-2  text-white">
+              <th className="border border-purple-800 px-4 py-2 text-white">
                 Entry Time
               </th>
-              <th className="border border-purple-800 px-4 py-2  text-white">
+              <th className="border border-purple-800 px-4 py-2 text-white">
                 Exit Time
               </th>
-              <th className="border border-purple-800 px-4 py-2  text-white">
+              <th className="border border-purple-800 px-4 py-2 text-white">
                 Break Time
               </th>
-              <th className="border border-purple-800 px-4 py-2  text-white">
+              <th className="border border-purple-800 px-4 py-2 text-white">
                 Hour Worked
               </th>
-              <th className="border border-purple-800 px-4 py-2  text-white">
+              <th className="border border-purple-800 px-4 py-2 text-white">
                 Remaining Time
               </th>
-              <th className="border border-purple-800 px-4 py-2  text-white">
+              <th className="border border-purple-800 px-4 py-2 text-white">
                 Current Date
               </th>
             </tr>
@@ -49,10 +49,14 @@ const AttendanceTable = ({
             ))}
           </tbody>
         </table>
-        <div className="flex flex-col  justify-center   gap-4 items-center text-white my-5 space-y-4 ">
+        <div className="flex flex-col justify-center gap-4 items-center text-white my-5 space-y-4">
           <div className="bg-[#b485ff] p-4 flex flex-col items-center text-center rounded-lg shadow-md transition-transform duration-200 w-full sm:w-1/2">
             <h1 className="text-lg font-bold">Number Of Hours Worked</h1>
             <span className="text-2xl">{HoursWorked.toFixed(2)}</span>
+          </div>
+          <div className="bg-[#b485ff] p-4 flex flex-col items-center text-center rounded-lg shadow-md transition-transform duration-200 w-full sm:w-1/2">
+            <h1 className="text-lg font-bold">Remaining Working Hours</h1>
+            <span className="text-2xl">{remainingHours.toFixed(2)}</span>
           </div>
           <div className="bg-[#b485ff] p-4 flex flex-col items-center text-center rounded-lg shadow-md transition-transform duration-200 w-full sm:w-1/2">
             <h1 className="text-lg font-bold">Attendance Percentage</h1>
@@ -63,5 +67,4 @@ const AttendanceTable = ({
     </div>
   )
 }
-
 export default AttendanceTable
