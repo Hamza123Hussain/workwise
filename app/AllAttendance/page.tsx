cdf97453ab384ca90adf2bf42b9aa7211b44e51a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-01 16:57:20 +0500

    added 2 views, for user and table

--- app/AllAttendance/page.tsx
+++ app/AllAttendance/page.tsx
@@ -6,10 +6,13 @@ import { UserFetched } from '@/utils/SignUpInterface'
 import { Allusers } from '@/functions/AUTH/Allusers'
 import Loader from '@/components/Loader'
 import UserSelection from '@/components/Layout/UserSelection'
+import AllAttendance from '@/components/Attendance/AllAttendance'
+import Usertableview from '@/components/Layout/Usertableview'
 const UserAttendance: React.FC = () => {
   const [Users, SetUserFetched] = useState<UserFetched[]>([])
   const user = useSelector((state: RootState) => state.user)
   const [loading, setLoading] = useState(true) // Start loading as true
+  const [isTableView, setIsTableView] = useState(true) // State to track the current view
   // Function to fetch users
   const Getusers = async () => {
     const Data = await Allusers(user.Email)
@@ -21,11 +24,7 @@ const UserAttendance: React.FC = () => {
   // Fetch attendance and users
   useEffect(() => {
     Getusers() // Fetch users
-    return () => {
-      Getusers() // Fetch attendance
-    } // Fetch users
   }, [user])
-
   if (loading) {
     return (
       <div className="min-h-screen flex justify-center items-center">
@@ -35,9 +34,16 @@ const UserAttendance: React.FC = () => {
   }
   return (
     <div className="p-6">
-      <>
+      {/* Conditionally render components based on the view */}
+      <Usertableview
+        setIsTableView={setIsTableView}
+        isTableView={isTableView}
+      />
+      {isTableView ? (
+        <AllAttendance />
+      ) : (
         <UserSelection type="Attendance" Users={Users} />
-      </>
+      )}
     </div>
   )
 }
