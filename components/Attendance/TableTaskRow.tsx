b5013f4d293d5e72c4ad248bc74b06eb88ffc12a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-01 16:59:39 +0500

    Update TableTaskRow.tsx

--- components/Attendance/TableTaskRow.tsx
+++ components/Attendance/TableTaskRow.tsx
@@ -21,29 +21,27 @@ const TaskTableRow = ({
   }, 0)
   return (
     <tbody className="min-w-full">
-      <tr key={userData} className="bg-black text-white ">
-        <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg  p-4">
-          {userData}
-        </td>
-        <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
-          {HighPirority}
-        </td>
-        <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
-          {Medium_Pirotiy}
-        </td>
-        <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
-          {records.length - (Medium_Pirotiy + HighPirority)}
-        </td>
-        <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
-          {tasksAssigned}
-        </td>
-        <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
-          {tasksCompleted}
-        </td>
-        <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
-          {(TaskCompletion / records.length).toFixed(2)}%
-        </td>
-      </tr>
+      <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg  p-4">
+        {userData}
+      </td>
+      <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
+        {HighPirority}
+      </td>
+      <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
+        {Medium_Pirotiy}
+      </td>
+      <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
+        {records.length - (Medium_Pirotiy + HighPirority)}
+      </td>
+      <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
+        {tasksAssigned}
+      </td>
+      <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
+        {tasksCompleted}
+      </td>
+      <td className="border border-purple-600  text-[12px] sm:text-base md:text-lg p-4">
+        {(TaskCompletion / records.length).toFixed(2)}%
+      </td>
     </tbody>
   )
 }
