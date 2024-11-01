9860e2f785190c31ba0f62ccdc49af9a69ee7f9c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-01 16:59:05 +0500

    passing text to function

--- components/Report/DownloadButton.tsx
+++ components/Report/DownloadButton.tsx
@@ -3,12 +3,14 @@ import React, { RefObject } from 'react'
 
 const DownloadButton = ({
   reportRef,
+  text,
 }: {
   reportRef: RefObject<HTMLDivElement>
+  text: string
 }) => {
   return (
     <button
-      onClick={() => downloadPDF(reportRef)}
+      onClick={() => downloadPDF(reportRef, text)}
       className="mt-5 px-4 py-2 text-white rounded bg-[#b473ff]"
     >
       Download as PDF
