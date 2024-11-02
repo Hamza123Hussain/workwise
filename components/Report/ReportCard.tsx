da0f4d5bb51b8372bd7e7eac715ea059730716fd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-02 15:36:58 +0500

    fixed styling and showing salary

--- components/Report/ReportCard.tsx
+++ components/Report/ReportCard.tsx
@@ -1,58 +1,65 @@
-import React, { useRef } from 'react'
-import ReportBody from './ReportBody'
-import ReportHead from './ReportHead'
-import DownloadButton from './DownloadButton'
-import { ReportCardProps } from '@/utils/Report_Interface'
 import { calculateTotalSalary } from '@/functions/Frontend/SalaryTotal'
+import { ReportCardProps } from '@/utils/Report_Interface'
+import { useRef } from 'react'
+import ReportHead from './ReportHead'
+import ReportBody from './ReportBody'
+import DownloadButton from './DownloadButton'
+
+// ReportCard.tsx
 const ReportCard: React.FC<ReportCardProps> = ({ mergedData }) => {
   const reportRef = useRef(null)
-  // Format the total salary to two decimal places
-  const formattedTotalSalary = calculateTotalSalary(mergedData).toFixed(0)
+  const formattedTotalSalary = Math.floor(
+    calculateTotalSalary(mergedData)
+  ).toLocaleString()
+
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div ref={reportRef}>
-        <div className="overflow-x-auto p-4 text-center w-[90vw] sm:w-auto">
-          <ReportHead />
-          <table className="border-collapse border border-purple-600 text-center text-white w-full">
+    <div className="flex flex-col justify-center items-center bg-gray-50 min-h-screen">
+      <div
+        ref={reportRef}
+        className="shadow-lg rounded-lg overflow-hidden w-[90vw] sm:w-auto"
+      >
+        <ReportHead />
+        <div className="overflow-x-auto p-4">
+          <table className="min-w-full text-center border-separate border-spacing-y-2">
             <thead>
-              <tr className="bg-[#a67dff] text-white">
-                <th className="border border-purple-500 p-2 text-xs">
+              <tr className="bg-[#b473ff] text-white text-lg">
+                <th className="p-3 whitespace-nowrap font-extrabold">
                   Username
                 </th>
-                <th className="border border-purple-500 p-2 text-xs whitespace-nowrap">
+                <th className="p-3 whitespace-nowrap font-extrabold">
                   High Priority Tasks
                 </th>
-                <th className="border border-purple-500 p-2 text-xs whitespace-nowrap">
+                <th className="p-3 whitespace-nowrap font-extrabold">
                   Medium Priority Tasks
                 </th>
-                <th className="border border-purple-500 p-2 text-xs whitespace-nowrap">
+                <th className="p-3 whitespace-nowrap font-extrabold">
                   Low Priority Tasks
                 </th>
-                <th className="border border-purple-500 p-2 text-xs whitespace-nowrap">
+                <th className="p-3 whitespace-nowrap font-extrabold">
                   Attendance Percentage
                 </th>
-                <th className="border border-purple-500 p-2 text-xs whitespace-nowrap">
+                <th className="p-3 whitespace-nowrap font-extrabold">
                   Task Percentage
                 </th>
-                <th className="border border-purple-500 p-2 text-xs whitespace-nowrap">
+                <th className="p-3 whitespace-nowrap font-extrabold">
                   Performance Percentage
                 </th>
-                <th className="border border-purple-500 p-2 text-xs">Salary</th>
+                <th className="p-3 whitespace-nowrap font-extrabold">Salary</th>
               </tr>
             </thead>
             <ReportBody mergedData={mergedData} />
           </table>
         </div>
-        {/* Total Salary Section */}
-        <div className="bg-[#a67dff] text-white mt-4 p-4 w-[30vw]  mx-auto rounded-md shadow-md text-center">
-          <h2 className="text-lg font-bold">Total Salaries To Be Paid</h2>
-          <p className="text-2xl">{formattedTotalSalary}</p>
+        <div className="bg-[#b473ff] text-white mt-6 py-6 text-center rounded-md shadow-md mx-6">
+          <h2 className="text-xl font-bold">Total Salaries To Be Paid</h2>
+          <p className="text-3xl font-semibold">{formattedTotalSalary}</p>
         </div>
       </div>
-      <div className="mt-4">
+      <div className="mt-6">
         <DownloadButton text="Performance" reportRef={reportRef} />
       </div>
     </div>
   )
 }
+
 export default ReportCard
