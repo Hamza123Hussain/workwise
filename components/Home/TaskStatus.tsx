56ba70d379a4d153219bef3de8add46b8576b928
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-02 16:26:49 +0500

    getting latest tasks only

--- components/Home/TaskStatus.tsx
+++ components/Home/TaskStatus.tsx
@@ -29,6 +29,15 @@ const TaskStatus = () => {
     fetchData()
   }, [user.Email])
 
+  // Get the current month (0 = January, 1 = February, ..., 11 = December)
+  const currentMonth = new Date().getMonth()
+
+  // Filter tasks for the current month
+  const currentMonthTasks = allTasks.filter((task) => {
+    const taskMonth = new Date(task.createdAt).getMonth()
+    return taskMonth === currentMonth
+  })
+
   return (
     <div className="border-2 border-purple-600 text-white flex-1 p-4 rounded-lg shadow-lg">
       {loading ? (
@@ -36,12 +45,12 @@ const TaskStatus = () => {
           <Loader />
         </div>
       ) : (
-        <div className=" text-black">
+        <div className="text-black">
           <h2 className="text-xl sm:text-2xl font-bold mb-6 text-[#a078ff]">
             Task Progress Overview
           </h2>
-
-          <TaskOverview allTasks={allTasks} />
+          <TaskOverview allTasks={currentMonthTasks} />{' '}
+          {/* Pass filtered tasks */}
         </div>
       )}
     </div>
