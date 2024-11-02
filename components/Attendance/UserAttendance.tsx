8b05ac8d3ee3d9fe0ce048bb1272255a36d6539f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-02 13:24:45 +0500

    cleaned code and removed unused

--- components/Attendance/UserAttendance.tsx
+++ components/Attendance/UserAttendance.tsx
@@ -5,29 +5,31 @@ import { AttendanceRecord } from '../../utils/AttendanceInterface'
 import AttendanceTable from './AttendanceTable'
 import Loader from '../Loader'
 import { getAttendance } from '@/functions/Frontend/GetUserAttendance'
+import SelectedMonths from '../Layout/SelectedMonths'
+import NoAttendance from './NoAttendance'
+import { months } from '@/utils/MonthsArray'
 const UserAttendance: React.FC = () => {
   const user = useSelector((state: RootState) => state.user)
   const [userAttendance, setAttendance] = useState<AttendanceRecord[]>([])
-  const [loading, setLoading] = useState(true) // Start loading as true
-  // Fetch attendance and users
+  const [loading, setLoading] = useState(true)
+  const [selectedMonth, setSelectedMonth] = useState<number>(
+    new Date().getMonth()
+  )
   useEffect(() => {
     const fetchAttendance = async () => {
       if (user.Email) {
+        setLoading(true)
         const attendance = await getAttendance(
           setLoading,
           user.Email,
           user.Name
         )
-        setAttendance(attendance) // Update the attendance state
+        setAttendance(attendance)
+        setLoading(false)
       }
-      setLoading(false) // Stop loading after fetching is complete
     }
-    fetchAttendance() // Fetch attendance
-    return () => {
-      fetchAttendance() // Fetch attendance
-    } // Fetch users
+    fetchAttendance()
   }, [user])
-
   if (loading) {
     return (
       <div className="min-h-screen flex justify-center items-center">
@@ -37,25 +39,23 @@ const UserAttendance: React.FC = () => {
   }
   return (
     <div className="p-6">
+      <h2 className="text-2xl font-semibold mb-4 text-[#c27cff] text-center">
+        Attendance Records for {months[selectedMonth]} - {user.Name}
+      </h2>
+      <SelectedMonths
+        selectedMonth={selectedMonth}
+        setSelectedMonth={setSelectedMonth}
+      />
       {!loading && userAttendance.length === 0 ? (
-        <div className="min-h-screen flex flex-col justify-center items-center text-center p-4">
-          <h1 className="text-6xl text-black font-bold mb-4">
-            No Attendance Records Found
-          </h1>
-          <p className="text-lg text-gray-300 mb-6">
-            It seems you have not recorded any attendance yet. Please check back
-            later or reach out to your administrator.
-          </p>
-          <button className="bg-purple-600 hover:bg-purple-700 text-black font-semibold py-2 px-4 rounded transition duration-300">
-            Go to Dashboard
-          </button>
-        </div>
+        <NoAttendance />
       ) : (
         <>
-          <h2 className="text-2xl font-semibold mb-4 text-[#c27cff] text-center">
-            All Attendance Records For {user.Name}
-          </h2>
-          <AttendanceTable Attendance={userAttendance} />
+          <AttendanceTable
+            Attendance={userAttendance.filter(
+              (record) =>
+                new Date(record.currentDate).getMonth() === selectedMonth
+            )}
+          />
         </>
       )}
     </div>
