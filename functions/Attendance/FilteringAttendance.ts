9945d93b0b83a570d7c26ce084ce6c2b156c6d5b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-02 13:23:34 +0500

    filtering attendance according to the month

---
+++ functions/Attendance/FilteringAttendance.ts
@@ -0,0 +1,16 @@
+import { AttendanceRecord } from '@/utils/AttendanceInterface'
+
+// Filter attendance records based on selected month
+export const filteredAttendance = (
+  groupedAttendance: { [key: string]: AttendanceRecord[] },
+  selectedMonth: number
+) =>
+  Object.keys(groupedAttendance).reduce((acc, key) => {
+    const recordsForMonth = groupedAttendance[key].filter(
+      (record) => new Date(record.currentDate).getMonth() === selectedMonth
+    )
+    if (recordsForMonth.length > 0) {
+      acc[key] = recordsForMonth
+    }
+    return acc
+  }, {} as { [key: string]: AttendanceRecord[] })
