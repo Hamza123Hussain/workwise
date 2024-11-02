eef45da5abfafa0f896f883b805db53828e85857
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-02 13:25:13 +0500

    Update AttendancePage.tsx

--- components/Attendance/AttendancePage.tsx
+++ components/Attendance/AttendancePage.tsx
@@ -5,7 +5,12 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Loader from '../Loader'
 import AttendanceTable from './AttendanceTable'
-const AttendancePage = () => {
+import SelectedMonths from '../Layout/SelectedMonths'
+import { months } from '@/utils/MonthsArray'
+const AttendancePage = ({ type }: { type: string }) => {
+  const [selectedMonth, setSelectedMonth] = useState<number>(
+    new Date().getMonth()
+  )
   const [loading, setLoading] = useState(true)
   const [UserAttendance, setAttendance] = useState<AttendanceRecord[]>([])
   const User = useSelector((state: RootState) => state.user)
@@ -26,7 +31,22 @@ const AttendancePage = () => {
         <Loader />
       </div>
     )
-  return <AttendanceTable Attendance={UserAttendance} />
+  return (
+    <>
+      {' '}
+      <h2 className="text-2xl font-semibold mb-4 text-[#c27cff] text-center">
+        All {type} Records For {months[selectedMonth]} - {SelectedUser}
+      </h2>
+      <SelectedMonths
+        setSelectedMonth={setSelectedMonth}
+        selectedMonth={selectedMonth}
+      />
+      <AttendanceTable
+        Attendance={UserAttendance.filter(
+          (record) => new Date(record.currentDate).getMonth() === selectedMonth
+        )}
+      />
+    </>
+  )
 }
-
 export default AttendancePage
