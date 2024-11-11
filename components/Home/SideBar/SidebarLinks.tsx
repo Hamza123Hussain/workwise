432c3c135f00848b608092706dcde750722eeccd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-11 16:14:47 +0500

    admin acess done

--- components/Home/SideBar/SidebarLinks.tsx
+++ components/Home/SideBar/SidebarLinks.tsx
@@ -1,29 +1,38 @@
-import { links } from '@/utils/LinkArray'
-import { User2 } from 'lucide-react'
 import Link from 'next/link'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/utils/Redux/Store/Store'
+
+import { User2 } from 'lucide-react'
 import { usePathname } from 'next/navigation'
+import { links } from '@/utils/LinkArray'
 
 const SideBarLinks = ({ closeSidebar }: { closeSidebar: () => void }) => {
+  const User = useSelector((state: RootState) => state.user) // Get the user from the state
   const pathname = usePathname()
   // Function to determine if a link is active
   const isActive = (path: string) => pathname === path
+
   return (
     <>
-      {links.map((link, index) => (
-        <Link
-          key={index}
-          href={link.href}
-          onClick={closeSidebar}
-          className={`gap-3 w-full flex items-center ${
-            isActive(link.href)
-              ? 'bg-white text-[#5925da] rounded-lg px-2'
-              : 'text-white'
-          } hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2`}
-        >
-          <link.icon size={18} />
-          <h5>{link.label}</h5>
-        </Link>
-      ))}
+      {links.map(
+        (link, index) =>
+          // Show link if it's either adminOnly: false or the user's email matches the required email for admin-only links
+          (!link.adminOnly || User.Email === link.email) && (
+            <Link
+              key={index}
+              href={link.href}
+              onClick={closeSidebar}
+              className={`gap-3 w-full flex items-center ${
+                isActive(link.href)
+                  ? 'bg-white text-[#5925da] rounded-lg px-2'
+                  : 'text-white'
+              } hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2`}
+            >
+              <link.icon size={18} />
+              <h5>{link.label}</h5>
+            </Link>
+          )
+      )}
       <Link
         href="/Profile"
         className="text-white gap-3 flex w-full items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
@@ -34,4 +43,5 @@ const SideBarLinks = ({ closeSidebar }: { closeSidebar: () => void }) => {
     </>
   )
 }
+
 export default SideBarLinks
--- utils/LinkArray.ts
+++ utils/LinkArray.ts
@@ -12,24 +12,27 @@ import {
 } from 'lucide-react'
 
 export const links = [
-  { href: '/', label: 'Home', icon: House, adminOnly: false },
+  { href: '/', label: 'Home', icon: House, adminOnly: false, email: '' },
   {
     href: '/createtask',
     label: 'Create Task',
     icon: ClipboardList,
     adminOnly: false,
+    email: '',
   },
   {
     href: '/usertasks',
     label: 'User Tasks',
     icon: ListTodo,
     adminOnly: false,
+    email: '',
   },
   {
     href: '/userattendance',
     label: 'User Attendance',
     icon: Calendar,
     adminOnly: false,
+    email: '',
   },
   // Admin-only links
   {
@@ -37,29 +40,34 @@ export const links = [
     label: 'Total Attendance',
     icon: PieChart,
     adminOnly: true,
+    email: 'octtoppus1@gmail.com',
   },
   {
     href: '/All_Tasks',
     label: 'All Task Details',
     icon: Rows3,
     adminOnly: true,
+    email: 'octtoppus1@gmail.com',
   },
   {
     href: '/report',
     label: 'Report',
     icon: FileText,
     adminOnly: true,
+    email: 'octtoppus1@gmail.com',
   },
   {
     href: '/users',
     label: 'All Users',
     icon: Users,
     adminOnly: true,
+    email: 'octtoppus1@gmail.com',
   },
   {
     href: '/Guide',
     label: 'Report Guide',
     icon: BookOpen,
     adminOnly: false,
+    email: '',
   },
 ]
