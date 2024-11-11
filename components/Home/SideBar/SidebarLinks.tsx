a3edd48af65205e305d5f4d5e5aedcc963099f46
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-11 14:42:37 +0500

    Update SidebarLinks.tsx

--- components/Home/SideBar/SidebarLinks.tsx
+++ components/Home/SideBar/SidebarLinks.tsx
@@ -1,84 +1,30 @@
-import { RootState } from '@/utils/Redux/Store/Store'
-import {
-  ClipboardList,
-  House,
-  Users,
-  FileText,
-  Calendar,
-  PieChart,
-  User2,
-  ListTodo,
-  Rows3,
-} from 'lucide-react' // Import additional icons
 import Link from 'next/link'
 import { useSelector } from 'react-redux'
+import { RootState } from '@/utils/Redux/Store/Store'
+import { links } from '@/utils/LinkArray'
+import { User2 } from 'lucide-react'
 
 const SideBarLinks = () => {
   const User = useSelector((state: RootState) => state.user)
 
   return (
     <>
-      <Link
-        href="/"
-        className="text-white gap-3 w-full flex items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
-      >
-        <House size={18} />
-        <h5>Home</h5>
-      </Link>
-      <Link
-        href="/createtask"
-        className="text-white gap-3 flex w-full items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
-      >
-        <ClipboardList size={18} />
-        <h5>Create Task</h5>
-      </Link>
-      <Link
-        href="/usertasks"
-        className="text-white gap-3 flex w-full items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
-      >
-        <ListTodo size={18} />
-        <h5>User Tasks</h5>
-      </Link>
-      <Link
-        href="/userattendance"
-        className="text-white gap-3 flex w-full items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
-      >
-        <Calendar size={18} />
-        <h5>User Attendance</h5>
-      </Link>
-
-      {User.Email === 'octtoppus1@gmail.com' && (
-        <>
+      {links.map((link, index) =>
+        // Only show links for the admin user
+        User.Email === 'octtoppus1@gmail.com' ? (
           <Link
-            href="/AllAttendance"
+            key={index}
+            href={link.href}
             className="text-white gap-3 w-full flex items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
           >
-            <PieChart size={18} />
-            <h5>Total Attendance</h5>
+            {/* Render icon as a React component */}
+            <link.icon size={18} />
+            <h5>{link.label}</h5>
           </Link>
-          <Link
-            href="/All_Tasks"
-            className="text-white gap-3 w-full flex items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
-          >
-            <Rows3 size={18} />
-            <h5>All Task Details</h5>
-          </Link>
-          <Link
-            href="/report"
-            className="text-white gap-3 w-full flex items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
-          >
-            <FileText size={18} />
-            <h5>Report</h5>
-          </Link>
-          <Link
-            href="/users"
-            className="text-white gap-3 flex w-full items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
-          >
-            <Users size={18} />
-            <h5>All Users</h5>
-          </Link>
-        </>
+        ) : null
       )}
+
+      {/* Profile link, always visible */}
       <Link
         href="/Profile"
         className="text-white gap-3 flex w-full items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
