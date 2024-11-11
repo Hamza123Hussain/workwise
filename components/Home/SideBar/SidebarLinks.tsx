c752f0519e285a042ef20e3c200bbefbec1eea2f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-11 16:09:41 +0500

    Update SidebarLinks.tsx

--- components/Home/SideBar/SidebarLinks.tsx
+++ components/Home/SideBar/SidebarLinks.tsx
@@ -1,36 +1,29 @@
-import Link from 'next/link'
-import { useSelector } from 'react-redux'
-import { RootState } from '@/utils/Redux/Store/Store'
 import { links } from '@/utils/LinkArray'
 import { User2 } from 'lucide-react'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 const SideBarLinks = ({ closeSidebar }: { closeSidebar: () => void }) => {
-  const User = useSelector((state: RootState) => state.user) // Get the user from the state
   const pathname = usePathname()
   // Function to determine if a link is active
   const isActive = (path: string) => pathname === path
   return (
     <>
-      {links.map((link, index) =>
-        User.Email === 'octtoppus1@gmail.com' ? (
-          <Link
-            key={index}
-            href={link.href}
-            onClick={closeSidebar}
-            className={`gap-3 w-full flex items-center ${
-              isActive(link.href)
-                ? 'bg-white text-[#5925da] rounded-lg px-2'
-                : 'text-white'
-            } hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2`}
-          >
-            <link.icon size={18} />
-            <h5>{link.label}</h5>
-          </Link>
-        ) : null
-      )}
-
-      {/* Profile link, always visible */}
+      {links.map((link, index) => (
+        <Link
+          key={index}
+          href={link.href}
+          onClick={closeSidebar}
+          className={`gap-3 w-full flex items-center ${
+            isActive(link.href)
+              ? 'bg-white text-[#5925da] rounded-lg px-2'
+              : 'text-white'
+          } hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2`}
+        >
+          <link.icon size={18} />
+          <h5>{link.label}</h5>
+        </Link>
+      ))}
       <Link
         href="/Profile"
         className="text-white gap-3 flex w-full items-center hover:bg-white hover:text-[#5925da] hover:rounded-lg hover:px-2"
