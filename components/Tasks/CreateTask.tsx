b06e39b1736d942039a63d898597f872e2638d68
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-18 17:51:37 +0500

    tweaked style

--- components/Tasks/CreateTask.tsx
+++ components/Tasks/CreateTask.tsx
@@ -35,15 +35,15 @@ const CreateTaskForm = () => {
   }
 
   return (
-    <div className="bg-white p-8 rounded-3xl shadow-lg max-w-lg w-full mx-auto my-8 border-2 border-[#bea2ff]">
-      <h1 className="text-2xl font-bold mb-6 text-purple-400 text-center">
+    <div className="bg-gradient-to-r from-indigo-100 to-purple-100 p-8 rounded-2xl shadow-xl max-w-lg w-full mx-auto my-8 border-2 border-[#be8bff]">
+      <h1 className="text-3xl font-extrabold text-center text-purple-500 mb-8">
         Create a New Task
       </h1>
       <form onSubmit={handleSubmit}>
         <TaskForm taskData={taskData} setTaskData={setTaskData} />
         <button
           type="submit"
-          className="w-full bg-gradient-to-r from-purple-600 to-indigo-600 text-white py-3 rounded-lg shadow hover:from-purple-500 hover:to-indigo-500 transition duration-300"
+          className="w-full bg-gradient-to-r from-purple-600 to-indigo-600 text-white py-3 rounded-xl shadow-md hover:from-purple-500 hover:to-indigo-500 transition duration-300 ease-in-out"
         >
           Create Task
         </button>
