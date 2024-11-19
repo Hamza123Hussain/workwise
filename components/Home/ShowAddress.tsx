1c45d255c097e7bfb99cbd883407523194180d72
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-19 12:57:14 +0500

    Update ShowAddress.tsx

--- components/Home/ShowAddress.tsx
+++ components/Home/ShowAddress.tsx
@@ -1,38 +1,26 @@
 import { LocationCoords } from '@/utils/AttendanceInterface'
-import React from 'react'
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
-
-const geoUrl =
-  'https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json'
+import { FaCompass, FaMapMarkedAlt } from 'react-icons/fa'
 
 const ShowAddress = ({ location }: { location: LocationCoords }) => {
   return (
-    <div className="bg-gradient-to-r from-purple-400 via-purple-500 to-purple-600 p-6 rounded-lg mt-5 shadow-xl max-w-lg mx-auto">
+    <div className="bg-gradient-to-r from-purple-400 via-purple-500 to-purple-600 p-6 rounded-lg mt-5 shadow-xl transition-transform transform hover:scale-105 max-w-lg mx-auto">
       <h1 className="text-3xl font-extrabold text-white">Your Address</h1>
       <p className="text-white text-lg mt-2">{location.location}</p>
 
       <div className="mt-6">
-        <div className="text-gray-100 text-md">
+        <div className="flex items-center space-x-2 text-gray-100 text-md">
+          <FaCompass className="text-yellow-300" />
           <p>
             <span className="font-semibold">Latitude:</span> {location.latitude}
           </p>
+        </div>
+        <div className="flex items-center space-x-2 text-gray-100 text-md mt-3">
+          <FaMapMarkedAlt className="text-blue-300" />
           <p>
             <span className="font-semibold">Longitude:</span>{' '}
             {location.longitude}
           </p>
         </div>
-      </div>
-
-      <div className="mt-6 h-64">
-        <ComposableMap>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography key={geo.rsmKey} geography={geo} />
-              ))
-            }
-          </Geographies>
-        </ComposableMap>
       </div>
     </div>
   )
