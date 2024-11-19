e2422eee962127352e494e4e56a099e54bf3902c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-19 12:15:08 +0500

    removed leaflet as it was causing issues in deployment

--- components/Home/ShowAddress.tsx
+++ components/Home/ShowAddress.tsx
@@ -1,23 +1,7 @@
 import { LocationCoords } from '@/utils/AttendanceInterface'
 import { FaCompass, FaMapMarkedAlt } from 'react-icons/fa'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { Icon } from 'leaflet'
-import 'leaflet/dist/leaflet.css'
 
 const ShowAddress = ({ location }: { location: LocationCoords }) => {
-  // Ensure that the location's coordinates are passed correctly as an array.
-  const position: [number, number] = [location.latitude, location.longitude] // Coordinate position for the map
-
-  // Custom icon for marker
-  const customIcon = new Icon({
-    iconUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-  })
-
   return (
     <div className="bg-gradient-to-r from-purple-400 via-purple-500 to-purple-600 p-6 rounded-lg mt-5 shadow-xl transition-transform transform hover:scale-105 max-w-lg mx-auto">
       <h1 className="text-3xl font-extrabold text-white">Your Address</h1>
@@ -37,24 +21,6 @@ const ShowAddress = ({ location }: { location: LocationCoords }) => {
             {location.longitude}
           </p>
         </div>
-      </div>
-
-      <div className="mt-6 h-64">
-        <MapContainer
-          center={position}
-          zoom={13}
-          scrollWheelZoom={false}
-          className="w-full h-full rounded-lg shadow-lg"
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {/* Corrected Marker component */}
-          <Marker position={position} icon={customIcon}>
-            <Popup>{location.location}</Popup>
-          </Marker>
-        </MapContainer>
       </div>
     </div>
   )
