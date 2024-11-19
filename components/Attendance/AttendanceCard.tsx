099edd5ead0bdb52fc1768861c194efc44e50b72
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-19 17:28:05 +0500

    made attendance cards to improve visibility on phone

--- components/Attendance/AttendanceCard.tsx
+++ components/Attendance/AttendanceCard.tsx
@@ -1,38 +1,95 @@
 import { convertHoursToString } from '@/functions/Attendance/TimeFunctions'
 import { AttendanceRecord } from '../../utils/AttendanceInterface'
+import { motion } from 'framer-motion'
+import { FaSignInAlt, FaSignOutAlt, FaRegClock } from 'react-icons/fa'
+import { GiPauseButton } from 'react-icons/gi'
+import { IoMdTime } from 'react-icons/io'
+import { BsFillCalendarEventFill } from 'react-icons/bs'
+
 const AttendanceCard = ({ element }: { element: AttendanceRecord }) => {
   return (
-    <tr key={element.id} className="border-b hover:bg-purple-80">
-      <td className="border border-purple-800 px-4 py-2  text-white text-xs ">
-        {element.UserData || '-'}
-      </td>
-      <td className="border border-purple-800 px-4 py-2  text-white  text-xs ">
-        {element.entry ? new Date(element.entry).toLocaleTimeString() : '-'}
-      </td>
-      <td className="border border-purple-800 px-4 py-2  text-white  text-xs ">
-        {element.exit ? new Date(element.exit).toLocaleTimeString() : '-'}
-      </td>{' '}
-      <td className="border border-purple-800 px-4 py-2  text-white  text-xs ">
-        {element.Break_Time ? convertHoursToString(element.Break_Time) : '-'}
-      </td>
-      <td className="border border-purple-800 px-4 py-2  text-white  text-xs ">
-        {element.Hours_Worked
-          ? convertHoursToString(element.Hours_Worked - element.Break_Time)
-          : ''}
-      </td>
-      <td className="border border-purple-800 px-4 py-2  text-white  text-xs ">
-        {element.Hours_Worked
-          ? element.Hours_Worked > 8
-            ? '0hrs, 0mins, 0secs'
-            : convertHoursToString(8 - element.Hours_Worked)
-          : ''}
-      </td>
-      <td className="border border-purple-800 px-4 py-2  text-white  text-xs ">
-        {element.currentDate
-          ? new Date(element.currentDate).toLocaleDateString()
-          : '-'}
-      </td>
-    </tr>
+    <motion.div
+      className="space-y-4 bg-white rounded-lg shadow-lg p-6 hover:shadow-2xl transition-all duration-300 transform hover:scale-105"
+      whileHover={{ scale: 1.05 }}
+      transition={{ duration: 0.3 }}
+    >
+      {/* Date Section */}
+      <div className="flex items-center gap-2 text-xl font-semibold text-purple-600">
+        <BsFillCalendarEventFill className="text-lg" />
+        <span>
+          {element.currentDate
+            ? new Date(element.currentDate).toLocaleDateString()
+            : '-'}
+        </span>
+      </div>
+
+      {/* Attendance Details Section */}
+      <div className="text-gray-800 space-y-3">
+        {/* Entry Time */}
+        <div className="flex items-center flex-col justify-between py-2 px-3 bg-gray-50 rounded-lg">
+          <div className="flex  items-center gap-2 text-sm">
+            <FaSignInAlt className="text-purple-600" />
+            <span className="font-medium">Entry Time</span>
+          </div>
+          <span className="text-sm text-gray-600">
+            {element.entry ? new Date(element.entry).toLocaleTimeString() : '-'}
+          </span>
+        </div>
+
+        {/* Exit Time */}
+        <div className="flex items-center flex-col justify-between py-2 px-3 bg-gray-50 rounded-lg">
+          <div className="flex  items-center gap-2 text-sm">
+            <FaSignOutAlt className="text-purple-600" />
+            <span className="font-medium">Exit Time</span>
+          </div>
+          <span className="text-sm text-gray-600">
+            {element.exit ? new Date(element.exit).toLocaleTimeString() : '-'}
+          </span>
+        </div>
+
+        {/* Break Time */}
+        <div className="flex items-center flex-col justify-between py-2 px-3 bg-gray-50 rounded-lg">
+          <div className="flex items-center  gap-2 text-sm">
+            <GiPauseButton className="text-purple-600" />
+            <span className="font-medium">Break Time</span>
+          </div>
+          <span className="text-sm text-gray-600">
+            {element.Break_Time
+              ? convertHoursToString(element.Break_Time)
+              : '-'}
+          </span>
+        </div>
+
+        {/* Hours Worked */}
+        <div className="flex items-center flex-col  justify-between py-2 px-3 bg-gray-50 rounded-lg">
+          <div className="flex items-center gap-2 text-sm">
+            <FaRegClock className="text-purple-600" />
+            <span className="font-medium">Hours Worked:</span>
+          </div>
+          <span className="text-sm text-gray-600">
+            {element.Hours_Worked
+              ? convertHoursToString(element.Hours_Worked - element.Break_Time)
+              : '0 hrs'}
+          </span>
+        </div>
+
+        {/* Remaining Time */}
+        <div className="flex items-center flex-col justify-between py-2 px-3 bg-gray-50 rounded-lg">
+          <div className="flex items-center gap-2 text-sm">
+            <IoMdTime className="text-purple-600" />
+            <span className="font-medium">Remaining Time:</span>
+          </div>
+          <span className="text-sm text-gray-600">
+            {element.Hours_Worked
+              ? element.Hours_Worked > 8
+                ? '0 hrs'
+                : convertHoursToString(8 - element.Hours_Worked)
+              : '8 hrs'}
+          </span>
+        </div>
+      </div>
+    </motion.div>
   )
 }
+
 export default AttendanceCard
--- components/Attendance/UserAttendance.tsx
+++ components/Attendance/UserAttendance.tsx
@@ -8,11 +8,13 @@ import { getAttendance } from '@/functions/Frontend/GetUserAttendance'
 import SelectedMonths from '../Layout/SelectedMonths'
 import NoAttendance from './NoAttendance'
 import { months } from '@/utils/MonthsArray'
+
 const UserAttendance: React.FC = () => {
   const user = useSelector((state: RootState) => state.user)
   const [userAttendance, setAttendance] = useState<AttendanceRecord[]>([])
   const [loading, setLoading] = useState(true)
   const Month = useSelector((state: RootState) => state.sort.Month)
+
   useEffect(() => {
     const fetchAttendance = async () => {
       if (user.Email) {
@@ -28,6 +30,17 @@ const UserAttendance: React.FC = () => {
     }
     fetchAttendance()
   }, [user])
+
+  // Sort the records to get the latest one
+  const filteredAttendance = userAttendance.filter(
+    (record) => new Date(record.currentDate).getMonth() === Month
+  )
+
+  const latestAttendance = filteredAttendance.sort(
+    (a, b) =>
+      new Date(b.currentDate).getTime() - new Date(a.currentDate).getTime()
+  )[0] // Get the latest record
+
   if (loading) {
     return (
       <div className="min-h-screen flex justify-center items-center">
@@ -35,24 +48,23 @@ const UserAttendance: React.FC = () => {
       </div>
     )
   }
+
   return (
     <div className="p-6">
       <h2 className="text-2xl font-semibold mb-4 text-[#c27cff] text-center">
         Attendance Records for {months[Month]} - {user.Name}
       </h2>
       <SelectedMonths />
-      {!loading && userAttendance.length === 0 ? (
-        <NoAttendance />
-      ) : (
-        <>
-          <AttendanceTable
-            Attendance={userAttendance.filter(
-              (record) => new Date(record.currentDate).getMonth() === Month
-            )}
-          />
-        </>
+
+      {/* Show the latest attendance record if it exists */}
+      {latestAttendance ? <div className="mb-4"></div> : <NoAttendance />}
+
+      {/* If there are multiple attendance records, show the table */}
+      {filteredAttendance.length > 1 && (
+        <AttendanceTable Attendance={filteredAttendance} />
       )}
     </div>
   )
 }
+
 export default UserAttendance
