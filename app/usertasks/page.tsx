6f1d6127c374191bd4b5a3bec385cb97bff157a5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-19 17:32:59 +0500

    showing tasks

--- app/usertasks/page.tsx
+++ app/usertasks/page.tsx
@@ -60,7 +60,7 @@ const UserTasks = () => {
 
       <Dropdowns />
 
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {filteredTasks(
           allTasks,
           SortTask.Status,
