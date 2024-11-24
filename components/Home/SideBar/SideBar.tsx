cceffa130bde743678a9828ef8bca0dfcb019ec0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-25 02:30:24 +0500

    new sidebar

--- components/Home/SideBar/SideBar.tsx
+++ components/Home/SideBar/SideBar.tsx
@@ -4,37 +4,48 @@ import UserDetails from '../UserDetails'
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false)
+
   const toggleSidebar = () => {
     setIsOpen(!isOpen)
   }
 
   return (
     <>
+      {/* Hamburger Icon */}
       <button
         onClick={toggleSidebar}
-        className="md:hidden p-2 text-[#8D6ED9] rounded-md absolute top-2 left-2 "
+        className="p-2 text-[#8D6ED9] rounded-md fixed top-2 left-2 z-20"
       >
         ☰
       </button>
-      <div className="flex">
-        <div
-          className={`fixed inset-0 z-10 sm:z-0 bg-[#8D6ED9] border-r-2 transition-transform transform md:relative md:flex md:flex-col min-h-screen px-5 w-64 ${
-            isOpen ? 'translate-x-0' : '-translate-x-full'
-          } md:translate-x-0 md:block`}
-        >
-          <button
-            className="text-white md:hidden ml-auto "
-            onClick={toggleSidebar}
-          >
-            &times;
-          </button>
 
-          <div className="flex flex-col space-y-4 px-4 my-5 items-start">
-            <UserDetails />
-            <SideBarLinks closeSidebar={toggleSidebar} />
-          </div>
+      {/* Sidebar */}
+      <div
+        className={`fixed inset-y-0 left-0 z-40 bg-[#8D6ED9] border-r-2 transition-transform transform min-h-screen px-5  ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
+      >
+        {/* Close Button */}
+        <button
+          className="text-white ml-auto block p-2"
+          onClick={toggleSidebar}
+        >
+          &times;
+        </button>
+
+        <div className="flex flex-col space-y-4 px-4 my-5 items-start">
+          <UserDetails />
+          <SideBarLinks closeSidebar={toggleSidebar} />
         </div>
       </div>
+
+      {/* Overlay */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-80 "
+          onClick={toggleSidebar}
+        ></div>
+      )}
     </>
   )
 }
