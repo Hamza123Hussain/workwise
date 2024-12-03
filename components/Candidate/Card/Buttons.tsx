5b1204e99f39508316249e2fe65a76601f57c6f9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-03 14:14:07 +0500

    Update Buttons.tsx

--- components/Candidate/Card/Buttons.tsx
+++ components/Candidate/Card/Buttons.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { FaCalendarAlt, FaInfoCircle } from 'react-icons/fa'
+import { FaCalendarAlt, FaInfoCircle, FaUserEdit } from 'react-icons/fa'
 
 const Buttons = ({ InterviewDate }: { InterviewDate: Date }) => {
   return (
     <div className="mt-6 flex sm:flex-row flex-col justify-center items-center gap-4">
       {/* Schedule Interview Button */}
       <button
-        className="flex items-center text-xs text-nowrap gap-2 px-6 py-2 bg-blue-500 text-white font-semibold rounded-full shadow-lg hover:from-teal-500 hover:to-blue-600 transition transform hover:scale-105"
+        className="flex items-center text-xs text-nowrap gap-2 px-6 py-2 bg-blue-500 text-white font-semibold rounded-full shadow-lg hover:bg-blue-600 transition transform hover:scale-105"
         onClick={() =>
           console.log(
             `Scheduled Interview on ${InterviewDate.toLocaleDateString()}`
@@ -18,10 +18,18 @@ const Buttons = ({ InterviewDate }: { InterviewDate: Date }) => {
 
       {/* Show All Details Button */}
       <button
-        className="flex items-center text-xs text-nowrap  gap-2 px-6 py-2 bg-green-400 to-purple-500 text-white font-semibold rounded-full shadow-lg hover:from-green-500 hover:to-purple-600 transition transform hover:scale-105"
+        className="flex items-center text-xs text-nowrap gap-2 px-6 py-2 bg-green-500 text-white font-semibold rounded-full shadow-lg hover:bg-green-600 transition transform hover:scale-105"
         onClick={() => console.log('Showing all details')}
       >
         <FaInfoCircle /> Show All Details
+      </button>
+
+      {/* Update Candidate Button */}
+      <button
+        className="flex items-center text-xs text-nowrap gap-2 px-6 py-2 bg-purple-500 text-white font-semibold rounded-full shadow-lg hover:bg-purple-600 transition transform hover:scale-105"
+        onClick={() => console.log('Updating candidate details')}
+      >
+        <FaUserEdit /> Update Candidate
       </button>
     </div>
   )
