81ed25754d4d65319d9e1b623212db450fed8f21
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-03 13:46:43 +0500

    this function gets all the candidates

---
+++ functions/Candidates/GetAllCandidates.ts
@@ -0,0 +1,19 @@
+import { ApiUrl } from '@/utils/AttendanceInterface'
+import axios from 'axios'
+export const GetCandidates = async (Email: string) => {
+  try {
+    const Response = await axios.get(`${ApiUrl}Api/Candidate/GetALLCandidate`, {
+      params: { Email },
+    })
+    if (Response.status === 200) {
+      console.log('ALL DATA HERE:', Response.data)
+      return Response.data // Return the fetched data for further use
+    } else {
+      console.warn('Unexpected response status:', Response.status)
+      return null
+    }
+  } catch (error) {
+    console.error('Error fetching candidates:', error)
+    throw error // Rethrow the error for the caller to handle if needed
+  }
+}
