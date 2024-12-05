80ed52309c61e5632aaf62e2d9895c3973d8e631
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-05 15:28:17 +0500

    get all notices

---
+++ functions/Notice/GetNotices.ts
@@ -0,0 +1,12 @@
+import { ApiUrl } from '@/utils/AttendanceInterface'
+import axios from 'axios'
+
+export const fetchAllNotices = async () => {
+  try {
+    const response = await axios.get(`${ApiUrl}Api/Notice/GetAllNotices`)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching notices:', error)
+    throw error
+  }
+}
