ebdc37ef4882deeaee019f42e638f03283bc9d49
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-05 16:39:16 +0500

    made the text responsive

--- components/Notice/NoticeCard.tsx
+++ components/Notice/NoticeCard.tsx
@@ -15,13 +15,17 @@ const NoticeCard: React.FC<NoticeProps> = ({
   date,
 }) => {
   return (
-    <div className="bg-gradient-to-r from-blue-100 via-blue-200 to-blue-300 shadow-lg rounded-lg p-4 mb-4 hover:scale-105 transition-transform duration-300">
+    <div className="bg-gradient-to-r from-blue-100 via-blue-200 to-blue-300 shadow-lg rounded-lg p-4 hover:scale-105 transition-transform duration-300">
       <div className="flex items-center space-x-3">
         <FaExclamationCircle className="text-2xl text-blue-600" />
-        <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
+        <h2 className="text-sm sm:text-xl md:text-2xl font-semibold text-gray-800">
+          {title}
+        </h2>
       </div>
-      <p className="text-sm text-gray-700 mb-2 mt-2">{description}</p>
-      <div className="flex justify-between text-xs text-gray-500">
+      <p className="text-xs sm:text-sm md:text-base text-gray-700 mb-2 mt-2">
+        {description}
+      </p>
+      <div className="flex justify-between text-xs sm:text-sm md:text-base text-gray-500">
         <span className="flex items-center space-x-1">
           <FaRegCommentAlt />
           <span>By: {author}</span>
