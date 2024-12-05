a9108df460e5f7adaf14c760d13c8cc325b5ba53
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-05 15:28:38 +0500

    creating a single notice

---
+++ functions/Notice/CreateNotice.ts
@@ -0,0 +1,18 @@
+import { ApiUrl } from '@/utils/AttendanceInterface'
+import axios from 'axios'
+
+interface NoticeInput {
+  title: string
+  description: string
+  author: string
+}
+
+export const createNotice = async (data: NoticeInput) => {
+  try {
+    const response = await axios.post(`${ApiUrl}Api/Notice/NewNotice`, data)
+    return response.data
+  } catch (error) {
+    console.error('Error creating notice:', error)
+    throw error
+  }
+}
