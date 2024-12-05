1a35f971eac9c7894070649ffe4147bdf02794e9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-05 15:29:08 +0500

    to be completed

---
+++ components/Notice/NoticeCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FaExclamationCircle, FaRegCommentAlt } from 'react-icons/fa'
+
+interface NoticeProps {
+  title: string
+  description: string
+  author: string
+  date: string
+}
+
+const NoticeCard: React.FC<NoticeProps> = ({
+  title,
+  description,
+  author,
+  date,
+}) => {
+  return (
+    <div className="bg-gradient-to-r from-blue-100 via-blue-200 to-blue-300 shadow-lg rounded-lg p-4 mb-4 hover:scale-105 transition-transform duration-300">
+      <div className="flex items-center space-x-3">
+        <FaExclamationCircle className="text-2xl text-blue-600" />
+        <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
+      </div>
+      <p className="text-sm text-gray-700 mb-2 mt-2">{description}</p>
+      <div className="flex justify-between text-xs text-gray-500">
+        <span className="flex items-center space-x-1">
+          <FaRegCommentAlt />
+          <span>By: {author}</span>
+        </span>
+        <span>{new Date(date).toLocaleDateString()}</span>
+      </div>
+    </div>
+  )
+}
+
+export default NoticeCard
