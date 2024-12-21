d11c7ac2d2b2c215c8ac03b518821bfa91577d75
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-21 17:35:57 +0500

    frontend function that creates a component through the backend

---
+++ functions/Candidates/CreateCandidate.ts
@@ -0,0 +1,21 @@
+import { ApiUrl } from '@/utils/AttendanceInterface'
+import { CreateCandidateData } from '@/utils/CandidateInterface'
+import axios from 'axios'
+
+export const createCandidate = async (CandidateData: CreateCandidateData) => {
+  try {
+    // Send the CandidateData directly, without wrapping it in an object
+    const response = await axios.post(
+      `${ApiUrl}Api/Candidate/CreateNewCandidate`,
+      { CandidateData } // Sending CandidateData directly as the body
+    )
+
+    if (response.status === 201) {
+      console.log('Candidate created successfully:', response.data.candidate)
+    } else {
+      console.error('Error creating candidate:', response.status)
+    }
+  } catch (error) {
+    console.error('Error creating candidate:', error)
+  }
+}
