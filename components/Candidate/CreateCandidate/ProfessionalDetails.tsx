b53364b3c093732b3ebadc8f972ceec965e0e908
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-21 17:35:30 +0500

    Create ProfessionalDetails.tsx

---
+++ components/Candidate/CreateCandidate/ProfessionalDetails.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import InputField from './Input'
+
+interface ProfessionalDetailsSectionProps {
+  HandleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  Position: string
+  Qualification: string
+  Portfolio: string
+  ExpectedSalary: number
+  CurrentSalary: number
+}
+
+const ProfessionalDetailsSection: React.FC<ProfessionalDetailsSectionProps> = ({
+  Position,
+  Qualification,
+  Portfolio,
+  ExpectedSalary,
+  CurrentSalary,
+  HandleChange,
+}) => {
+  return (
+    <div>
+      <h3 className="text-xl font-semibold mb-4">Professional Details</h3>
+      <InputField
+        label="Position"
+        id="Position"
+        name="Position"
+        value={Position}
+        onChange={HandleChange}
+        type="text"
+        placeholder="Enter desired position"
+      />
+      <InputField
+        label="Qualification"
+        id="Qualification"
+        name="Qualification"
+        value={Qualification}
+        onChange={HandleChange}
+        type="text"
+        placeholder="Enter qualification"
+      />
+      <InputField
+        label="Portfolio"
+        id="Portfolio"
+        name="Portfolio"
+        value={Portfolio}
+        onChange={HandleChange}
+        type="text"
+        placeholder="Enter portfolio URL"
+      />
+      <InputField
+        label="Expected Salary"
+        id="ExpectedSalary"
+        name="ExpectedSalary"
+        value={ExpectedSalary}
+        onChange={HandleChange}
+        type="number"
+        placeholder="Enter expected salary"
+      />
+      <InputField
+        label="Current Salary"
+        id="CurrentSalary"
+        name="CurrentSalary"
+        value={CurrentSalary}
+        onChange={HandleChange}
+        type="number"
+        placeholder="Enter current salary"
+      />
+    </div>
+  )
+}
+
+export default ProfessionalDetailsSection
