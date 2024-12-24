4790cc03cafe6db9a353aef006b3f2ab711967c5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-24 14:14:57 +0500

    fixed layout

--- components/Candidate/Card/Buttons.tsx
+++ components/Candidate/Card/Buttons.tsx
@@ -1,30 +1,25 @@
 import React from 'react'
-import { FaCalendarAlt } from 'react-icons/fa'
 import { BiDetail } from 'react-icons/bi'
 import UpdateModal from './UpdateCandidate/UpdateModal'
+import { CandidateData } from '@/utils/CandidateInterface'
+import UpdateInterviewDateModal from './UpdateCandidate/UpdateInterviewDate'
 
-const Buttons = ({ InterviewDate }: { InterviewDate: Date }) => {
+const Buttons = ({ candidate }: { candidate: CandidateData }) => {
   return (
-    <div className="flex mt-4 flex-col items-center gap-4">
-      <button
-        className="flex items-center text-xs text-nowrap gap-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded-full shadow-lg hover:bg-blue-600 transition transform hover:scale-105 w-full sm:w-auto"
-        onClick={() =>
-          console.log(
-            `Scheduled Interview on ${InterviewDate.toLocaleDateString()}`
-          )
-        }
-      >
-        <FaCalendarAlt size={20} />
-        <span className="hidden sm:inline">Scheduled Interview</span>
-      </button>
-      <button
-        className="flex items-center text-xs text-nowrap gap-2 px-4 py-2 bg-green-500 text-white font-semibold rounded-full shadow-lg hover:bg-green-600 transition transform hover:scale-105 w-full sm:w-auto"
-        onClick={() => console.log('Showing all details')}
-      >
+    <div className="flex mt-4 items-center gap-4 justify-between">
+      {/* Edit Candidate Button */}
+      <UpdateModal candidate={candidate} />
+      {/* Update Interview Date Button */}
+      <UpdateInterviewDateModal
+        Interviewdate={candidate.InterviewDate ? candidate.InterviewDate : ''}
+      />
+      {/* View Details Button */}
+      <button className="flex items-center text-xs gap-2 px-4 py-2 bg-green-500 text-white font-semibold rounded-full shadow-lg hover:bg-green-600 transition transform hover:scale-105 w-full sm:w-auto group">
         <BiDetail size={20} />
-        <span className="hidden sm:inline">View Details</span>
+        <span className="hidden group-hover:inline-block text-sm ml-2">
+          View Details
+        </span>
       </button>
-      <UpdateModal />
     </div>
   )
 }
