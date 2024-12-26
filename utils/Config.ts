7686e636c9744c58fd0e0afd7f0e025ec66b283e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-26 15:26:58 +0500

    Create Config.ts

---
+++ utils/Config.ts
@@ -0,0 +1,15 @@
+const apiKey = process.env.FIREBASE_API_KEY
+const authDomain = process.env.FIREBASE_AUTH_DOMAIN
+const projectId = process.env.FIREBASE_PROJECT_ID
+const storageBucket = process.env.FIREBASE_STORAGE_BUCKET
+const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID
+const appId = process.env.FIREBASE_APP_ID
+
+export {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+}
