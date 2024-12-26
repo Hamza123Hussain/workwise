a6c43b4492c640812e995ee8b1e7c2ca8ea3ee3f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-26 16:52:02 +0500

    made the structure better

--- app/Chats/page.tsx
+++ app/Chats/page.tsx
@@ -1,23 +1,31 @@
 'use client'
 import ChatMessages from '@/components/Chats/AllMessages'
+import InputBar from '@/components/Chats/InputBar'
 import Users from '@/components/Chats/Users'
 import React from 'react'
 
 const Chats = () => {
   return (
-    <div className="flex flex-col lg:flex-row mt-10 gap-6 px-4">
+    <div className="flex flex-col lg:flex-row mt-20 mb-5  px-4">
       {/* Sidebar for Users */}
-      <div className="lg:w-1/4 w-full bg-gray-800 rounded-lg shadow-md p-4">
+      <div className="lg:w-1/4 w-full bg-gray-800 shadow-md p-4">
         <h2 className="text-lg font-semibold text-white mb-4">Users</h2>
         <Users />
       </div>
 
       {/* Chat Messages Section */}
-      <div className="lg:w-3/4 w-full bg-white rounded-lg shadow-md p-4">
-        <h2 className="text-lg font-semibold text-gray-800 mb-4">
+      <div className="lg:w-3/4 w-full  shadow-md border-l-2 border-l-purple-500">
+        <header className="bg-purple-500 text-white py-3 px-6">
+          <h2 className="text-xl font-bold">Chat Messages</h2>
+          <p className="text-sm text-gray-200">
+            View and send messages in real-time.
+          </p>
+        </header>
+        {/* <h2 className="text-lg font-semibold bg-white text-gray-800 mb-4">
           Chat Messages
-        </h2>
+        </h2> */}
         <ChatMessages />
+        <InputBar />
       </div>
     </div>
   )
