b7588c2177720b4fda5f59ce05aba5ec3ad974f9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-26 17:28:13 +0500

    Create ChatLoader.tsx

---
+++ components/Chats/ChatLoader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ChatLoader = () => {
+  return (
+    <section className="dots-container">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </section>
+  )
+}
+
+export default ChatLoader
