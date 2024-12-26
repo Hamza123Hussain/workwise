3f08956313a890b4c1c8c61fd2540887b24440ec
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-26 15:26:34 +0500

    structure fixed and made clean

--- components/Chats/AllMessages.tsx
+++ components/Chats/AllMessages.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { database } from '@/utils/FireBaseConfig'
+import { onChildAdded, ref } from '@firebase/database'
 
 interface Message {
   recipientId: string
@@ -11,12 +13,10 @@ interface Message {
 const ChatMessages = () => {
   const [messages, setMessages] = useState<Message[]>([])
   const [loading, setLoading] = useState<boolean>(true)
-  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        // Make a GET request to the backend endpoint
         const response = await axios.get(
           `https://workwise-backend-puce.vercel.app/Api/Message/GetMessages?chatId=user1-user2`
         )
@@ -31,21 +31,38 @@ const ChatMessages = () => {
     fetchMessages()
   }, [])
 
+  useEffect(() => {
+    const chatRef = ref(database, `chats/user1-user2/messages`)
+
+    const unsubscribe = onChildAdded(chatRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const newMessage = {
+          id: snapshot.key,
+          ...snapshot.val(),
+        }
+        setMessages((prevMessages) => [...prevMessages, newMessage])
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
   if (loading) return <div>Loading messages...</div>
 
-  if (error) return <div className="text-red-500">{error}</div>
-
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md">
-      <h2 className="text-lg font-semibold mb-4">Chat Messages</h2>
+    <div className="flex flex-col gap-3">
       {messages.length === 0 ? (
-        <p>No messages found for this chat.</p>
+        <p className="text-center text-gray-500">
+          No messages found for this chat.
+        </p>
       ) : (
         <ul className="space-y-3">
           {messages.map((message, index) => (
             <li
               key={index}
-              className={`p-3 rounded-lg ${
+              className={`p-3 rounded-lg shadow-md ${
                 message.userId === 'user1'
                   ? 'bg-blue-100 text-right ml-auto'
                   : 'bg-gray-100 text-left mr-auto'
