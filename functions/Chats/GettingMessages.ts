b24d563d0718a1799cbb227e6cf88b6f077be734
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-26 17:27:43 +0500

    cleaning up

--- functions/Chats/GettingMessages.ts
+++ functions/Chats/GettingMessages.ts
@@ -3,11 +3,9 @@ import axios from 'axios'
 // Define the fetchMessages function with TypeScript compatibility
 export const fetchMessages = async (
   chatId: string,
-  setMessages: (messages: Message[]) => void,
-  setLoading: (isLoading: boolean) => void
+  setMessages: (messages: Message[]) => void
 ): Promise<void> => {
   try {
-    setLoading(true) // Set loading to true before fetching
     const response = await axios.get<{ messages: Message[] }>(
       `https://workwise-backend-puce.vercel.app/Api/Message/GetMessages`,
       {
@@ -17,7 +15,5 @@ export const fetchMessages = async (
     setMessages(response.data.messages) // Set the fetched messages
   } catch (err) {
     console.error('Error fetching messages:', err)
-  } finally {
-    setLoading(false) // Ensure loading is set to false after the fetch
   }
 }
