e732acb3efb4e9677c4836177591d41cef386156
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-26 16:52:36 +0500

    Create SingleMessage.tsx

---
+++ components/Chats/SingleMessage.tsx
@@ -0,0 +1,23 @@
+import { Message } from '@/utils/MessageInterface'
+import React from 'react'
+
+const SingleMessage = ({ message }: { message: Message }) => {
+  return (
+    <li
+      key={message.timestamp}
+      className={`p-3 rounded-lg shadow-md ${
+        message.userId === 'user1'
+          ? 'bg-blue-100 text-right ml-auto'
+          : 'bg-gray-100 text-left mr-auto'
+      }`}
+      style={{ maxWidth: '20%' }}
+    >
+      <p className="text-sm font-medium">{message.text}</p>
+      <p className="text-xs text-gray-500">
+        {new Date(message.timestamp).toLocaleString()}
+      </p>
+    </li>
+  )
+}
+
+export default SingleMessage
