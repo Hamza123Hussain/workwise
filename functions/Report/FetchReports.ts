5669162d9b2f02fbba21ee5b0109127e5e90ac67
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-30 17:44:02 +0500

    get all reports data

---
+++ functions/Report/FetchReports.ts
@@ -0,0 +1,25 @@
+import { ApiUrl } from '@/utils/AttendanceInterface'
+import { NewReport } from '@/utils/NewReportInterface'
+import axios from 'axios'
+
+// Function to fetch all reports
+export const fetchReports = async (
+  Email: string,
+  setReports: (report: NewReport[]) => void,
+  setFilteredReports: (report: NewReport[]) => void,
+  setError: (error: string) => void,
+  setLoading: (loading: boolean) => void
+) => {
+  try {
+    const response = await axios.get<NewReport[]>(
+      `${ApiUrl}Api/Report/AllReports?email=${Email}`
+    )
+    setReports(response.data)
+    setFilteredReports(response.data) // Initialize filteredReports with all reports
+  } catch (err) {
+    setError('Failed to fetch reports')
+    console.error(err)
+  } finally {
+    setLoading(false)
+  }
+}
