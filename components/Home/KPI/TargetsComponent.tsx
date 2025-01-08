2f8f5c836c6235fd7882806df3d4954803bf00ed
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-08 18:38:26 +0500

    fixed style a bit

--- components/Home/KPI/TargetsComponent.tsx
+++ components/Home/KPI/TargetsComponent.tsx
@@ -3,19 +3,26 @@ import React from 'react'
 
 const TargetsComponent = ({ target }: { target: Target }) => {
   return (
-    <div key={target.TargetName} className="py-3 flex justify-between">
+    <div
+      key={target.TargetName}
+      className="py-3 flex gap-2 justify-between items-center"
+    >
+      {/* Left Section: Target Name and Priority */}
       <div>
         <h3 className="text-gray-800 font-medium">{target.TargetName}</h3>
         <p className="text-sm text-gray-500">{target.Priority} Priority</p>
       </div>
-      <div className="text-right">
+      {/* Right Section: Score */}
+      <div className="flex items-center gap-1">
         <span className="text-indigo-600 font-semibold">
           {target.ValueAchieved}
         </span>
-        <span className="text-gray-500"> / {target.TargetValue}</span>
+        <span className="text-gray-500">/</span>
+        <span className="text-gray-700 font-semibold">
+          {target.TargetValue}
+        </span>
       </div>
     </div>
   )
 }
-
 export default TargetsComponent
