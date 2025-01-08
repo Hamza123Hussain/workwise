5d465abc09a64623f1b32a6ffac39a5c065e7e93
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-08 18:03:38 +0500

    has to be completed

---
+++ components/Home/KPI/ShowingKpi.tsx
@@ -0,0 +1,11 @@
+import KPIData from './KPIData'
+
+const ShowingKpi = () => {
+  return (
+    <div className="flex flex-col sm:flex-row p-5">
+      <KPIData />
+    </div>
+  )
+}
+
+export default ShowingKpi
