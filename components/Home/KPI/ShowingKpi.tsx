1504e3813c956d636240260cd1a0c51f3b166b05
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-08 18:38:45 +0500

    Update ShowingKpi.tsx

--- components/Home/KPI/ShowingKpi.tsx
+++ components/Home/KPI/ShowingKpi.tsx
@@ -1,9 +1,11 @@
 import KPIData from './KPIData'
+import MonthlyHoursWorked from './ShowingUserHours'
 
 const ShowingKpi = () => {
   return (
-    <div className="flex flex-col sm:flex-row p-5">
+    <div className="flex flex-col sm:flex-row p-5 gap-5">
       <KPIData />
+      <MonthlyHoursWorked />
     </div>
   )
 }
