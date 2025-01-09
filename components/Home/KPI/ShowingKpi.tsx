a5dbe5826e8a8ec01a8535bb5d4602056d0647f9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-09 15:24:16 +0500

    used redux slice

--- components/Home/KPI/ShowingKpi.tsx
+++ components/Home/KPI/ShowingKpi.tsx
@@ -1,12 +1,42 @@
+import { useDispatch, useSelector } from 'react-redux'
 import KPIData from './KPIData'
 import MonthlyHoursWorked from './ShowingUserHours'
-
+import { RootState } from '@/utils/Redux/Store/Store'
+import { useEffect, useState } from 'react'
+import { GetSingleKpi } from '@/functions/Kpi/GetSingleKpi'
+import { setUserKpi } from '@/utils/Redux/Slice/kpi/KpiSlice'
+import ChatLoader from '@/components/Chats/ChatLoader'
 const ShowingKpi = () => {
+  const Dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
+  const user = useSelector((state: RootState) => state.user)
+  useEffect(() => {
+    const fetchSingleKpi = async () => {
+      try {
+        setLoading(true)
+        const data = await GetSingleKpi(user._id)
+        if (data) Dispatch(setUserKpi(data))
+      } catch (error) {
+        console.error('Error fetching KPI data:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    if (user._id) fetchSingleKpi()
+  }, [user._id, Dispatch])
   return (
-    <div className="flex flex-col sm:flex-row p-5 gap-5">
-      <KPIData />
-      <MonthlyHoursWorked />
-    </div>
+    <>
+      {loading ? (
+        <div className="flex justify-center items-center min-h-[50vh]">
+          <ChatLoader />
+        </div>
+      ) : (
+        <div className="flex flex-col sm:flex-row p-5 gap-5">
+          <KPIData />
+          <MonthlyHoursWorked />{' '}
+        </div>
+      )}
+    </>
   )
 }
 
