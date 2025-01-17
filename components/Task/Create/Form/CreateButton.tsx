683ce9d0ff660868c627bc09fd1fe4b982d1c8a2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-17 18:27:00 +0500

    creating new task and updating instanlty

--- components/Task/Create/Form/CreateButton.tsx
+++ components/Task/Create/Form/CreateButton.tsx
@@ -1,12 +1,22 @@
 import { createTask } from '@/functions/UserTasks/CreateUserTask'
 import { RoleTask } from '@/utils/Interfaces/TaskformInterface'
+import {
+  addTask,
+  setOpen,
+} from '@/utils/Redux/Slice/UserTaskSlice/UsetTaskSlice'
 import React from 'react'
 import toast from 'react-hot-toast'
+import { useDispatch } from 'react-redux'
 
 const CreateButton = ({ FormData }: { FormData: RoleTask }) => {
+  const Dispatch = useDispatch()
   const TaskCreation = async () => {
     const TaskCreated = await createTask(FormData)
-    if (TaskCreated) toast.success('Task Has Been Created Successfully')
+    if (TaskCreated) {
+      toast.success('Task Has Been Created Successfully')
+      Dispatch(addTask(TaskCreated.data))
+      Dispatch(setOpen(false))
+    }
   }
   return (
     <div className="flex justify-end gap-4">
