d7e902765e5bc425afa0c39e3866e8e2a81d5eff
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-17 14:53:07 +0500

    creating tasks for the user

---
+++ functions/UserTasks/CreateUserTask.ts
@@ -0,0 +1,19 @@
+import { ApiUrl } from '@/utils/Interfaces/AttendanceInterface'
+import { RoleTask } from '@/utils/Interfaces/TaskformInterface'
+import axios from 'axios'
+
+export const createTask = async (taskData: RoleTask) => {
+  try {
+    const response = await axios.post(
+      '  http://localhost:8000/Api/UserTask/CreateNewTask   ',
+      taskData
+    )
+    // Check if the response is successful
+    if (response.status === 201) {
+      console.log('Task created successfully:', response.data)
+      return response.data // Return the response data
+    }
+  } catch (error) {
+    console.error('Error creating task:', error)
+  }
+}
