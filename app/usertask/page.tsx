29b6d401db8d536e9f1fa58541a3ddc805ca780b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-17 17:38:34 +0500

    Update page.tsx

--- app/usertask/page.tsx
+++ app/usertask/page.tsx
@@ -1,8 +1,5 @@
 'use client'
 import React, { useEffect } from 'react'
-import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
-import { motion } from 'framer-motion'
-import Form from '@/components/Task/Create/Form/Form'
 import { getUserTasks } from '@/functions/UserTasks/GetUserTasks'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState, AppDispatch } from '@/utils/Redux/Store/Store'
@@ -11,25 +8,21 @@ import {
   setTasks,
 } from '@/utils/Redux/Slice/UserTaskSlice/UsetTaskSlice'
 import Layout from '@/components/Task/Card/Layout'
-
+import ModalForTaskCreation from '@/components/Task/Create/Modal'
 const UserTasks = () => {
   const User = useSelector((state: RootState) => state.user)
-  const { open } = useSelector((state: RootState) => state.userTaskSlice)
   const dispatch = useDispatch<AppDispatch>()
-
   const GetUserTasks = async () => {
     const TasksFetched = await getUserTasks(User._id)
     if (TasksFetched) {
       dispatch(setTasks(TasksFetched)) // Store tasks in Redux
     }
   }
-
   useEffect(() => {
     if (User._id) {
       GetUserTasks()
     }
   }, [User._id])
-
   return (
     <div className="my-10 px-4 sm:px-8">
       <div className="flex items-center justify-between mb-6">
@@ -43,33 +36,11 @@ const UserTasks = () => {
           Create Task
         </button>
       </div>
-
       {/* Modal for Task Creation */}
-      <Dialog
-        onClose={() => dispatch(setOpen(false))}
-        open={open}
-        className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
-      >
-        <motion.div
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.9 }}
-          transition={{ duration: 0.3 }}
-          className="fixed inset-0 flex items-center justify-center p-4"
-        >
-          <DialogPanel className="w-full max-w-lg rounded-lg bg-white p-6 shadow-xl space-y-4">
-            <DialogTitle className="text-xl font-semibold text-gray-800">
-              Create New Task
-            </DialogTitle>
-            <Form />
-          </DialogPanel>
-        </motion.div>
-      </Dialog>
-
+      <ModalForTaskCreation />
       {/* Task Cards Layout */}
       <Layout />
     </div>
   )
 }
-
 export default UserTasks
