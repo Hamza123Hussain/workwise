36c296fd17eb9c2601a02ad830abe098009c292b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-30 14:32:31 +0500

    Create KpiReportCard.tsx

---
+++ components/KpiReport/KpiReportCard.tsx
@@ -0,0 +1,48 @@
+import { Kpi } from '@/utils/Interfaces/KPIInterface'
+import React from 'react'
+import { renderPieChart } from './PieChartTasks'
+import TargetReportCard from './TargetReportCard'
+
+const KpiReportCard = ({ userKpi }: { userKpi: Kpi }) => {
+  /**
+   * Sorts the targets by their priority (High > Medium > Low).
+   * The `priorityOrder` object assigns numerical values to each priority level:
+   * - High: 1
+   * - Medium: 2
+   * - Low: 3
+   * Targets are then sorted in ascending order of their numerical priority values.
+   */
+  const sortedTargets = [...userKpi.Targets].sort((a, b) => {
+    const priorityOrder = { High: 1, Medium: 2, Low: 3 } // Define priority order
+    return priorityOrder[a.Priority] - priorityOrder[b.Priority] // Sort based on priority
+  })
+  return (
+    <div
+      id={`kpi-${userKpi.UserId}`} // Unique identifier for each user's KPI card
+      key={userKpi.UserId}
+      className="shadow-xl hover:shadow-2xl transition-all duration-300 ease-in-out rounded-xl z-50 border-2 border-transparent hover:border-gray-400 p-6"
+    >
+      {/* User Details Section */}
+      <div>
+        {/* Display user's name and email */}
+        <h2 className="text-3xl font-extrabold text-gray-400 drop-shadow-lg">
+          {userKpi.UserName}
+        </h2>
+        <p className="text-gray-400 text-lg">{userKpi.UserEmail}</p>
+      </div>
+
+      {/* Target Details Section */}
+      <h3 className="text-2xl font-semibold text-black mt-2">Targets</h3>
+      <div className="space-y-6 ">
+        {/* Loop through the sorted targets and display each target's details */}
+        {sortedTargets.map((target) => (
+          <TargetReportCard target={target} key={target.TargetName} />
+        ))}
+      </div>
+      {/* Render a pie chart for the user's tasks */}
+      {renderPieChart(userKpi.Targets)}
+    </div>
+  )
+}
+
+export default KpiReportCard
