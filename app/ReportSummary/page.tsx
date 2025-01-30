ee7f493827b5889b6b99958b626c7a0942fca2d5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-30 16:09:36 +0500

    summary of the report has been made

---
+++ app/ReportSummary/page.tsx
@@ -0,0 +1,117 @@
+'use client'
+import React, { useState, useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@/utils/Redux/Store/Store'
+import Loader from '@/components/NewReport/Loader'
+import { TaskPriorityChart } from '@/components/Report/TaskReport'
+import SalaryReport from '@/components/Report/SalaryReport'
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
+import { GetAllKpi } from '@/functions/Kpi/GetAllKpis'
+import { setKpis } from '@/utils/Redux/Slice/kpi/KpiListSlice'
+import KpiReportCard from '@/components/Home/KPI/Card/KpiReportCard'
+
+const AdminReports: React.FC = () => {
+  const currentDate = new Date()
+  const year = currentDate.getFullYear()
+  const reportRef = useRef<HTMLDivElement>(null)
+  const user = useSelector((state: RootState) => state.user)
+  const kpis = useSelector((state: RootState) => state.KpiList)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchKpis = async () => {
+      try {
+        setLoading(true)
+        const data = await GetAllKpi(user._id)
+        if (data) dispatch(setKpis(data))
+      } catch (error) {
+        console.error('Error fetching KPIs:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchKpis()
+  }, [user._id, dispatch])
+
+  const downloadPDF = async () => {
+    if (!reportRef.current) return
+    const canvas = await html2canvas(reportRef.current, { scale: 2 })
+    const imgData = canvas.toDataURL('image/png')
+    const pdf = new jsPDF('p', 'mm', 'a4')
+    const pdfWidth = pdf.internal.pageSize.getWidth()
+    const pdfHeight = (canvas.height * pdfWidth) / canvas.width
+    pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight)
+    pdf.save(`Performance_Report_${'January'}_${year}.pdf`)
+  }
+
+  const Totalsalary = kpis.reduce((acc, element) => {
+    return acc + element.Salary
+  }, 0)
+
+  const LowTasks = kpis.reduce((acc, kpi) => {
+    const Lowtask = kpi.Targets.filter(
+      (target) => target.Priority === 'Low'
+    ).length
+    return acc + Lowtask
+  }, 0)
+
+  const HighTasks = kpis.reduce((acc, kpi) => {
+    const HighTask = kpi.Targets.filter(
+      (target) => target.Priority === 'High'
+    ).length
+    return acc + HighTask
+  }, 0)
+
+  const TotalTasks = kpis.reduce((acc, kpi) => {
+    return acc + kpi.Targets.length
+  }, 0)
+
+  return (
+    <div className="p-5 mt-10 z-20">
+      {loading && (
+        <div className="flex justify-center items-center min-h-screen">
+          <Loader />
+        </div>
+      )}
+
+      <div className="flex justify-between items-center mb-1">
+        <button
+          onClick={downloadPDF}
+          className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-700 transition"
+        >
+          Download PDF
+        </button>
+      </div>
+      <div ref={reportRef} className="space-y-2">
+        <h1 className="text-2xl md:text-3xl font-bold text-center text-gray-800 mb-5">
+          Performance Report - {'January'} {year}
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+          {kpis.length > 0 ? (
+            kpis.map((report) => (
+              <KpiReportCard report={report} key={report.UserId} />
+            ))
+          ) : (
+            <p className="text-center text-gray-500 col-span-full">
+              No reports available.
+            </p>
+          )}
+        </div>
+        <div>
+          <TaskPriorityChart
+            lowPriorityTasks={LowTasks}
+            highPriorityTasks={HighTasks}
+            totalTasks={TotalTasks}
+          />
+        </div>
+        <div>
+          <SalaryReport formattedTotalSalary={Totalsalary} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AdminReports
--- components/Home/KPI/Card/KpiReportCard.tsx
+++ components/Home/KPI/Card/KpiReportCard.tsx
@@ -2,24 +2,39 @@ import { Kpi } from '@/utils/Interfaces/KPIInterface'
 import React from 'react'
 
 const KpiReportCard = ({ report }: { report: Kpi }) => {
+  const highTasks = report.Targets.filter(
+    (task) => task.Priority === 'High'
+  ).length
+  const mediumTasks = report.Targets.filter(
+    (task) => task.Priority === 'Medium'
+  ).length
+  const lowTasks = report.Targets.filter(
+    (task) => task.Priority === 'Low'
+  ).length
+
+  const taskCompletionPercentage =
+    (report.PointsGained / report.TotalPoints) * 100
+  const attendancePercentage = (report.HoursWorked / 184) * 100
+  const performancePercentage =
+    attendancePercentage * 0.2 + taskCompletionPercentage * 0.8
+
+  const calculatedSalary =
+    report.Salary * (report.HoursWorked / 184) * 100 * 0.2 +
+    (report.PointsGained / report.TotalPoints) * 100 * 0.8
+
   return (
-    <div
-      key={report.UserId}
-      className="bg-white shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out rounded-xl p-8 border border-gray-300 max-w-md mx-auto"
-    >
-      {/* Header Section */}
+    <div className="bg-white shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out rounded-xl p-8 border border-gray-300 max-w-md mx-auto">
       <div className="mb-6">
         <h2 className="text-3xl font-bold text-blue-700">{report.UserName}</h2>
       </div>
 
-      {/* Task Details */}
       <div className="grid grid-cols-1 gap-4 w-full">
         <div className="bg-blue-50 p-4 rounded-lg">
           <p className="text-gray-700">
             <span className="font-bold text-blue-800">
               High Priority Tasks:
             </span>{' '}
-            {report.Targets.filter((task) => task.Priority === 'High').length}
+            {highTasks}
           </p>
         </div>
         <div className="bg-yellow-50 p-4 rounded-lg">
@@ -27,7 +42,7 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
             <span className="font-bold text-yellow-800">
               Medium Priority Tasks:
             </span>{' '}
-            {report.Targets.filter((task) => task.Priority === 'High').length}
+            {mediumTasks}
           </p>
         </div>
         <div className="bg-green-50 p-4 rounded-lg">
@@ -35,7 +50,7 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
             <span className="font-bold text-green-800">
               Low Priority Tasks:
             </span>{' '}
-            {report.Targets.filter((task) => task.Priority === 'High').length}
+            {lowTasks}
           </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg">
@@ -46,37 +61,34 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
         </div>
       </div>
 
-      {/* Performance Details */}
       <div className="grid grid-cols-1 gap-4 w-full mt-6">
         <div className="bg-indigo-50 p-4 rounded-lg">
           <p className="text-gray-700">
             <span className="font-bold text-indigo-800">Task Completion:</span>{' '}
-            {(report.PointsGained / report.TotalPoints) * 100}%
+            {taskCompletionPercentage.toFixed(2)}%
           </p>
         </div>
         <div className="bg-purple-50 p-4 rounded-lg">
-          {/* <p className="text-gray-700">
+          <p className="text-gray-700">
             <span className="font-bold text-purple-800">Attendance:</span>{' '}
-            {report.AttendancePercentage}%
-          </p> */}
+            {attendancePercentage.toFixed(2)}%
+          </p>
         </div>
         <div className="bg-red-50 p-4 rounded-lg">
-          {/* <p className="text-gray-700">
+          <p className="text-gray-700">
             <span className="font-bold text-red-800">Performance:</span>{' '}
-            {report.PerformancePercentage}%
-          </p> */}
+            {performancePercentage.toFixed(2)}%
+          </p>
         </div>
       </div>
 
-      {/* Divider */}
       <div className="w-full border-t border-gray-400 my-6"></div>
 
-      {/* Salary Details */}
       <div className="flex items-center justify-between w-full">
-        <p className="text-2xl font-bold text-gray-900">Salary:</p>
-        {/* <p className="text-2xl font-bold text-green-700">
-          PKR {formattedTotalPaid}
-        </p> */}
+        <p className="text-lg font-bold text-gray-900">Salary:</p>
+        <p className="text-lg font-bold text-green-700">
+          PKR {calculatedSalary.toFixed(2)}
+        </p>
       </div>
     </div>
   )
