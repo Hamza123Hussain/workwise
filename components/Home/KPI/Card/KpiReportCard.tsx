9a449b1f6c008a69d91be107b6d34f4726045f2b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-04 14:17:06 +0500

    Update KpiReportCard.tsx

--- components/Home/KPI/Card/KpiReportCard.tsx
+++ components/Home/KPI/Card/KpiReportCard.tsx
@@ -2,13 +2,13 @@ import { Kpi } from '@/utils/Interfaces/KPIInterface'
 import React from 'react'
 
 const KpiReportCard = ({ report }: { report: Kpi }) => {
-  const highTasks = report.Targets.filter(
+  const highTargets = report.Targets.filter(
     (task) => task.Priority === 'High'
   ).length
-  const mediumTasks = report.Targets.filter(
+  const mediumTargets = report.Targets.filter(
     (task) => task.Priority === 'Medium'
   ).length
-  const lowTasks = report.Targets.filter(
+  const lowTargets = report.Targets.filter(
     (task) => task.Priority === 'Low'
   ).length
 
@@ -18,10 +18,7 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
   const performancePercentage =
     attendancePercentage * 0.2 + taskCompletionPercentage * 0.8
 
-  const calculatedSalary =
-    report.Salary * (report.HoursWorked / 184) * 100 * 0.2 +
-    (report.PointsGained / report.TotalPoints) * 100 * 0.8
-
+  const calculatedSalary = (report.Salary * performancePercentage) / 100
   return (
     <div className="bg-white shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out rounded-xl p-8 border border-gray-300 max-w-md mx-auto">
       <div className="mb-6">
@@ -32,30 +29,30 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
         <div className="bg-blue-50 p-4 rounded-lg">
           <p className="text-gray-700">
             <span className="font-bold text-blue-800">
-              High Priority Tasks:
+              High Priority Targets:
             </span>{' '}
-            {highTasks}
+            {highTargets}
           </p>
         </div>
         <div className="bg-yellow-50 p-4 rounded-lg">
-          <p className="text-gray-700">
+          <p className="text-gray-700 text-nowrap">
             <span className="font-bold text-yellow-800">
-              Medium Priority Tasks:
+              Medium Priority Targets:
             </span>{' '}
-            {mediumTasks}
+            {mediumTargets}
           </p>
         </div>
         <div className="bg-green-50 p-4 rounded-lg">
           <p className="text-gray-700">
             <span className="font-bold text-green-800">
-              Low Priority Tasks:
+              Low Priority Targets:
             </span>{' '}
-            {lowTasks}
+            {lowTargets}
           </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg">
           <p className="text-gray-700">
-            <span className="font-bold text-gray-800">Total Tasks:</span>{' '}
+            <span className="font-bold text-gray-800">Total Targets:</span>{' '}
             {report.Targets.length}
           </p>
         </div>
@@ -63,8 +60,10 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
 
       <div className="grid grid-cols-1 gap-4 w-full mt-6">
         <div className="bg-indigo-50 p-4 rounded-lg">
-          <p className="text-gray-700">
-            <span className="font-bold text-indigo-800">Task Completion:</span>{' '}
+          <p className="text-gray-700 text-nowrap">
+            <span className="font-bold text-indigo-800">
+              Target Completion:
+            </span>{' '}
             {taskCompletionPercentage.toFixed(2)}%
           </p>
         </div>
@@ -87,7 +86,11 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
       <div className="flex items-center justify-between w-full">
         <p className="text-lg font-bold text-gray-900">Salary:</p>
         <p className="text-lg font-bold text-green-700">
-          PKR {calculatedSalary.toFixed(2)}
+          PKR{' '}
+          {calculatedSalary.toLocaleString('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}
         </p>
       </div>
     </div>
