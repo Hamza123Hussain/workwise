d3724765f1a1277510b6f10e6cb725ca6bc42b17
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-04 14:16:47 +0500

    Update CompleteButton.tsx

--- components/Task/Card/CompleteButton.tsx
+++ components/Task/Card/CompleteButton.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { FaCheckCircle } from 'react-icons/fa'
+// import { FaCheckCircle } from 'react-icons/fa'
 
 const CompleteButton = ({ Completed }: { Completed: boolean }) => {
   return (
     <>
-      <div className="flex items-center mb-6">
+      {/* <div className="flex items-center mb-6">
         <FaCheckCircle
           className={`mr-2 text-xl ${
             Completed ? 'text-green-500' : 'text-gray-500'
@@ -13,9 +13,9 @@ const CompleteButton = ({ Completed }: { Completed: boolean }) => {
         <span className="text-lg">
           {Completed ? 'Task Completed' : 'Task Not Completed'}
         </span>
-      </div>
+      </div> */}
       <button
-        className={`w-full py-3 rounded-md font-semibold text-lg ${
+        className={`w-full py-3 font-semibold text-lg ${
           Completed
             ? 'bg-gray-400 text-gray-700 cursor-not-allowed'
             : 'bg-blue-500 text-white hover:bg-blue-600'
