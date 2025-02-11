1c77dbca6ed14f1aac0796fdbfdeba7152a6453f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-11 12:57:45 +0500

    Update CurrentAttendance.ts

--- functions/Attendance/CurrentAttendance.ts
+++ functions/Attendance/CurrentAttendance.ts
@@ -1,11 +1,13 @@
+import { ApiUrl } from '@/utils/Interfaces/AttendanceInterface'
 import axios from 'axios'
 
 export const CurrentAttendance = async (Email: string) => {
   const Response = await axios.get(
-    `https://workwise-backend-puce.vercel.app/Api/Attendance/CurrentAttendance?Email=${Email}`
+    `${ApiUrl}Api/Attendance/CurrentAttendance?Email=${Email}`
   )
   try {
     if (Response.status == 200) {
+      console.log('Attendance Data Here : ', Response.data)
       return Response.data
     } else if (Response.status === 404) {
       return false
