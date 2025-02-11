2b462f61e5ac91ef5355bff88563897f14b09b2d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-11 12:57:18 +0500

    Update Homepage.tsx

--- components/Home/Homepage.tsx
+++ components/Home/Homepage.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TaskStatus from './TaskStatus'
 import TimeBtn from './TimeBtn'
 import Image from 'next/image'
 import ShowingKpi from './KPI/ShowingKpi'
-// import RecentTasks from '../Tasks/RecentTasks'
+import { CurrentAttendance } from '@/functions/Attendance/CurrentAttendance'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@/utils/Redux/Store/Store'
+import {
+  SetAttendanceID,
+  ToggleCheckin,
+} from '@/utils/Redux/Slice/AttendanceSlice/Attendance_Slice'
 const HomePage = () => {
+  const UserEmail = useSelector((state: RootState) => state.user.Email)
+  const Dispatch = useDispatch()
+  useEffect(() => {
+    const GetCurrentAttendance = async () => {
+      const AttendanceData = await CurrentAttendance(UserEmail)
+      if (AttendanceData) {
+        Dispatch(SetAttendanceID(AttendanceData._id))
+        if (AttendanceData.exit) {
+          Dispatch(ToggleCheckin(false))
+        } else {
+          Dispatch(ToggleCheckin(true))
+        }
+      }
+    }
+    GetCurrentAttendance()
+  }, [])
   return (
     <div className="px-4 sm:px-6 w-full min-h-screen">
       {/* Welcome Message */}
@@ -27,10 +49,8 @@ const HomePage = () => {
           <TimeBtn />
           <TaskStatus />
         </div>
-        {/* <RecentTasks /> */}
       </div>
     </div>
   )
 }
-
 export default HomePage
