db1c53878c79a72f5844afbe2fe56d65a689398e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-17 17:28:44 +0500

    Update KpiReportCard.tsx

--- components/KpiReport/KpiReportCard.tsx
+++ components/KpiReport/KpiReportCard.tsx
@@ -29,25 +29,19 @@ const KpiReportCard = ({ userKpi }: { userKpi: Kpi }) => {
       key={userKpi.UserId}
       className="shadow-xl hover:shadow-2xl transition-all duration-300 ease-in-out rounded-xl z-50 border-2 border-transparent hover:border-gray-400 p-6"
     >
-      {/* User Details Section */}
-      <div>
-        {/* Display user's name and email */}
-        <h2 className="text-3xl font-extrabold text-gray-400 drop-shadow-lg">
-          {userKpi.UserName}
-        </h2>
-        <p className="text-gray-400 text-lg">{userKpi.UserEmail}</p>
-      </div>
-
+      {/* Display user's name and email */}
+      <h2 className="text-3xl font-extrabold text-gray-900 drop-shadow-lg">
+        {userKpi.UserName}
+      </h2>
       {/* Target Details Section */}
       <div className=" flex flex-col justify-between">
-        <h3 className="text-2xl font-semibold text-black my-4">Targets</h3>
+        <h3 className="text-2xl font-extrabold text-black my-4">Targets</h3>
         <div className=" flex flex-col gap-2">
           {/* Loop through the sorted targets and display each target's details */}
           {sortedTargets.map((target) => (
             <TargetReportCard target={target} key={target.TargetName} />
           ))}
         </div>
-
         {/* Render a pie chart for the user's tasks */}
         {renderPieChart(
           userKpi.Targets,
