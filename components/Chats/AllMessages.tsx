26e8da204d00e432989d4e77a3abd884f2fcf497
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-18 18:44:41 +0500

    Update AllMessages.tsx

--- components/Chats/AllMessages.tsx
+++ components/Chats/AllMessages.tsx
@@ -1,62 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import { database } from '@/utils/FireBaseConfig'
-import { onChildAdded, ref } from '@firebase/database'
-import { Message } from '@/utils/Interfaces/MessageInterface'
-import { fetchMessages } from '@/functions/Chats/GettingMessages'
+import React from 'react'
 import SingleMessage from './SingleMessage'
-import ChatLoader from './ChatLoader'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/utils/Redux/Store/Store'
-
 const ChatMessages = () => {
-  const [messages, setMessages] = useState<Message[]>([])
-  const [loading, setLoading] = useState<boolean>(true)
-  const User = useSelector((state: RootState) => state.user)
-  const Recipient = useSelector(
-    (state: RootState) => state.Chat.recipientDetails
-  )
-  const messageIds = new Set<string>() // To track unique message IDs
-  useEffect(() => {
-    setLoading(true)
-    // Fetch initial messages
-    fetchMessages(`${Recipient.Email}-${User.Email}`, (fetchedMessages) => {
-      // Add only unique messages to the state
-      const uniqueMessages = fetchedMessages.filter(
-        (message) => !messageIds.has(message.timestamp.toString())
-      )
-      uniqueMessages.forEach((message) =>
-        messageIds.add(message.timestamp.toString())
-      )
-      setMessages(uniqueMessages)
-      setLoading(false)
-    })
-  }, [])
-  useEffect(() => {
-    setLoading(true)
-    // Listen for new messages in Firebase
-    const chatRef = ref(database, `chats/user1-hamza/messages`)
-    const unsubscribe = onChildAdded(chatRef, (snapshot) => {
-      if (snapshot.exists()) {
-        const newMessage = {
-          id: snapshot.key,
-          ...snapshot.val(),
-        }
-        // Add only unique messages
-        setMessages((prevMessages) => [...prevMessages, newMessage])
-        setLoading(false)
-      }
-    })
-    return () => {
-      unsubscribe() // Cleanup listener on component unmount
-    }
-  }, [])
+  const messages = useSelector((state: RootState) => state.Messages.messages)
+  // useEffect(() => {
+  //   setLoading(true)
+  //   // Listen for new messages in Firebase
+  //   const chatRef = ref(database, `chats/user1-hamza/messages`)
+  //   const unsubscribe = onChildAdded(chatRef, (snapshot) => {
+  //     if (snapshot.exists()) {
+  //       const newMessage = {
+  //         id: snapshot.key,
+  //         ...snapshot.val(),
+  //       }
+  //       // Add only unique messages
+  //       setMessages((prevMessages) => [...prevMessages, newMessage])
+  //       setLoading(false)
+  //     }
+  //   })
+  //   return () => {
+  //     unsubscribe() // Cleanup listener on component unmount
+  //   }
+  // }, [])
   return (
     <div className="flex flex-col gap-3 w-full p-4 h-[70vh] bg-white">
-      {loading ? (
-        <div className=" flex justify-center min-h-screen items-center">
-          <ChatLoader />
-        </div>
-      ) : messages.length === 0 ? (
+      {messages.length === 0 ? (
         <p className="text-center text-gray-500  overflow-y-auto">
           No messages found for this chat.
         </p>
