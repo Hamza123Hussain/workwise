ad2aa1ec0c3f193cc0049afeb046a890d1099718
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-18 18:44:38 +0500

    Update Users.tsx

--- components/Chats/Users.tsx
+++ components/Chats/Users.tsx
@@ -1,16 +1,14 @@
 import { Allusers } from '@/functions/AUTH/Allusers'
 import { RootState } from '@/utils/Redux/Store/Store'
 import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { GettingOnlyUserDetails, Userss } from '@/functions/Kpi/OnlyUserDetails'
 import Loader2 from '../Loader2'
-import { SetRecipentDetails } from '@/utils/Redux/Slice/chatslice/chatslice'
+import SingleUser from './SingleUser'
 const Users = () => {
-  const Dispatch = useDispatch()
   const [AllUsers, SetAllUsers] = useState<Userss[]>([])
   const User = useSelector((state: RootState) => state.user)
   const [loading, setloading] = useState(false)
-  const [UserActive, SetUserActive] = useState('')
   useEffect(() => {
     const GetAllUsers = async () => {
       setloading(true)
@@ -22,16 +20,7 @@ const Users = () => {
     GetAllUsers()
     setloading(false)
   }, [User.Email])
-  const SetRecipentData = (Name: string, Email: string, UserID: string) => {
-    Dispatch(
-      SetRecipentDetails({
-        Name,
-        Email,
-        UserID,
-      })
-    )
-    SetUserActive(Name)
-  }
+
   return (
     <div className="flex flex-col gap-3 h-[70vh] overflow-y-auto">
       {loading ? (
@@ -41,21 +30,12 @@ const Users = () => {
       ) : (
         Allusers.length > 0 &&
         AllUsers.map((element) => (
-          <div
-            onClick={() =>
-              SetRecipentData(
-                element.UserName,
-                element.UserEmail,
-                element.UserId
-              )
-            }
-            key={element.UserEmail}
-            className={`flex items-center ${
-              element.UserName === UserActive ? 'bg-gray-700' : ''
-            } cursor-pointer gap-3 p-2 hover:bg-gray-700 rounded-lg transition-colors`}
-          >
-            <h6 className="text-white font-medium">{element.UserName}</h6>
-          </div>
+          <SingleUser
+            UserName={element.UserName}
+            key={element._id}
+            UserEmail={element.UserEmail}
+            UserId={element._id}
+          />
         ))
       )}
     </div>
