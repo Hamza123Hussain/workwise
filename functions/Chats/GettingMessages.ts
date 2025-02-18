47960a7b1937ba7faa1803b0c877c6c1903babd5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-18 18:51:45 +0500

    Update GettingMessages.ts

--- functions/Chats/GettingMessages.ts
+++ functions/Chats/GettingMessages.ts
@@ -5,7 +5,6 @@ export const fetchMessages = async (UserID: string, RecipentID: string) => {
     const response = await axios.get(
       `https://workwise-backend-puce.vercel.app/Api/Message/GetMessages?RecipentID=${UserID}&UserID=${RecipentID}`
     )
-    console.log('USER MESSAGES : ', response.data.messages)
     return response.data.messages
   } catch (err) {
     console.error('Error fetching messages:', err)
