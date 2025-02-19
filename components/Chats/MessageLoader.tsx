337677587c2973f3279a22e8bfd079f0f28f718c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-19 14:43:22 +0500

    Create MessageLoader.tsx

---
+++ components/Chats/MessageLoader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MessageLoader = () => {
+  return (
+    /* From Uiverse.io by ArnavK-09 */
+    <div className="p-3 animate-spin drop-shadow-2xl bg-gradient-to-bl from-white via-blue to-purple-600 md:w-48 md:h-48 h-32 w-32 aspect-square rounded-full">
+      <div className="rounded-full h-full w-full bg-slate-100 dark:bg-zinc-900 background-blur-md"></div>
+    </div>
+  )
+}
+
+export default MessageLoader
