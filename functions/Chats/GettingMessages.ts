d9b66853b7d580286e83a492020cce69219ae325
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-19 13:15:33 +0500

    Update GettingMessages.ts

--- functions/Chats/GettingMessages.ts
+++ functions/Chats/GettingMessages.ts
@@ -1,10 +1,14 @@
 import axios from 'axios'
+import toast from 'react-hot-toast'
 // Define the fetchMessages function with TypeScript compatibility
 export const fetchMessages = async (UserID: string, RecipentID: string) => {
   try {
+    if (!UserID) return toast.error('Sender ID is required')
+    if (!RecipentID) return toast.error('Recipient ID is required')
     const response = await axios.get(
-      `https://workwise-backend-puce.vercel.app/Api/Message/GetMessages?RecipentID=${UserID}&UserID=${RecipentID}`
+      `https://workwise-backend-puce.vercel.app/Api/Message/GetMessages?RecipentID=${RecipentID}&UserID=${UserID}`
     )
+    console.log('Messages:', response.data.messages)
     return response.data.messages
   } catch (err) {
     console.error('Error fetching messages:', err)
