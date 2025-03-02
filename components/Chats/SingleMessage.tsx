7ec463bc812d3bab61e0ab1eb0cacafe837e0c9f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-02 17:07:13 +0500

    Update SingleMessage.tsx

--- components/Chats/SingleMessage.tsx
+++ components/Chats/SingleMessage.tsx
@@ -1,13 +1,15 @@
 import { MessageInterface } from '@/utils/Interfaces/MessageInterface'
+import { RootState } from '@/utils/Redux/Store/Store'
 import React from 'react'
-
+import { useSelector } from 'react-redux'
 const SingleMessage = ({ message }: { message: MessageInterface }) => {
+  const User = useSelector((state: RootState) => state.user)
   return (
     <li
       key={message.timestamp}
       className={`p-3 rounded-lg shadow-md w-fit ${
-        message.userId === 'user1'
-          ? 'bg-blue-100 text-right ml-auto'
+        message.userId === User._id
+          ? 'bg-blue-300 text-right ml-auto'
           : 'bg-gray-100 text-left mr-auto'
       }`}
     >
