de80d7a7a854b359e735e7901504055b65eb4deb
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-04 12:28:28 +0500

    Create Filters.tsx

---
+++ components/Task/Filters/Filters.tsx
@@ -0,0 +1,46 @@
+import {
+  ResetFlags,
+  setMonth,
+} from '@/utils/Redux/Slice/Sorting_Slice/Sorting_Slice'
+import { RootState } from '@/utils/Redux/Store/Store'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { months } from '@/utils/Array/MonthsArray'
+import YearFilter from './YearFilter'
+import PriorityFilter from './PriorityFilter'
+const Filters = () => {
+  const SortTask = useSelector((state: RootState) => state.sort)
+  const Dispatch = useDispatch()
+  const FilterTasksBy_Month = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    Dispatch(ResetFlags())
+    Dispatch(setMonth(parseInt(e.target.value))) // Update Redux state
+  }
+  return (
+    <div className="flex gap-4 justify-end items-center">
+      {/* Year Filter */}
+      <YearFilter />
+      {/* Month Filter */}
+      <div className="flex flex-col">
+        <label htmlFor="monthFilter" className="font-medium text-[#ac58ff]">
+          Select Month
+        </label>
+        <select
+          id="monthFilter"
+          value={SortTask.Month}
+          onChange={(e) => FilterTasksBy_Month(e)}
+          className="border-2 border-white text-white p-2 rounded-lg 
+              focus:outline-none focus:ring focus:ring-white bg-[#a56edd] transition ease-in-out"
+        >
+          {months.map((month, index) => (
+            <option key={index} value={index}>
+              {month}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* Priority Filter */}
+      <PriorityFilter />
+    </div>
+  )
+}
+export default Filters
