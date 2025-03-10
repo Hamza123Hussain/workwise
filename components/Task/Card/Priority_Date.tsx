8c9755927e3f5d52c359e790c289fedfee8bac46
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-10 11:29:37 +0500

    Update Priority_Date.tsx

--- components/Task/Card/Priority_Date.tsx
+++ components/Task/Card/Priority_Date.tsx
@@ -3,8 +3,9 @@ import { MdPriorityHigh, MdDateRange } from 'react-icons/md'
 interface PriorityDateProps {
   Priority: string
   DueDate: string
+  CreatedAt: string | undefined
 }
-const PriorityDate = ({ Priority, DueDate }: PriorityDateProps) => {
+const PriorityDate = ({ Priority, DueDate, CreatedAt }: PriorityDateProps) => {
   const formatDueDate = (dateStr: string) => {
     if (!dateStr) return 'No due date'
     const date = new Date(dateStr)
@@ -23,11 +24,19 @@ const PriorityDate = ({ Priority, DueDate }: PriorityDateProps) => {
         <span className="text-gray-800 font-semibold text-lg">{Priority}</span>
       </div>
       {/* Due Date */}
-      <div className="flex items-center">
-        <MdDateRange className="text-yellow-500 text-xl mr-2" />
-        <span className="text-gray-700 text-lg">
-          Due Date: {formatDueDate(DueDate)}
-        </span>
+      <div className="flex justify-between sm:flex-row flex-col w-full  items-center">
+        <div className="flex items-center">
+          <MdDateRange className="text-yellow-500 text-xl mr-2" />
+          <span className="text-gray-700 text-lg">
+            Created At: {formatDueDate(CreatedAt ? CreatedAt : '')}
+          </span>
+        </div>
+        <div className="flex items-center">
+          <MdDateRange className="text-yellow-500 text-xl mr-2" />
+          <span className="text-gray-700 text-lg">
+            Due Date: {formatDueDate(DueDate)}
+          </span>
+        </div>{' '}
       </div>
     </div>
   )
