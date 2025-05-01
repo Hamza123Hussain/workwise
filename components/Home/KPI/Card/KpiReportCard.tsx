69f2f8b62b0098601330b445e4e1625f0fa85a64
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-01 14:18:06 +0500

    Update KpiReportCard.tsx

--- components/Home/KPI/Card/KpiReportCard.tsx
+++ components/Home/KPI/Card/KpiReportCard.tsx
@@ -1,6 +1,5 @@
 import { Kpi } from '@/utils/Interfaces/KPIInterface'
 import React from 'react'
-
 const KpiReportCard = ({ report }: { report: Kpi }) => {
   const highTargets = report.Targets.filter(
     (task) => task.Priority === 'High'
@@ -11,20 +10,18 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
   const lowTargets = report.Targets.filter(
     (task) => task.Priority === 'Low'
   ).length
-
   const taskCompletionPercentage =
     (report.PointsGained / report.TotalPoints) * 100
-  const attendancePercentage = (report.HoursWorked / 184) * 100
+  const attendancePercentage = (report.HoursWorked / 160) * 100
   const performancePercentage =
-    attendancePercentage * 0.2 + taskCompletionPercentage * 0.8
+    attendancePercentage * 0.1 + taskCompletionPercentage * 0.9
 
   const calculatedSalary = (report.Salary * performancePercentage) / 100
   return (
     <div className="bg-white shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out rounded-xl p-8 border border-gray-300 max-w-md mx-auto">
       <div className="mb-6">
-        <h2 className="text-3xl font-bold text-blue-700">{report.UserName}</h2>
+        <h2 className="font-bold text-blue-700 text-xl">{report.UserName}</h2>
       </div>
-
       <div className="grid grid-cols-1 gap-4 w-full">
         <div className="bg-blue-50 p-4 rounded-lg">
           <p className="text-gray-700">
@@ -57,7 +54,6 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
           </p>
         </div>
       </div>
-
       <div className="grid grid-cols-1 gap-4 w-full mt-6">
         <div className="bg-indigo-50 p-4 rounded-lg">
           <p className="text-gray-700 text-nowrap">
@@ -80,9 +76,7 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
           </p>
         </div>
       </div>
-
       <div className="w-full border-t border-gray-400 my-6"></div>
-
       <div className="flex items-center justify-between w-full">
         <p className="text-lg font-bold text-gray-900">Salary:</p>
         <p className="text-lg font-bold text-green-700">
@@ -96,5 +90,4 @@ const KpiReportCard = ({ report }: { report: Kpi }) => {
     </div>
   )
 }
-
 export default KpiReportCard
