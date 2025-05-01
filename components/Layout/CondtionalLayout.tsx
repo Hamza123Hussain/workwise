d138366402422adf47e7bf8763bea481cfc871a2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-01 14:17:58 +0500

    Update CondtionalLayout.tsx

--- components/Layout/CondtionalLayout.tsx
+++ components/Layout/CondtionalLayout.tsx
@@ -9,7 +9,6 @@ import SignIn from '../Auth/SignIn'
 import Sidebar from '../Home/SideBar/SideBar'
 import HomePage from '../Home/Homepage'
 import Loader3 from '../Loader3'
-
 const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
   const User = useSelector((state: RootState) => state.user)
   const dispatch = useDispatch()
@@ -19,30 +18,23 @@ const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
     console.log('Checking if client-side:', isClient)
   }, [isClient])
-
   // Fetch and decrypt user data from localStorage
   useEffect(() => {
     const encryptedData = localStorage.getItem('UserData')
-    console.log('Encrypted Data from localStorage:', encryptedData)
-
     if (encryptedData) {
       try {
         const decryptedData = decryptData(encryptedData)
-        console.log('Decrypted Data:', decryptedData)
         dispatch(GetUserData(decryptedData))
       } catch (error) {
         console.error('Error decrypting user data:', error)
       }
     }
   }, [dispatch])
-
   useEffect(() => {
     setIsClient(true)
   }, [])
-
   const authPages = ['/login', '/signup', '/reset']
   const isAuthPage = authPages.includes(pathname)
-
   if (!isClient) {
     return (
       <div className=" min-h-screen justify-center items-center flex">
@@ -50,7 +42,6 @@ const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
       </div>
     ) // Show loading state
   }
-
   return User.Email ? (
     <div className="flex bg-white min-h-screen w-[100vw]">
       <Sidebar />
