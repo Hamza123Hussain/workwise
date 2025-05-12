d7b680999be5b9fefa2f4f34f40b3e8f8d8cc53b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-12 12:37:07 +0500

    Create TasksPerMonth.tsx

---
+++ components/KpiReport/UserReport/TasksPerMonth.tsx
@@ -0,0 +1,67 @@
+import { RoleTask } from '@/utils/Interfaces/TaskformInterface'
+import React, { useState } from 'react'
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+const TasksPerMonth = ({ monthlyTasks }: { monthlyTasks: RoleTask[] }) => {
+  const [TaskFilter, SetFilter] = useState('')
+  const COLORS = ['#60a5fa', '#facc15', '#f87171'] // Blue, Yellow, Red
+  const lowPriorityTasks = monthlyTasks.filter(
+    (task) => task.Priority === 'Low'
+  )
+  const mediumPriorityTasks = monthlyTasks.filter(
+    (task) => task.Priority === 'Medium'
+  )
+  const highPriorityTasks = monthlyTasks.filter(
+    (task) => task.Priority === 'High'
+  )
+  const data = [
+    { name: 'Low', value: lowPriorityTasks.length },
+    { name: 'Medium', value: mediumPriorityTasks.length },
+    { name: 'High', value: highPriorityTasks.length },
+  ]
+  return (
+    <div className=" mx-auto p-6 flex justify-between">
+      {TaskFilter && (
+        <div className="mt-4 text-center text-sm text-gray-700">
+          <span className="font-medium">
+            {monthlyTasks.filter((task) => task.Priority === TaskFilter).length}
+          </span>{' '}
+          tasks with <span className="font-bold">{TaskFilter}</span> priority
+        </div>
+      )}
+      <div className=" w-1/2 h-72">
+        <ResponsiveContainer>
+          <PieChart>
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              outerRadius={90}
+              dataKey="value"
+              isAnimationActive
+              labelLine={false}
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index]}
+                  onClick={() => SetFilter(entry.name)} // Set filter on slice click
+                  cursor="pointer"
+                />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend verticalAlign="bottom" iconType="circle" />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  )
+}
+export default TasksPerMonth
