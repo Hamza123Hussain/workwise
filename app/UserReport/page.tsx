aeaa3cbba2be8cbabfe3573272457269adc61bac
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-12 14:20:42 +0500

    Update page.tsx

--- app/UserReport/page.tsx
+++ app/UserReport/page.tsx
@@ -1,12 +1,12 @@
 'use client'
-import SalaryGaugeChart from '@/components/KpiReport/SalaryPieChart'
 import TasksPerMonth from '@/components/KpiReport/UserReport/TasksPerMonth'
+import UserPerformanceSalary from '@/components/KpiReport/UserReport/UserPerformanceSalary'
 import { getUserTasksForCurrentMonth } from '@/functions/UserTasks/GetTasksForCurrentMonth'
 import { RoleTask } from '@/utils/Interfaces/TaskformInterface'
 import { RootState } from '@/utils/Redux/Store/Store'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-const UserReport = () => {
+const UserUserKpi = () => {
   const User = useSelector((state: RootState) => state.user)
   const [monthlyTasks, setMonthlyTasks] = useState<RoleTask[]>([])
   useEffect(() => {
@@ -19,7 +19,7 @@ const UserReport = () => {
     }
     getMonthlyStats()
   }, [User._id])
-  // Categorize tasks by priority
+
   return (
     <div className=" flex flex-col">
       <div className="my-10 px-4 sm:px-8">
@@ -28,8 +28,8 @@ const UserReport = () => {
         </h1>
         <TasksPerMonth monthlyTasks={monthlyTasks} />
       </div>
-      <SalaryGaugeChart Salary={User.Salary} calculatedsalary={11} />
+      <UserPerformanceSalary User={User} />
     </div>
   )
 }
-export default UserReport
+export default UserUserKpi
