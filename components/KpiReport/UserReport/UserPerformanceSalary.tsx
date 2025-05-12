99a1707a79875b56ceccc66d27359db6ab1baf19
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-12 16:21:22 +0500

    Update UserPerformanceSalary.tsx

--- components/KpiReport/UserReport/UserPerformanceSalary.tsx
+++ components/KpiReport/UserReport/UserPerformanceSalary.tsx
@@ -7,38 +7,37 @@ import { RootState } from '@/utils/Redux/Store/Store'
 import { User } from '@/utils/Interfaces/Report_Interface'
 import { FaBullseye } from 'react-icons/fa'
 import TargetsComponent from '@/components/Home/KPI/TargetsComponent'
+
 const UserPerformanceSalary = ({ User }: { User: User }) => {
-  const Dispatch = useDispatch()
-  // Categorize tasks by priority
+  const dispatch = useDispatch()
   const UserKpi = useSelector((state: RootState) => state.Kpi)
+
   const taskCompletionPercentage =
     (UserKpi.PointsGained / UserKpi.TotalPoints) * 100
   const attendancePercentage = (UserKpi.HoursWorked / 160) * 100
   const performancePercentage =
     attendancePercentage * 0.1 + taskCompletionPercentage * 0.9
   const calculatedSalary = (UserKpi.Salary * performancePercentage) / 100
+
   useEffect(() => {
-    const fetchSingleKpi = async () => {
-      try {
-        const data = await GetSingleKpi(User._id)
-        if (data) Dispatch(setUserKpi(data))
-      } catch (error) {
-        console.error('Error fetching KPI data:', error)
-      }
+    if (User._id) {
+      GetSingleKpi(User._id).then((data) => {
+        if (data) dispatch(setUserKpi(data))
+      })
     }
-    if (User._id) fetchSingleKpi()
-  }, [User._id, Dispatch])
+  }, [User._id, dispatch])
+
   return (
-    <div className=" flex flex-col sm:flex-row justify-center items-center  ml-12  w-full">
+    <div className="flex flex-col sm:flex-row gap-6 w-full mt-10">
       <SalaryGaugeChart
         Salary={User.Salary}
         calculatedsalary={calculatedSalary}
       />
-      <div className=" flex flex-col justify-center items-center">
-        <h2 className="text-lg font-bold text-gray-700 flex  items-center gap-2">
+      <div className="flex flex-col items-center w-full">
+        <h2 className="text-lg font-semibold text-gray-700 flex items-center gap-2">
           <FaBullseye /> Targets
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center items-center border-2 border-gray-200 rounded-lg p-4 gap-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4 w-full border p-4 rounded-lg shadow-sm">
           {UserKpi.Targets?.length ? (
             UserKpi.Targets.map((target) => (
               <TargetsComponent key={target.TargetName} target={target} />
@@ -51,4 +50,5 @@ const UserPerformanceSalary = ({ User }: { User: User }) => {
     </div>
   )
 }
+
 export default UserPerformanceSalary
