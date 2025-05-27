1b4b7bd1f8c5e06b4d7a00168be0d1386ce63ed0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-27 12:48:44 +0500

    Update AttendanceTable.tsx

--- components/Attendance/AttendanceTable.tsx
+++ components/Attendance/AttendanceTable.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import AttendanceCard from './AttendanceCard'
 import { AttendanceRecord } from '../../utils/Interfaces/AttendanceInterface'
-
 const AttendanceTable = ({
   Attendance,
 }: {
@@ -13,38 +12,31 @@ const AttendanceTable = ({
     0
   )
   // Calculate remaining hours and attendance percentage
-  const remainingHours = 184 - HoursWorked
-  const attendancePercentage = ((HoursWorked / 184) * 100).toFixed(2)
-
+  const remainingHours = 176 - HoursWorked
+  const attendancePercentage = ((HoursWorked / 176) * 100).toFixed(2)
   // Define state to handle current page of cards (group of 3 cards at a time)
   const [currentPage, setCurrentPage] = useState(0)
-
   // Define how many cards per page (3 cards per page)
   const cardsPerPage = 3
-
   // Reverse attendance data to show the latest card first
   const reversedAttendance = [...Attendance].reverse()
-
   // Slice the attendance data to show only the cards for the current page
   const currentCards = reversedAttendance.slice(
     currentPage * cardsPerPage,
     (currentPage + 1) * cardsPerPage
   )
-
   // Function to handle next page
   const nextPage = () => {
     if ((currentPage + 1) * cardsPerPage < Attendance.length) {
       setCurrentPage(currentPage + 1)
     }
   }
-
   // Function to handle previous page
   const prevPage = () => {
     if (currentPage > 0) {
       setCurrentPage(currentPage - 1)
     }
   }
-
   return (
     <div className="mx-auto px-4 py-6 flex flex-col">
       {/* Attendance Cards */}
@@ -53,7 +45,6 @@ const AttendanceTable = ({
           <AttendanceCard key={element.id} element={element} />
         ))}
       </div>
-
       {/* Pagination Controls */}
       <div className="flex justify-center gap-6 my-6">
         {/* Previous Button */}
@@ -65,7 +56,6 @@ const AttendanceTable = ({
             Previous
           </button>
         )}
-
         {/* Next Button */}
         {(currentPage + 1) * cardsPerPage < Attendance.length && (
           <button
@@ -76,7 +66,6 @@ const AttendanceTable = ({
           </button>
         )}
       </div>
-
       {/* Attendance Statistics */}
       <div className="flex flex-col sm:flex-row justify-center gap-6 items-center text-white my-6 space-y-6 sm:space-y-0 sm:space-x-6">
         <div className="bg-[#b485ff] p-4 flex flex-col items-center text-center rounded-lg shadow-md transition-transform duration-200 w-full sm:w-1/3">
@@ -95,5 +84,4 @@ const AttendanceTable = ({
     </div>
   )
 }
-
 export default AttendanceTable
