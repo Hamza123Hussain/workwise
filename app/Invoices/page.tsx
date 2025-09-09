1beb9e56eb840a75f574543b1ca2ac436bfd80fa
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-09-09 13:57:30 +0500

    Update page.tsx

--- app/Invoices/page.tsx
+++ app/Invoices/page.tsx
@@ -1,9 +1,26 @@
 'use client'
 import InvoiceList from '@/components/Invoices/InvoiceUi'
-import React from 'react'
+import { fetchInvoices } from '@/utils/Redux/Slice/InvoiceSlice/InvoiceSlice'
+import { AppDispatch, RootState } from '@/utils/Redux/Store/Store'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Invoices = () => {
-  return <InvoiceList />
+  const dispatch = useDispatch<AppDispatch>()
+  const { invoices, loading } = useSelector((state: RootState) => state.Invoice)
+
+  // Fetch invoices on load
+  useEffect(() => {
+    dispatch(fetchInvoices())
+  }, [dispatch])
+
+  return (
+    <>
+      {' '}
+      {loading && <p>Loading...</p>}
+      <InvoiceList Invoices={invoices} />
+    </>
+  )
 }
 
 export default Invoices
