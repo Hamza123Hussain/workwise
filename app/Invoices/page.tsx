bb89ee283be58d3d7f40663e1d082900e9d116c1
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-09-09 13:25:33 +0500

    Create page.tsx

---
+++ app/Invoices/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import InvoiceList from '@/components/Invoices/InvoiceUi'
+import React from 'react'
+
+const Invoices = () => {
+  return <InvoiceList />
+}
+
+export default Invoices
