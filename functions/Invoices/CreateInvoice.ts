7bf28b82d257d39df153145d85b4d06785d0ede2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-09-09 13:25:41 +0500

    Create CreateInvoice.ts

---
+++ functions/Invoices/CreateInvoice.ts
@@ -0,0 +1,22 @@
+import { ApiUrl } from '@/utils/Interfaces/AttendanceInterface'
+import { Invoice } from '@/utils/Interfaces/InvoiceInterface'
+import axios from 'axios'
+
+export const createInvoice = async (Userid: string, data: Invoice) => {
+  const Response = await axios.post(`${ApiUrl}Api/Invoice/CreateInvoice`, {
+    Userid,
+    data,
+  })
+
+  try {
+    if (Response.status === 404) {
+      console.log('Not Authorized To Acess This Route')
+    }
+    if (Response.status === 201) {
+      return Response.data
+    }
+  } catch (error) {
+    // Handle errors during the API call
+    console.error('Error creating Creating Invoice:', error)
+  }
+}
