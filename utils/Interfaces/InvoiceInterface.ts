781ac0f0c327a2ca15514061ba25dfa3832d7db5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-09-09 13:25:26 +0500

    Create InvoiceInterface.ts

---
+++ utils/Interfaces/InvoiceInterface.ts
@@ -0,0 +1,11 @@
+// ✅ Invoice Interface
+export interface Invoice {
+  id: string
+  title: string
+  description: string
+  date: string
+  approvalStatus: boolean
+  companyName: string
+  createdBy: string
+  financialYear: string
+}
