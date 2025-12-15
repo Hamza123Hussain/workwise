c1e218c7a48fb91f198ecfbee9dafd099a97b965
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-12-15 13:22:46 +0500

    Update MainComponent.tsx

--- components/Finance/MainComponent.tsx
+++ components/Finance/MainComponent.tsx
@@ -1,59 +1,43 @@
-// import React, { useState } from 'react'
-// import Modal from './Modal'
-// import PaymentTable from './PaymentTable'
-// import InvoiceSummary from './InvoiceSummary'
-// // import Buttons from './Buttons'
-// const MainFinanceComponent = () => {
-//   const [Payments, SetPayments] = useState([
-//     {
-//       ID: 'INV00025',
-//       Name: 'Hamza Hussain',
-//       Createdon: '22 Feb 2025',
-//       Amount: '50,000',
-//       Paid: '50,000',
-//       PaymentMode: 'Cash',
-//       Status: 'Paid',
-//       PaidTo: 'Global Grads',
-//     },
-//     {
-//       ID: 'INV00025',
-//       Name: 'Hamza Hussain',
-//       Createdon: '22 Feb 2025',
-//       Amount: '50,000',
-//       Paid: '50,000',
-//       PaymentMode: 'Cash',
-//       Status: 'UnPaid',
-//       PaidTo: 'Global Grads',
-//     },
-//     {
-//       ID: 'INV00025',
-//       Name: 'Hamza Hussain',
-//       Createdon: '22 Feb 2025',
-//       Amount: '50,000',
-//       Paid: '50,000',
-//       PaymentMode: 'Cash',
-//       Status: 'Partially Paid',
-//       PaidTo: 'Global Grads',
-//     },
-//   ])
-//   const [isModalOpen, setIsModalOpen] = useState(false)
-//   return (
-//     <div className=" flex flex-col gap-3 px-3">
-//       <InvoiceSummary />
-//       <hr className=" w-full" />
-//       <div className=" flex justify-between px-5">
-//         <h3 className=" text-[20px] text-[#001A67] font-bold">Invoices</h3>
-//         <Buttons setIsModalOpen={setIsModalOpen} />
-//       </div>
-//       <PaymentTable Payments={Payments} />
-//       {isModalOpen && (
-//         <Modal
-//           Payments={Payments}
-//           setPayments={SetPayments}
-//           setIsModalOpen={setIsModalOpen}
-//         />
-//       )}
-//     </div>
-//   )
-// }
-// export default MainFinanceComponent
+import React, { useEffect, useState } from 'react'
+
+import InvoiceSummary from './InvoiceSummary'
+import PaymentTable from './PaymentTable'
+import Modal from './Modal'
+import Buttons from './Buttons'
+import { GetInvoices } from '@/functions/Invoices/GetInvoices'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/utils/Redux/Store/Store'
+// import Buttons from './Buttons'
+const MainFinanceComponent = () => {
+  const User = useSelector((state: RootState) => state.user)
+  useEffect(() => {
+    const AllPayments = async () => {
+      const Data = await GetInvoices(User._id)
+      if (Data) {
+        SetPayments(Data)
+      }
+    }
+    AllPayments()
+  }, [User._id])
+  const [Payments, SetPayments] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  return (
+    <div className=" flex flex-col gap-3 px-3">
+      <InvoiceSummary Payments={Payments} />
+      <hr className=" w-full" />
+      <div className=" flex justify-between px-5">
+        <h3 className=" text-[20px] text-[#001A67] font-bold">Invoices</h3>
+        <Buttons setIsModalOpen={setIsModalOpen} />
+      </div>
+      <PaymentTable Payments={Payments} />
+      {isModalOpen && (
+        <Modal
+          Payments={Payments}
+          setPayments={SetPayments}
+          setIsModalOpen={setIsModalOpen}
+        />
+      )}
+    </div>
+  )
+}
+export default MainFinanceComponent
