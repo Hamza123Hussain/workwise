4d0fd1e662d8dd59e11a1f35886c69cd66c6c1bc
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-12-15 13:22:32 +0500

    Update Header.tsx

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,8 +1,23 @@
-import Image from 'next/image'
+'use client'
 import React from 'react'
-
+import Image from 'next/image'
 const Header = () => {
-  return <Image width={250} height={250} src="/Logo.png" alt="Logo" />
+  return (
+    <div className="relative flex items-center justify-between bg-[#E9E9E9]">
+      {/* Left Logo */}
+      <div className="flex items-center p-2 gap-2">
+        <Image
+          className="mt-2"
+          src={'/Logo.png'}
+          width={150}
+          height={150}
+          alt="Logo"
+        />
+        <h6 className="mt-3 text-black font-extrabold"> | </h6>
+        <h6 className="mt-4 text-black font-extrabold">HRM</h6>
+      </div>
+    </div>
+  )
 }
 
 export default Header
