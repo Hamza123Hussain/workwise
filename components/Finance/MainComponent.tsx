fdf52401c7569e16ffd6ae30be0afd8a3bd87dcf
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-12-31 17:20:43 +0500

    Update MainComponent.tsx

--- components/Finance/MainComponent.tsx
+++ components/Finance/MainComponent.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-
 import InvoiceSummary from './InvoiceSummary'
 import PaymentTable from './PaymentTable'
 import Modal from './Modal'
@@ -7,29 +6,45 @@ import Buttons from './Buttons'
 import { GetInvoices } from '@/functions/Invoices/GetInvoices'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/utils/Redux/Store/Store'
-// import Buttons from './Buttons'
+
 const MainFinanceComponent = () => {
   const User = useSelector((state: RootState) => state.user)
+  const [Payments, SetPayments] = useState<any[]>([])
+  const [filteredPayments, SetFilteredPayments] = useState<any[]>([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   useEffect(() => {
     const AllPayments = async () => {
       const Data = await GetInvoices(User._id)
       if (Data) {
         SetPayments(Data)
+        SetFilteredPayments(Data)
       }
     }
     AllPayments()
   }, [User._id])
-  const [Payments, SetPayments] = useState([])
-  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  // Sorting handler
+  const handleSort = (value: string) => {
+    if (value === '') {
+      SetFilteredPayments(Payments)
+      return
+    }
+    const sorted = [...Payments].filter(
+      (p) => p.PaidTo === value // Sort by CreatedBy as example
+    )
+    SetFilteredPayments(sorted)
+  }
+
   return (
-    <div className=" flex flex-col gap-3 px-3">
-      <InvoiceSummary Payments={Payments} />
-      <hr className=" w-full" />
-      <div className=" flex justify-between px-5">
-        <h3 className=" text-[20px] text-[#001A67] font-bold">Invoices</h3>
-        <Buttons setIsModalOpen={setIsModalOpen} />
+    <div className="flex flex-col gap-3 px-3">
+      <InvoiceSummary Payments={filteredPayments} />
+      <hr className="w-full" />
+      <div className="flex justify-between px-5">
+        <h3 className="text-[20px] text-[#001A67] font-bold">Invoices</h3>
+        <Buttons setIsModalOpen={setIsModalOpen} handleSort={handleSort} />
       </div>
-      <PaymentTable Payments={Payments} />
+      <PaymentTable Payments={filteredPayments} />
       {isModalOpen && (
         <Modal
           Payments={Payments}
@@ -40,4 +55,5 @@ const MainFinanceComponent = () => {
     </div>
   )
 }
+
 export default MainFinanceComponent
