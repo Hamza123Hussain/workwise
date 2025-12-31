4e98564fde6caa33b7bf5fc9b43483c212560bf9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-12-31 14:45:08 +0500

    Update KpiCard.tsx

--- components/Home/KPI/Card/KpiCard.tsx
+++ components/Home/KPI/Card/KpiCard.tsx
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import { Kpi } from '@/utils/Interfaces/KPIInterface'
 import TargetComponent from './TargetComponent'
 import CardHeader from './CardHeader'
+import EditTargetsModal from './EditTargetModal'
+
 const KpiCard = ({ kpi }: { kpi: Kpi }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [userTargets, setUserTargets] = useState(kpi.Targets)
+
+  const handleUpdate = (updatedTargets: typeof userTargets) => {
+    setUserTargets(updatedTargets)
+  }
+
   return (
-    <motion.div
-      key={kpi.UserId}
-      className="bg-white shadow-lg rounded-lg p-6 border-t-4 border-purple-500 hover:shadow-xl transition-shadow duration-300"
-      initial={{ y: 10, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      <CardHeader
-        UserEmail={kpi.UserEmail}
-        UserName={kpi.UserName}
-        Targets={kpi.Targets}
-        PointsGained={kpi.PointsGained}
-        TotalPoints={kpi.TotalPoints}
+    <>
+      <motion.div
+        className="bg-white shadow-lg rounded-lg p-6 border-t-4 border-purple-500 hover:shadow-xl transition-all duration-300 flex flex-col"
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <CardHeader
+          UserEmail={kpi.UserEmail}
+          UserName={kpi.UserName}
+          Targets={userTargets}
+          PointsGained={kpi.PointsGained}
+          TotalPoints={kpi.TotalPoints}
+          onEdit={() => setIsModalOpen(true)}
+        />
+        <div className="divide-y divide-gray-200">
+          {userTargets.map((target) => (
+            <TargetComponent target={target} key={target.TargetName} />
+          ))}
+        </div>
+      </motion.div>
+
+      <EditTargetsModal
+        user={{ ...kpi, Targets: userTargets }}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onUpdate={handleUpdate}
       />
-      <div className="divide-y divide-gray-200 h-[50vh] overflow-y-auto">
-        {kpi.Targets.map((target) => (
-          <TargetComponent target={target} key={target.TargetName} />
-        ))}
-      </div>
-    </motion.div>
+    </>
   )
 }
+
 export default KpiCard
