22a96d390a710f43ec22fc30d39902b056e4c1fd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-12-31 14:41:18 +0500

    Update RoutesArray.tsx

--- utils/Array/RoutesArray.tsx
+++ utils/Array/RoutesArray.tsx
@@ -1,4 +1,5 @@
 import { ChartPie, LayoutDashboard, Receipt } from 'lucide-react'
+import { BiTask } from 'react-icons/bi'
 
 export const Routes = [
   {
@@ -15,5 +16,15 @@ export const Routes = [
     Name: 'Invoices',
     Path: '/invoices',
     SVG: <Receipt />,
+  },
+  {
+    Name: 'Admin',
+    Path: '/Admin',
+    SVG: <Receipt />,
+  },
+  {
+    Name: 'TaskBoard',
+    Path: '/TaskBoard',
+    SVG: <BiTask />,
   },
 ]
