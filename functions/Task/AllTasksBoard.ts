e8a86ae22fed23642b23c456c877c9f2d7bdcda7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-12-31 14:41:23 +0500

    Create AllTasksBoard.ts

---
+++ functions/Task/AllTasksBoard.ts
@@ -0,0 +1,18 @@
+//----------------------------------
+// 3️⃣ GET TASKS GROUPED BY DATE
+
+import axios from 'axios'
+
+//----------------------------------
+export const getTasksGroupedByDate = async () => {
+  try {
+    const response = await axios.get(
+      `https://crm-backend-wcpj.vercel.app/Api/TaskBoard/GetAllTasks`
+    )
+
+    return response.data.groupedTasks // returns { "2025-12-27": [...], ... }
+  } catch (error) {
+    console.error('Error fetching tasks:', error)
+    throw error
+  }
+}
