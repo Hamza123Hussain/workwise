d9cb1d0bb03a3a71b560764bb4b9e8b82f515954
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-01-02 12:46:20 +0500

    Update AddTaskModal.tsx

--- components/TaskBoard/AddTaskModal.tsx
+++ components/TaskBoard/AddTaskModal.tsx
@@ -26,16 +26,26 @@ const AddTaskModal = ({
   return (
     <div className="fixed inset-0 bg-black/30 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
-        <h3 className="text-lg font-semibold mb-4">Add Task</h3>
+        <h3 className="text-lg font-semibold mb-4 ">Add Task</h3>
         <input
           placeholder="Task description..."
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          className="mb-4"
+          className="mb-4 border-2 border-gray-400 w-full rounded-lg px-2"
         />
         <div className="flex justify-end gap-2">
-          <button onClick={onClose}>Cancel</button>
-          <button onClick={handleAdd}>Add Task</button>
+          <button
+            onClick={onClose}
+            className=" rounded-md px-2 bg-black text-white"
+          >
+            Cancel
+          </button>
+          <button
+            className="rounded-md px-2 bg-black text-white"
+            onClick={handleAdd}
+          >
+            Add Task
+          </button>
         </div>
       </div>
     </div>
