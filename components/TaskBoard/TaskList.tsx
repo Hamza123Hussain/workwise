5a65fa179ceaf551e1191abb53eb9d39e0244b19
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-01-19 12:39:03 +0500

    Update TaskList.tsx

--- components/TaskBoard/TaskList.tsx
+++ components/TaskBoard/TaskList.tsx
@@ -3,11 +3,29 @@
 import React from 'react'
 import TaskCard from './TaskCard'
 
-const TaskList = ({ tasks }: { tasks: any[] }) => {
+const TaskList = ({
+  tasks,
+  onEdit,
+  onDelete,
+  onComplete,
+}: {
+  tasks: any[]
+  onEdit: (task: any) => void
+  onDelete: (task: any) => void
+  onComplete: (task: any) => void
+}) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {tasks.length > 0 ? (
-        tasks.map((task) => <TaskCard key={task._id} simpleTask={task} />)
+        tasks.map((task) => (
+          <TaskCard
+            key={task._id}
+            simpleTask={task}
+            onEdit={() => onEdit(task)}
+            onDelete={() => onDelete(task)}
+            onComplete={() => onComplete(task)}
+          />
+        ))
       ) : (
         <p className="text-gray-500 col-span-full">No tasks today.</p>
       )}
