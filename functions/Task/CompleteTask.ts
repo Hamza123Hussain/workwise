9b6e9e6db2c88619496f973ee0f40c7685b8c483
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-01-26 15:39:47 +0500

    Update CompleteTask.ts

--- functions/Task/CompleteTask.ts
+++ functions/Task/CompleteTask.ts
@@ -1,16 +1,27 @@
 import axios from 'axios'
+import { updateKPI } from '../Kpi/UpdateKpi'
 
 //----------------------------------
 // 2️⃣ UPDATE TASK
 //----------------------------------
-export const completeTask = async (taskId: string, completed: boolean) => {
+export const completeTask = async (
+  taskId: string,
+  completed: boolean,
+  Userid: string,
+  taskname: string,
+) => {
   try {
     const response = await axios.put(
       `https://crm-backend-wcpj.vercel.app/Api/TaskBoard/CompleteTask?id=${taskId}`,
       { completed },
     )
 
-    return response.data
+    if (response.data) {
+      const Data = await updateKPI(Userid, taskname)
+      if (Data.success) {
+        return Data.success
+      }
+    }
   } catch (error) {
     console.error('Error updating task:', error)
     throw error
