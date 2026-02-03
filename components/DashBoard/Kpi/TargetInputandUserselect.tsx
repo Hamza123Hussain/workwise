788a8eb8c1076d3a698b2e543f44b5985b74e4b2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-02-03 12:37:47 +0500

    Update TargetInputandUserselect.tsx

--- components/DashBoard/Kpi/TargetInputandUserselect.tsx
+++ components/DashBoard/Kpi/TargetInputandUserselect.tsx
@@ -1,49 +1,4 @@
-import { Allusers } from '@/functions/AUTH/Allusers'
-import { setUser } from '@/utils/Redux/Slice/User_Selected_Slice/Slice'
-import { RootState } from '@/utils/Redux/Store/Store'
 import { motion } from 'framer-motion'
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
-/* =========================
-   Assigned User Select
-========================= */
-const AssignedUserSelect = () => {
-  const [allUsers, setAllUsers] = useState<any[]>([])
-  const dispatch = useDispatch()
-  const userSelect = useSelector((state: RootState) => state.UserSelect)
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const data = await Allusers('octtoppus1@gmail.com')
-      if (data) setAllUsers(data)
-    }
-    fetchUsers()
-  }, [])
-
-  return (
-    <div>
-      <label className="block text-gray-600 mb-2">Assigned To</label>
-      <select
-        value={userSelect.id || ''}
-        onChange={(e) => {
-          const user = allUsers.find((u) => u._id === e.target.value)
-          if (user) {
-            dispatch(setUser({ id: user._id, name: user.Name || user.Email }))
-          }
-        }}
-        className="w-full p-3 border-2 rounded-md focus:ring-2 focus:ring-blue-500"
-      >
-        <option value="">Select User</option>
-        {allUsers.map((user) => (
-          <option key={user._id} value={user._id}>
-            {user.Name || user.Email}
-          </option>
-        ))}
-      </select>
-    </div>
-  )
-}
 
 /* =========================
    Target Input
@@ -138,23 +93,5 @@ export const TargetInput = ({
         <option value="Low">Low</option>
       </select>
     </motion.div>
-
-    {/* Total Points (Auto-calculated later) */}
-    <motion.div
-      initial={{ opacity: 0, x: -20 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.45 }}
-    >
-      <label className="block text-gray-600 mb-1">Total Points</label>
-      <input
-        type="number"
-        value={target.TotalPoints}
-        disabled
-        className="w-full p-3 border-2 rounded-md bg-gray-100"
-      />
-    </motion.div>
-
-    {/* Assigned User */}
-    <AssignedUserSelect />
   </div>
 )
