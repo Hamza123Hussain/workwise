1f6385eb6958d165f4a1ea8459cca1a2b1862bb9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-02-12 14:13:35 +0500

    Update DeleteRole.tsx

--- components/RoleTask/DeleteRole.tsx
+++ components/RoleTask/DeleteRole.tsx
@@ -4,18 +4,20 @@ interface DeleteConfirmProps {
   TaskID: string
   RoleTasksId: string
   userId: string
+  onDeleted: any
 }
 
 export default function DeleteConfirm({
   RoleTasksId,
   userId,
   TaskID,
+  onDeleted,
 }: DeleteConfirmProps) {
   // Example function call inside DeleteConfirm
   const handleDeleteTask = async () => {
     if (confirm('Delete this task?')) {
       await UpdateRoleTasks(RoleTasksId, userId, { RemoveTaskId: TaskID })
-      // trigger refresh
+      onDeleted()
     }
   }
 
