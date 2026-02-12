9514688aaaca89a340c04b7ae38991bdcdce5344
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-02-12 13:49:38 +0500

    Update Roles.tsx

--- components/RoleTask/Roles.tsx
+++ components/RoleTask/Roles.tsx
@@ -1,14 +1,11 @@
-import { useSelector } from 'react-redux'
 import AddRoleForm from './RoleForm'
 import RoleList from './RoleList'
-import { RootState } from '@/utils/Redux/Store/Store'
 
 export default function RolesPage() {
-  const User = useSelector((state: RootState) => state.user)
   return (
     <main className="p-6 max-w-6xl mx-auto">
       <h1 className="text-3xl font-semibold mb-6">Tasks Management</h1>
-      <AddRoleForm userId={User._id} />
+      <AddRoleForm />
       <RoleList />
     </main>
   )
